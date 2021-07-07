5845390f3963fcf85e832f1e6066123caa0790bf
Artem Dudinskyi <artem.dudynskiy@gmail.com>
2021-07-07 22:30:33 +0300

    eject app

---
+++ android/app/src/main/java/co/wonder/wonderapp/generated/BasePackageList.java
@@ -0,0 +1,46 @@
+package co.wonder.wonderapp.generated;
+
+import java.util.Arrays;
+import java.util.List;
+import org.unimodules.core.interfaces.Package;
+
+public class BasePackageList {
+  public List<Package> getPackageList() {
+    return Arrays.<Package>asList(
+        new expo.modules.appauth.AppAuthPackage(),
+        new expo.modules.application.ApplicationPackage(),
+        new expo.modules.av.AVPackage(),
+        new expo.modules.branch.BranchPackage(),
+        new expo.modules.camera.CameraPackage(),
+        new expo.modules.constants.ConstantsPackage(),
+        new expo.modules.contacts.ContactsPackage(),
+        new expo.modules.crypto.CryptoPackage(),
+        new expo.modules.device.DevicePackage(),
+        new expo.modules.errorrecovery.ErrorRecoveryPackage(),
+        new expo.modules.facebook.FacebookPackage(),
+        new expo.modules.filesystem.FileSystemPackage(),
+        new expo.modules.firebase.analytics.FirebaseAnalyticsPackage(),
+        new expo.modules.firebase.core.FirebaseCorePackage(),
+        new expo.modules.font.FontLoaderPackage(),
+        new expo.modules.google.signin.GoogleSignInPackage(),
+        new expo.modules.imageloader.ImageLoaderPackage(),
+        new expo.modules.imagemanipulator.ImageManipulatorPackage(),
+        new expo.modules.permissions.PermissionsPackage(),
+        new expo.modules.imagepicker.ImagePickerPackage(),
+        new expo.modules.keepawake.KeepAwakePackage(),
+        new expo.modules.lineargradient.LinearGradientPackage(),
+        new expo.modules.localization.LocalizationPackage(),
+        new expo.modules.location.LocationPackage(),
+        new expo.modules.medialibrary.MediaLibraryPackage(),
+        new expo.modules.notifications.NotificationsPackage(),
+        new expo.modules.screenorientation.ScreenOrientationPackage(),
+        new expo.modules.securestore.SecureStorePackage(),
+        new expo.modules.sms.SMSPackage(),
+        new expo.modules.splashscreen.SplashScreenPackage(),
+        new expo.modules.sqlite.SQLitePackage(),
+        new expo.modules.updates.UpdatesPackage(),
+        new expo.modules.videothumbnails.VideoThumbnailsPackage(),
+        new expo.modules.webbrowser.WebBrowserPackage()
+    );
+  }
+}
