18f88d19cb9cffe5e4652882877f1c20e7415026
coffeenaut <coffeenaut815@gmail.com>
2021-12-05 13:17:01 -0500

    form UI fully added

--- wondrous-app/pages/index.tsx
+++ wondrous-app/pages/index.tsx
@@ -13,18 +13,14 @@ const Login = () => {
 	const [email, setEmail] = useState("");
 	const [password, setPassword] = useState("");
 
-	const validForm = () => {
-		return email.length > 0 && password.length > 0;
+	const handleSubmit = (event) => {
+		event.preventDefault();
+		//store email & password
+		// route to home ---- router.push('/home')
 	}
 
-	const handleSubmit = (event) => {
-		if(validForm) {
-			//store email & password
-			// route to home ---- () => router.push('/home')}
-		}
-		else {
-			//print error
-		}
+	const loginWithWallet = (event) => {
+		// do wallet login work here
 	}
 
 	return (
@@ -35,10 +31,10 @@ const Login = () => {
 					<SmallLogo />
 					<h1>Login</h1>
 					<Form onSubmit={handleSubmit}>
-						<Field type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email address" url="/images/login/email.png"/>
-						<Field type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" url="/images/login/lock.png"/>
+						<Field type="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter email address" url="/images/login/email.png" required/>
+						<Field type="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter password" url="/images/login/lock.png" required/>
 						<Button highlighted type="submit" marginTop="25px">
-							Log me in
+								Log me in
 						</Button>
 					</Form>
 					<LineWithText>
@@ -46,9 +42,9 @@ const Login = () => {
 							or
 						</PaddedParagraph>
 					</LineWithText>
-					<Button>
+					<Button onClick={loginWithWallet}>
 						<Image src="/images/login/metamask-logo.png" alt="" height="18" width="17"/>
-						<PaddedParagraph padding="0 10px" color="white">
+						<PaddedParagraph padding="0 10px">
 							Log in with MetaMask
 						</PaddedParagraph>
 					</Button>
