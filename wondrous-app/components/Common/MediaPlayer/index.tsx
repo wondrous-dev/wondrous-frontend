f7a4c7f5fe006383d1057d8cdc53f31ba3ed82a4
Joaquin Ayuso de Paul <joaxap@gmail.com>
2021-12-14 12:29:54 -0800

    More cleanup on Media Component based on comments

--- wondrous-app/components/Common/MediaPlayer/index.tsx
+++ wondrous-app/components/Common/MediaPlayer/index.tsx
@@ -8,42 +8,47 @@ import {
 } from './styles'
 
 export const TaskMedia = (props) => {
-	const { id = '', media = {}, } = props
+	const { id = '', media = {} } = props
 	const { type = '', url = '' } = media
+
+	const mediaContentComponents = {
+		image: <TaskImage src={url} />,
+		video: (
+			<TaskVideo
+				url={url}
+				config={{
+					youtube: {
+						playerVars: {
+							controls: 0,
+						},
+					},
+				}}
+				width="100%"
+				height="100%"
+			/>
+		),
+		audio: (
+			<TaskAudio
+				url={url}
+				config={{
+					soundcloud: {
+						show_artwork: false,
+						download: false,
+						sharing: false,
+					},
+				}}
+				width="100%"
+				height="54px"
+			/>
+		),
+	}
+	const mediaContent = mediaContentComponents[type] || (
+		<TaskMediaUnsuported>Medua Not supported.</TaskMediaUnsuported>
+	)
 
 	return (
 		<TaskMediaWrapper key={'media-task-' + id}>
-			{type == 'image' ? (
-				<TaskImage src={url} />
-			) : type == 'video' ? (
-				<TaskVideo
-					url={url}
-					config={{
-						youtube: {
-							playerVars: {
-								controls: 0,
-							},
-						},
-					}}
-					width="100%"
-					height="100%"
-				/>
-			) : type == 'audio' ? (
-				<TaskAudio
-					url={url}
-					config={{
-						soundcloud: {
-							show_artwork: false,
-							download: false,
-							sharing: false,
-						},
-					}}
-					width="100%"
-					height="54px"
-				/>
-			) : (
-				<TaskMediaUnsuported>Medua Not supported.</TaskMediaUnsuported>
-			)}
+			{mediaContent}
 		</TaskMediaWrapper>
 	)
 }
