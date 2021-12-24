004e96da3f47eab0e574dd3930f69a6bc1e4136f
juniusfree <juniusfree@gmail.com>
2021-12-24 18:00:46 +0800

    Added profile/about page and the related componets

---
+++ wondrous-app/components/profile/tabs/styles.tsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import styled from 'styled-components'
+import { Tabs } from '@material-ui/core'
+
+export const StyledTabs = styled((props) => (
+	<Tabs
+		{...props}
+		TabIndicatorProps={{ children: <span className="MuiTabs-indicatorSpan" /> }}
+	/>
+))({
+	'&.MuiTabs-root': {
+		marginTop: 30,
+		width: 1037,
+		margin: '0 auto',
+	},
+	'& .MuiTabs-flexContainer': {
+		display: 'flex',
+		justifyContent: 'space-between',
+		maxWidth: 680,
+		margin: '0 auto',
+
+		'& a': {
+			textDecoration: 'none',
+		},
+	},
+	'& .MuiButtonBase-root': {
+		position: 'relative',
+		width: 210,
+		borderBottom: '2px solid #4B4B4B',
+
+		//text
+		fontWeight: 600,
+		fontSize: '16px',
+		lineHeight: '19px',
+		textAlign: 'center',
+		color: '#FFFFFF',
+	},
+	'& .MuiTabs-indicator': {
+		display: 'flex',
+		justifyContent: 'center',
+		backgroundColor: 'transparent',
+	},
+	'& .MuiTabs-indicatorSpan': {
+		maxWidth: 210,
+		width: '100%',
+		backgroundColor: '#7427FF',
+	},
+})
---
+++ wondrous-app/components/profile/tabs/tabs.tsx
@@ -0,0 +1,51 @@
+import { Tab } from '@material-ui/core'
+import Link from 'next/link'
+import { useRouter } from 'next/router'
+import React from 'react'
+import { StyledTabs } from './styles'
+
+const tabsLinks = [
+	{
+		href: '/profile/boards',
+		label: 'Boards',
+	},
+	{
+		href: '/profile/activities',
+		label: 'Activity',
+	},
+	{
+		href: '/profile/about',
+		label: 'About',
+	},
+]
+
+const Tabs = (props) => {
+	const { children } = props
+
+	const router = useRouter()
+
+	const { pathname } = router
+
+	return (
+		<div>
+			{console.log(pathname)}
+			<StyledTabs value={pathname}>
+				{tabsLinks.map((tab) => (
+					<Link
+						// @ts-ignore
+						value={tab.href}
+						key={tab.href}
+						href={tab.href}
+					>
+						<a>
+							<Tab label={tab.label} />
+						</a>
+					</Link>
+				))}
+			</StyledTabs>
+			<div>{children}</div>
+		</div>
+	)
+}
+
+export default Tabs
