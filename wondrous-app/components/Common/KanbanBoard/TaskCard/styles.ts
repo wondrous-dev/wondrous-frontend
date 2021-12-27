bb3b5f649bcc709107fc85bfdad074834580f692
juniusfree <juniusfree@gmail.com>
2021-12-27 09:44:57 +0800

    Added a border to TaskCardWrapper

--- wondrous-app/components/Common/KanbanBoard/TaskCard/styles.ts
+++ wondrous-app/components/Common/KanbanBoard/TaskCard/styles.ts
@@ -1,18 +1,37 @@
-import {Card, IconButton, Typography} from "@material-ui/core";
-import {LogoSquare} from "../../ci";
-import UserAvatar from "../../../Icons/userAvatar";
-import styled from "styled-components";
+import { Card, IconButton, Typography } from '@material-ui/core'
+import { LogoSquare } from '../../ci'
+import UserAvatar from '../../../Icons/userAvatar'
+import styled from 'styled-components'
 
-export const TaskCardWrapper = styled(Card)({
-  '&.MuiCard-root': {
-    width: '100%',
-    minHeight: 273,
-    background: 'linear-gradient(180deg, #1E1E1E 0%, #141414 100%)',
-    borderRadius: 6,
-    padding: 15,
-    marginTop: 30,
-  },
-})
+export const TaskCardWrapper = styled(Card)`
+	&.MuiCard-root {
+		width: 100%;
+		min-height: 273px;
+		background: linear-gradient(
+			169.47deg,
+			rgba(75, 75, 75, 0.6) 7.84%,
+			rgba(35, 35, 35, 0.6) 108.71%
+		);
+		border-radius: 6px;
+		padding: 15px;
+		margin-top: 30px;
+		position: relative;
+		z-index: 1;
+	}
+
+	&.MuiCard-root:before {
+		content: '';
+		position: absolute;
+		top: 0;
+		left: 0;
+		bottom: 0;
+		right: 0;
+		margin: 0.05em;
+		z-index: -1;
+		border-radius: inherit;
+		background: linear-gradient(180deg, #1e1e1e 0%, #141414 100%);
+	}
+`
 
 export const TaskCardHeader = styled.div`
 	width: 100%;
