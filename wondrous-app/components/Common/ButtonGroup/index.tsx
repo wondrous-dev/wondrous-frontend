b38d0f9fa8d7050fe1b4b2d03ba458910eb85b7a
juniusfree <juniusfree@gmail.com>
2021-12-29 12:45:32 +0800

    Updated the ButtonGroup

--- wondrous-app/components/Common/ButtonGroup/index.tsx
+++ wondrous-app/components/Common/ButtonGroup/index.tsx
@@ -1,11 +1,26 @@
-import * as React from 'react'
+import { useState } from 'react'
 import { ButtonGroupStyled, ButtonStyled } from './styles'
 
 export const ButtonGroup = () => {
+	const [selected, setSelected] = useState(0)
+
+	const handleClick = (index) => {
+		setSelected(index)
+	}
+
+	const buttons = ['List', 'Grid']
+
 	return (
-		<ButtonGroupStyled variant="contained" aria-label="outlined primary button group">
-			<ButtonStyled>List</ButtonStyled>
-			<ButtonStyled>Grid</ButtonStyled>
+		<ButtonGroupStyled>
+			{buttons.map((button, index) => (
+				<ButtonStyled
+					key={index}
+					onClick={() => handleClick(index)}
+					variant={selected === index ? 'contained' : 'outlined'}
+				>
+					{button}
+				</ButtonStyled>
+			))}
 		</ButtonGroupStyled>
 	)
 }
