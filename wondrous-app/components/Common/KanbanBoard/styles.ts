94d5a589df13042005ab10a407eb4d21249ae85a
Joaquin Ayuso de Paul <joaxap@gmail.com>
2022-01-12 12:05:03 -0800

    Drop zone to be as high as column

--- wondrous-app/components/Common/KanbanBoard/styles.ts
+++ wondrous-app/components/Common/KanbanBoard/styles.ts
@@ -4,6 +4,8 @@ export const KanbanBoardContainer = styled.div`
 	width: 100%;
 	height: 100%;
 	display: flex;
+	flex-direction: row;
+	align-items: stretch;
 	justify-content: space-between;
 	margin-top: 65px;
 `
