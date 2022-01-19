129dd7fa404426543795176665e558a41e355423
Valentyn Yefimov <valentine.efimov@gmail.com>
2022-01-20 00:03:02 +0200

    Fragments for organization roles

--- wondrous-app/graphql/mutations/org.ts
+++ wondrous-app/graphql/mutations/org.ts
@@ -1,10 +1,36 @@
-import { gql } from '@apollo/client'
-
+import { gql } from '@apollo/client';
+import { OrgRoleFragment } from '../fragments/org';
 
 export const CREATE_ORG_INVITE_LINK = gql`
-    mutation createOrgInviteLink($input: OrgInviteLinkInput) {
-        createOrgInviteLink (input: $input) {
-            token
-        }
+  mutation createOrgInviteLink($input: OrgInviteLinkInput) {
+    createOrgInviteLink(input: $input) {
+      token
     }
-`
+  }
+`;
+
+export const CREATE_ORG_ROLE = gql`
+  mutation createOrgRole($input: OrgRoleInput) {
+    createOrgRole(input: $input) {
+      ...OrgRoleFragment
+    }
+  }
+  ${OrgRoleFragment}
+`;
+
+export const UPDATE_ORG_ROLE = gql`
+  mutation updateOrgRole($input: OrgRoleUpdateInput) {
+    updateOrgRole(input: $input) {
+      ...OrgRoleFragment
+    }
+  }
+  ${OrgRoleFragment}
+`;
+
+export const DELETE_ORG_ROLE = gql`
+  mutation deleteOrgRole($id: ID!) {
+    deleteOrgRole(id: $id) {
+      success
+    }
+  }
+`;
