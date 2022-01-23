082f7eb57edaf9d4a8469579602ba4fd13025477
Valentyn Yefimov <valentine.efimov@gmail.com>
2022-01-23 20:46:21 +0200

    Fix lint errors

--- wondrous-app/components/Common/Image/DefaultUserImage.tsx
+++ wondrous-app/components/Common/Image/DefaultUserImage.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
 
 export default function DefaultUserImage(props) {
-  return <img src="/images/default/user-image.png" {...props} />;
+  // eslint-disable-next-line @next/next/no-img-element
+  return <img src="/images/default/user-image.png" {...props} alt="User" />;
 }
