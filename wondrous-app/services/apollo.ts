643a365334a1f765d51046dc162f61470a73f425
Andros <wong22@hotmail.co.uk>
2022-01-23 13:58:52 -0600

    fix: use new https link

--- wondrous-app/services/apollo.ts
+++ wondrous-app/services/apollo.ts
@@ -1,74 +1,59 @@
-import {
-	ApolloClient,
-	InMemoryCache,
-	HttpLink,
-	split,
-	defaultDataIdFromObject,
-} from '@apollo/client'
-import { setContext } from '@apollo/client/link/context'
-import { getMainDefinition, offsetLimitPagination } from '@apollo/client/utilities'
-import {
-	getAuthHeader,
-	getWaitlistAuthHeader,
-} from '../components/Auth/withAuth'
+import { ApolloClient, InMemoryCache, HttpLink, split, defaultDataIdFromObject } from '@apollo/client';
+import { setContext } from '@apollo/client/link/context';
+import { getMainDefinition, offsetLimitPagination } from '@apollo/client/utilities';
+import { getAuthHeader, getWaitlistAuthHeader } from '../components/Auth/withAuth';
 
 // Staging is http://34.135.9.199/graphql
-const graphqlUri =
-	process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL || 'http://34.135.9.199/graphql'
+const graphqlUri = process.env.NEXT_PUBLIC_GRAPHQL_SERVER_URL || 'https://apistaging.wonderapp.co/graphql';
 
-const wsBaseUrl = graphqlUri
-const wsUri = wsBaseUrl.replace(
-	/^https?/,
-	process.env.NEXT_PUBLIC_ENV === 'dev' ? 'ws' : 'wss'
-)
+const wsBaseUrl = graphqlUri;
+const wsUri = wsBaseUrl.replace(/^https?/, process.env.NEXT_PUBLIC_ENV === 'dev' ? 'ws' : 'wss');
 
 const httpLink = new HttpLink({
-	uri: graphqlUri,
-	credentials: 'include',
-})
+  uri: graphqlUri,
+  credentials: 'include',
+});
 
 const getAuth = () => {
-	const token = getAuthHeader()
-	return token ? `Bearer ${token}` : ''
-}
+  const token = getAuthHeader();
+  return token ? `Bearer ${token}` : '';
+};
 
 const getWaitlistAuth = () => {
-	const token = getWaitlistAuthHeader()
-	return token ? `Bearer ${token}` : ''
-}
+  const token = getWaitlistAuthHeader();
+  return token ? `Bearer ${token}` : '';
+};
 
 const authLink = setContext((_, { headers }) => {
-	let auth = getAuth() || getWaitlistAuth()
+  let auth = getAuth() || getWaitlistAuth();
 
-	return {
-		headers: {
-			...headers,
-			Authorization: auth,
-		},
-	}
-})
+  return {
+    headers: {
+      ...headers,
+      Authorization: auth,
+    },
+  };
+});
 
 const cache = new InMemoryCache({
-	typePolicies: {
-		Query: {
-			fields: {
-				whoami(existingData, { args, toReference }) {
-					return existingData || toReference({ __typename: 'User', ...args })
-				},
-				users(existingData, { args, toReference }) {
-					return existingData || toReference({ __typename: 'User', ...args })
-				},
-				waitlistUsers(existingData, { args, toReference }) {
-					return (
-						existingData || toReference({ __typename: 'WaitlistUser', ...args })
-					)
-				},
-			},
-		},
-	},
-})
+  typePolicies: {
+    Query: {
+      fields: {
+        whoami(existingData, { args, toReference }) {
+          return existingData || toReference({ __typename: 'User', ...args });
+        },
+        users(existingData, { args, toReference }) {
+          return existingData || toReference({ __typename: 'User', ...args });
+        },
+        waitlistUsers(existingData, { args, toReference }) {
+          return existingData || toReference({ __typename: 'WaitlistUser', ...args });
+        },
+      },
+    },
+  },
+});
 
 export default new ApolloClient({
-	link: authLink.concat(httpLink),
-	cache,
-})
+  link: authLink.concat(httpLink),
+  cache,
+});
