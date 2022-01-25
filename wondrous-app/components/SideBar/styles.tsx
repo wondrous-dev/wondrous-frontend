14e84e4cf416cd5e0047e2c97f7e161fc4bbf804
Andros <wong22@hotmail.co.uk>
2022-01-24 21:44:01 -0600

    feat: fix pod pagination

--- wondrous-app/components/SideBar/styles.tsx
+++ wondrous-app/components/SideBar/styles.tsx
@@ -1,113 +1,109 @@
-import React from 'react'
-import styled from 'styled-components'
-import { Drawer, IconButton, List, ListItem } from '@material-ui/core'
-import { Black97 } from '../../theme/colors'
-import { SIDEBAR_WIDTH } from '../../utils/constants'
+import React from 'react';
+import styled from 'styled-components';
+import { Drawer, IconButton, List, ListItem } from '@material-ui/core';
+import { Black97 } from '../../theme/colors';
+import { SIDEBAR_WIDTH } from '../../utils/constants';
 
 export const DrawerComponent = styled(Drawer)`
-	&& {
-		& .MuiDrawer-paperAnchorDockedLeft {
-			background-color: ${Black97};
-			z-index: 200;
-			margin-top: 50px;
-			transition: 0.3s;
-		}
+  && {
+    & .MuiDrawer-paperAnchorDockedLeft {
+      background-color: ${Black97};
+      z-index: 200;
+      margin-top: 50px;
+      transition: 0.3s;
+    }
 
-		&.active .MuiDrawer-paperAnchorDockedLeft {
-			left: -80px;
-		}
-	}
-`
+    &.active .MuiDrawer-paperAnchorDockedLeft {
+      left: -80px;
+    }
+  }
+`;
 
 export const DrawerContainer = styled.div`
-	width: ${SIDEBAR_WIDTH};
-	display: flex;
-	flex-grow: 1;
-	flex-direction: column;
-	justify-content: space-between;
-	align-items: center;
-	padding-top: 50px;
-`
+  width: ${SIDEBAR_WIDTH};
+  display: flex;
+  flex-grow: 1;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  padding-top: 50px;
+`;
 
 export const DrawerTopBlock = styled.div`
-	min-height: 250px;
-	display: flex;
-	align-items: flex-start;
-	flex-direction: column;
-	justify-content: space-evenly;
-`
+  min-height: 250px;
+  display: flex;
+  align-items: flex-start;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
 
 export const DrawerUserImage = styled.img`
-	display: flex;
-	margin: 0 auto;
-	width: 48px;
-	height: 48px;
-`
+  display: flex;
+  margin: 0 auto;
+  width: 48px;
+  height: 48px;
+`;
 
 export const DrawerList = styled(List)`
-	& {
-		min-height: 152px;
-		display: flex;
-		flex-direction: column;
-		align-content: flex-start;
-		justify-content: space-evenly;
-		padding: 0;
-		margin: 1em auto;
-	}
-`
+  & {
+    min-height: 152px;
+    display: flex;
+    flex-direction: column;
+    align-content: flex-start;
+    justify-content: flex-start;
+    padding: 0;
+    margin: 1em auto;
+  }
+`;
 export const DrawerListItem = styled(ListItem)`
-	& {
-		display: flex;
-		height: 36px;
-		padding: 0.5em auto;
-		margin: 0.5em auto;
-	}
-`
+  & {
+    display: flex;
+    height: 36px;
+    padding: 0.5em auto;
+    margin: 0.5em auto;
+  }
+`;
 
 export const DrawerListItemIcon = styled.img`
-	width: 36px;
-	height: 36px;
-`
+  width: 36px;
+  height: 36px;
+`;
 
 export const DrawerBottomBlock = styled.div`
-	min-height: 85px;
-	display: flex;
-	align-items: center;
-	justify-content: space-between;
-	flex-direction: column;
-	margin-bottom: 120px;
-`
+  min-height: 85px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  flex-direction: column;
+  margin-bottom: 120px;
+`;
 export const DrawerBottomButton = styled(IconButton)`
-	&& {
-		width: 45px;
-		height: 45px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
-	}
-`
+  && {
+    width: 45px;
+    height: 45px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+`;
 
 export const DrawerBackButton = styled(DrawerBottomButton)`
-	&& {
-		position: fixed;
-		bottom: 20px;
-		left: 20px;
-		width: 45px;
-		height: 45px;
-		display: flex;
-		align-items: center;
-		justify-content: center;
+  && {
+    position: fixed;
+    bottom: 20px;
+    left: 20px;
+    width: 45px;
+    height: 45px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
 
-		background: linear-gradient(
-			169.47deg,
-			rgba(75, 75, 75, 0.6) 7.84%,
-			rgba(35, 35, 35, 0.6) 108.71%
-		);
+    background: linear-gradient(169.47deg, rgba(75, 75, 75, 0.6) 7.84%, rgba(35, 35, 35, 0.6) 108.71%);
 
-		transition: transform 0.2s;
+    transition: transform 0.2s;
 
-		&.active {
-			transform: rotate(180deg);
-		}
-	}
-`
+    &.active {
+      transform: rotate(180deg);
+    }
+  }
+`;
