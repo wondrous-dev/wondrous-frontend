9c257707a2d5b6bd2b60b90f347bd6a10cc86c69
Andros <wong22@hotmail.co.uk>
2022-01-26 17:17:17 -0600

    fix: fetch submissions and proposals

--- wondrous-app/pages/pod/[podId]/boards.tsx
+++ wondrous-app/pages/pod/[podId]/boards.tsx
@@ -19,6 +19,7 @@ import {
   TASK_STATUS_IN_REVIEW,
   TASK_STATUS_ARCHIVED,
   DEFAULT_STATUS_ARR,
+  STATUS_OPEN,
 } from '../../../utils/constants';
 
 import { PodBoardContext } from '../../../utils/contexts';
@@ -172,7 +173,7 @@ const BoardsPage = () => {
         variables: {
           input: {
             podId,
-            statuses,
+            statuses: [STATUS_OPEN],
             offset: 0,
             limit: LIMIT,
           },
@@ -182,7 +183,7 @@ const BoardsPage = () => {
         variables: {
           input: {
             podId,
-            statuses,
+            statuses: [STATUS_OPEN],
             offset: 0,
             limit: LIMIT,
           },
