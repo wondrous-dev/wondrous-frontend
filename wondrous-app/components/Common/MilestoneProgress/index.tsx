18acbfed26221c3b9ad18c67c8d733e15623dae8
juniusfree <juniusfree@gmail.com>
2022-01-27 21:27:27 +0800

    fix: type error in MilestoneProgress

--- wondrous-app/components/Common/MilestoneProgress/index.tsx
+++ wondrous-app/components/Common/MilestoneProgress/index.tsx
@@ -12,12 +12,12 @@ import {
 
 export const MilestoneProgress = (props) => {
     const { milestoneId, color = "#396CFF" } = props
-    const [tasksTotal, setTaskTotal] = useState(0)
-    const [tasksCompleted, setTaskCompleted] = useState(0)
+    const [tasksTotal, setTaskTotal] = useState(null)
+    const [tasksCompleted, setTaskCompleted] = useState(null)
     const [getPerStatusTaskCountForMilestone] = useLazyQuery(GET_PER_STATUS_TASK_COUNT_FOR_MILESTONE, {
         onCompleted: (data) => {
             const { getPerStatusTaskCountForMilestone } = data
-            setTaskTotal(Object.values(getPerStatusTaskCountForMilestone).filter(i => typeof i === "number").reduce((a, b) => a + b, 0))
+            setTaskTotal(Object.values(getPerStatusTaskCountForMilestone)?.filter(i => typeof (i) === "number")?.reduce((a: number, b: number) => a + b, 0) ?? 0)
             setTaskCompleted(getPerStatusTaskCountForMilestone?.completed + getPerStatusTaskCountForMilestone?.archived)
         }
     })
