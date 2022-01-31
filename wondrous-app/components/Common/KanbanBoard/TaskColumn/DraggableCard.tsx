59dbaa33e47e1570e05eee454396ce0467d8750d
Andros <wong22@hotmail.co.uk>
2022-01-30 22:01:16 -0600

    fix: make dragging cards autoscroll

--- wondrous-app/components/Common/KanbanBoard/TaskColumn/DraggableCard.tsx
+++ wondrous-app/components/Common/KanbanBoard/TaskColumn/DraggableCard.tsx
@@ -1,58 +1,53 @@
-import { forwardRef, useImperativeHandle, useRef } from 'react'
-import { DragSource, DropTarget } from 'react-dnd'
+import { forwardRef, useImperativeHandle, useRef } from 'react';
+import { DragSource, DropTarget } from 'react-dnd';
+import SmoothScrollPlugin from '../../../../utils/dragAndDrop';
 
 export const ItemTypes = {
   CARD: 'card',
-}
+};
+const plugin = new SmoothScrollPlugin();
 
-const Card = (
-  { children, isDragging, isSpacer, connectDragSource, connectDropTarget },
-  ref
-) => {
-  const elementRef = useRef(null)
-  connectDragSource(elementRef)
-  connectDropTarget(elementRef)
-  const opacity = isDragging ? 0 : 1
+const Card = ({ children, isDragging, isSpacer, connectDragSource, connectDropTarget }, ref) => {
+  const elementRef = useRef(null);
+  connectDragSource(elementRef);
+  connectDropTarget(elementRef);
+  const opacity = isDragging ? 0 : 1;
   useImperativeHandle(ref, () => ({
     getNode: () => elementRef.current,
-  }))
+  }));
   return (
-    <div
-      ref={elementRef}
-      style={{ opacity, width: '100%' }}
-      className="draggable"
-    >
+    <div ref={elementRef} style={{ opacity, width: '100%' }} className="draggable">
       {children}
     </div>
-  )
-}
+  );
+};
 
 const DraggableCard = DropTarget(
   ItemTypes.CARD,
   {
     hover(props, monitor, component) {
-      const { moveCard, status: columnStatus } = props as any
+      const { moveCard, status: columnStatus } = props as any;
 
       if (!component) {
-        return null
+        return null;
       }
       // node = HTML Div element from imperative API
-      const node = component.getNode()
+      const node = component.getNode();
       if (!node) {
-        return null
+        return null;
       }
-      const draggableItem = monitor.getItem()
-      const { id, orgId, podId, status: dragStatus } = draggableItem
+      const draggableItem = monitor.getItem();
+      const { id, orgId, podId, status: dragStatus } = draggableItem;
 
-      const statusesEqual = dragStatus === columnStatus
+      const statusesEqual = dragStatus === columnStatus;
 
       if (statusesEqual) {
-        return
+        return;
       }
 
-      moveCard(id, columnStatus)
+      moveCard(id, columnStatus);
 
-      monitor.getItem().status = columnStatus
+      monitor.getItem().status = columnStatus;
     },
   },
   (connect) => ({
@@ -62,20 +57,26 @@ const DraggableCard = DropTarget(
   DragSource(
     ItemTypes.CARD,
     {
-      beginDrag: (props: any) => ({
-        id: props.id,
-        assigneeId: props.assigneeId,
-        createdBy: props.createdBy,
-        orgId: props.orgId,
-        podId: props.podId,
-        status: props.status,
-      }),
+      beginDrag: (props: any) => {
+        plugin.onDragStart();
+        return {
+          id: props.id,
+          assigneeId: props.assigneeId,
+          createdBy: props.createdBy,
+          orgId: props.orgId,
+          podId: props.podId,
+          status: props.status,
+        };
+      },
+      endDrag: () => {
+        plugin.onDragEnd();
+      },
     },
     (connect, monitor) => ({
       connectDragSource: connect.dragSource(),
       isDragging: monitor.isDragging(),
     })
   )(forwardRef(Card))
-)
+);
 
-export default DraggableCard
+export default DraggableCard;
