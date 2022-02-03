86c0c37d0433ad4c8f74ea13ae1d490e92f8f054
Andros <wong22@hotmail.co.uk>
2022-02-02 23:18:36 -0600

    fix: add email verification

--- wondrous-app/graphql/mutations/user.ts
+++ wondrous-app/graphql/mutations/user.ts
@@ -1,44 +1,50 @@
-import { gql } from '@apollo/client'
-import { LoggedinUserFragment, ProfileUserFragment } from '../fragments/user'
+import { gql } from '@apollo/client';
+import { LoggedinUserFragment, ProfileUserFragment } from '../fragments/user';
 
 export const LOGIN_MUTATION = gql`
-	mutation emailSignin($email: String!, $password: String!) {
-		emailSignin(input: { email: $email, password: $password }) {
-			user {
-				...LoggedinUser
-			}
-			token
-		}
-	}
-	${LoggedinUserFragment}
-`
+  mutation emailSignin($email: String!, $password: String!) {
+    emailSignin(input: { email: $email, password: $password }) {
+      user {
+        ...LoggedinUser
+      }
+      token
+    }
+  }
+  ${LoggedinUserFragment}
+`;
 export const LOGIN_WALLET_MUTATION = gql`
-	mutation signinWithWeb3($web3Address: String!, $signedMessage: String!) {
-		signinWithWeb3(
-			input: { web3Address: $web3Address, signedMessage: $signedMessage }
-		) {
-			user {
-				...LoggedinUser
-			}
-			token
-		}
-	}
-	${LoggedinUserFragment}
-`
+  mutation signinWithWeb3($web3Address: String!, $signedMessage: String!) {
+    signinWithWeb3(input: { web3Address: $web3Address, signedMessage: $signedMessage }) {
+      user {
+        ...LoggedinUser
+      }
+      token
+    }
+  }
+  ${LoggedinUserFragment}
+`;
 
 export const REDEEM_ORG_INVITE_LINK = gql`
-	mutation redeemOrgInviteLink($token: String!) {
-		redeemOrgInviteLink(token: $token) {
-			success
-		}
-	}
-`
+  mutation redeemOrgInviteLink($token: String!) {
+    redeemOrgInviteLink(token: $token) {
+      success
+    }
+  }
+`;
 
 export const UPDATE_USER = gql`
-	mutation updateUser($input: UserUpdateInput!) {
-		updateUser(input: $input) {
-			...UserProfile
-		}
-	}
-	${ProfileUserFragment}
-`
+  mutation updateUser($input: UserUpdateInput!) {
+    updateUser(input: $input) {
+      ...UserProfile
+    }
+  }
+  ${ProfileUserFragment}
+`;
+
+export const CONFIRM_EMAIL_ADDRESS = gql`
+  mutation confirmEmailAddress($token: String!) {
+    confirmEmailAddress(token: $token) {
+      success
+    }
+  }
+`;
