93899ca68571d7c80ff82ade0087ad0de55f17b8
juniusfree <juniusfree@gmail.com>
2022-02-10 11:47:23 +0800

    refactor: moved DashboardPanelWrapper to dashboard

--- wondrous-app/components/Dashboard/styles.tsx
+++
@@ -1,5 +0,0 @@
-import styled from 'styled-components';
-
-export const DashboardPanelWrapper = styled.div`
-  margin-top: -140px;
-`;
--- wondrous-app/pages/dashboard.tsx
+++ wondrous-app/pages/dashboard.tsx
@@ -2,8 +2,12 @@ import React from 'react';
 import { withAuth } from '../components/Auth/withAuth';
 import Boards from '../components/Dashboard/boards';
 import DashboardPanel from '../components/Common/DashboardPanel';
-import { DashboardPanelWrapper } from '../components/Dashboard/styles';
 import Wrapper from '../components/Dashboard/wrapper';
+import styled from 'styled-components';
+
+const DashboardPanelWrapper = styled.div`
+  margin-top: -140px;
+`;
 
 const Dashboard = () => {
   return (
