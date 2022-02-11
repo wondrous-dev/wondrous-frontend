2e0f33ad2a3165bdd1246e6620a32ced091fa26b
terryli0095 <terryli0095@gmail.com>
2022-02-11 01:09:15 -0600

    add wallet redirect

---
+++ wondrous-app/pages/pod/settings/[podId]/wallet.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import Wallets from '../../../../components/Settings/WalletSetup';
+
+const GeneralSettingsPage = () => {
+  return <Wallets />;
+};
+
+export default GeneralSettingsPage;
