7d23e8cb4520a4b3fc1f31d093b8a2420aea4338
juniusfree <juniusfree@gmail.com>
2022-02-16 15:48:57 +0800

    refactor: split the TaskSubtask component

---
+++ wondrous-app/components/Common/TaskSubtaskList/styles.tsx
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+import { Task } from '../Task';
+import { TaskInner, TaskWrapper } from '../Task/styles';
+
+// https://www.designcise.com/web/tutorial/why-is-styled-components-styled-wrapper-not-working-with-existing-react-component
+export const SubtaskTaskItem = styled(Task)`
+  ${TaskWrapper} {
+    padding: 0;
+  }
+  ${TaskInner} {
+    background: #0f0f0f;
+    text-align: left;
+  }
+`;
