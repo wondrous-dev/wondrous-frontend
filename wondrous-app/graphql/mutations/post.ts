bebb447f10391b188afdc51e43ef4b83ec917970
juniusfree <juniusfree@gmail.com>
2022-02-25 18:14:34 +0800

    fix: post mutation graphql

--- wondrous-app/graphql/mutations/post.ts
+++ wondrous-app/graphql/mutations/post.ts
@@ -1,17 +1,20 @@
 import { gql } from '@apollo/client';
+import { PostFragment } from '../fragments/post';
 
 export const CREATE_POST = gql`
   mutation createPost($input: PostInput!) {
     createPost(input: $input) {
-      id
+      ...PostFragment
     }
   }
+  ${PostFragment}
 `;
 
 export const UPDATE_POST = gql`
   mutation updatePost($input: UpdatePostInput!) {
     updatePost(input: $input) {
-      id
+      ...PostFragment
     }
   }
+  ${PostFragment}
 `;
