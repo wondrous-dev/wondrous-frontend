e63f37d15e73cf7cf072186dd86e43e32bb0c2b2
Andros <wong22@hotmail.co.uk>
2022-02-24 22:49:20 -0800

    feat: add is public on frontend as option when creating tasks

--- wondrous-app/pages/signup.tsx
+++ wondrous-app/pages/signup.tsx
@@ -56,10 +56,7 @@ const Signup = () => {
   const signupWithWallet = async () => {
     if (wonderWeb3.address && wonderWeb3.chain && !wonderWeb3.connecting) {
       // Retrieve Signed Message
-      const messageToSign = await getUserSigningMessage(
-        wonderWeb3.address,
-        SupportedChainType.ETH
-      );
+      const messageToSign = await getUserSigningMessage(wonderWeb3.address, SupportedChainType.ETH);
 
       if (messageToSign) {
         const signedMessage = await wonderWeb3.signMessage(messageToSign);
@@ -100,44 +97,49 @@ const Signup = () => {
           <CardBody>
             <SmallLogo />
             <h1>Signup</h1>
-            <Form onSubmit={handleSubmit}>
-              {errorMessage ? <LoginError>{errorMessage}</LoginError> : ''}
-              <Field
-                type="email"
-                name="email"
-                value={email}
-                onChange={(e) => setEmail(e.target.value)}
-                placeholder="Enter email address"
-                icon={EmailIcon}
-                required
-              />
-              <Field
-                type="password"
-                name="password"
-                value={password}
-                onChange={(e) => setPassword(e.target.value)}
-                placeholder="Enter password"
-                icon={LockIcon}
-                required
-              />
-              <Field
-                type="password"
-                name="repassword"
-                value={repassword}
-                onChange={(e) => setRePassword(e.target.value)}
-                placeholder="Re-enter password"
-                icon={LockIcon}
-                required
-              />
-              <Button highlighted type="submit" marginTop="25px">
-                Sign up
-              </Button>
-            </Form>
-            <LineWithText>
-              <PaddedParagraph padding="0 10px" color={Grey50}>
-                or
-              </PaddedParagraph>
-            </LineWithText>
+            {!process.env.NEXT_PUBLIC_PRODUCTION && (
+              <>
+                <Form onSubmit={handleSubmit}>
+                  {errorMessage ? <LoginError>{errorMessage}</LoginError> : ''}
+                  <Field
+                    type="email"
+                    name="email"
+                    value={email}
+                    onChange={(e) => setEmail(e.target.value)}
+                    placeholder="Enter email address"
+                    icon={EmailIcon}
+                    required
+                  />
+                  <Field
+                    type="password"
+                    name="password"
+                    value={password}
+                    onChange={(e) => setPassword(e.target.value)}
+                    placeholder="Enter password"
+                    icon={LockIcon}
+                    required
+                  />
+                  <Field
+                    type="password"
+                    name="repassword"
+                    value={repassword}
+                    onChange={(e) => setRePassword(e.target.value)}
+                    placeholder="Re-enter password"
+                    icon={LockIcon}
+                    required
+                  />
+                  <Button highlighted type="submit" marginTop="25px">
+                    Sign up
+                  </Button>
+                </Form>
+                <LineWithText>
+                  <PaddedParagraph padding="0 10px" color={Grey50}>
+                    or
+                  </PaddedParagraph>
+                </LineWithText>
+              </>
+            )}
+
             {wonderWeb3.connecting ? (
               <Button disabled className="disabled">
                 <PaddedParagraph padding="0 10px">Continue on your wallet</PaddedParagraph>
@@ -154,7 +156,7 @@ const Signup = () => {
               </Button>
             )}
           </CardBody>
-          <CardFooter>
+          {/* <CardFooter>
             <Line size="80%" />
             <CenteredFlexRow marginTop="16px">
               Have an account?&nbsp;
@@ -166,7 +168,7 @@ const Signup = () => {
               &nbsp; or &nbsp;
               <StyledLink href="/forgot-email">email</StyledLink>?
             </CenteredFlexRow>
-          </CardFooter>
+          </CardFooter> */}
         </Card>
       </LoginWrapper>
     </AuthLayout>
