ed2573939949981681e35a3f13e262f7df147a66
juniusfree <juniusfree@gmail.com>
2022-03-01 17:24:45 +0800

    fix: user profile org card layout and button

---
+++ wondrous-app/components/profile/about/aboutOrganizationsCard.tsx
@@ -0,0 +1,47 @@
+import { IconButton } from '@material-ui/core';
+import { useRouter } from 'next/router';
+import React from 'react';
+import { SafeImage } from '../../Common/Image';
+import { DAOIcon } from '../../Icons/dao';
+import RightArrowIcon from '../../Icons/rightArrow';
+import {
+  OrganizationsCard,
+  OrganizationsCardContent,
+  OrganizationsCardHeader,
+  OrganizationsCardHeaderName,
+  OrganisationsCardNoLogo,
+} from './styles';
+
+const AboutOrganizationsCard = (props) => {
+  const router = useRouter();
+  const { name, description, profilePicture, thumbnailPicture, username } = props;
+  const handleOnClick = () => router.push(`/organization/${username}/boards`);
+  return (
+    <OrganizationsCard>
+      <OrganizationsCardHeader>
+        {thumbnailPicture || profilePicture ? (
+          <SafeImage
+            src={thumbnailPicture || profilePicture}
+            style={{
+              width: '40px',
+              height: '40px',
+              borderRadius: '4px',
+              marginRight: '8px',
+            }}
+          />
+        ) : (
+          <OrganisationsCardNoLogo>
+            <DAOIcon />
+          </OrganisationsCardNoLogo>
+        )}
+        <OrganizationsCardHeaderName>{name}</OrganizationsCardHeaderName>
+        <IconButton onClick={handleOnClick}>
+          <RightArrowIcon />
+        </IconButton>
+      </OrganizationsCardHeader>
+      <OrganizationsCardContent>{description}</OrganizationsCardContent>
+    </OrganizationsCard>
+  );
+};
+
+export default AboutOrganizationsCard;
