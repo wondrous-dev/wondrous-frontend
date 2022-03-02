03f300c6b642ab4c759b16bfe86bdbed80f2c114
Andros <wong22@hotmail.co.uk>
2022-03-02 14:38:20 -0800

    fix: redirect if username already exists

--- wondrous-app/pages/onboarding/welcome/index.tsx
+++ wondrous-app/pages/onboarding/welcome/index.tsx
@@ -1,13 +1,15 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useRouter } from 'next/router';
 import { useMutation } from '@apollo/client';
 
 import { InviteWelcomeBox } from '../../../components/Onboarding/welcome';
 import { MainWrapper } from '../../../components/Onboarding/styles';
 import { UPDATE_USER } from '../../../graphql/mutations';
+import { useMe } from '../../../components/Auth/withAuth';
 
 const ContributorOnboardingPage = () => {
   const router = useRouter();
+  const user = useMe();
   const [updateUser] = useMutation(UPDATE_USER, {
     onCompleted: () => {
       router.push('/onboarding/build-profile', undefined, {
@@ -15,6 +17,13 @@ const ContributorOnboardingPage = () => {
       });
     },
   });
+  useEffect(() => {
+    if (user?.username) {
+      router.push('/dashboard', undefined, {
+        shallow: true,
+      });
+    }
+  }, [user?.username]);
   return (
     <MainWrapper>
       <InviteWelcomeBox updateUser={updateUser} />
