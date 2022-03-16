a62aaad2316f16e25f4d5e77034d8e8bad326b60
Andros <wong22@hotmail.co.uk>
2022-03-16 12:52:36 -0700

    fix: add more instruction

--- wondrous-app/components/DiscordNotificationSetup/index.tsx
+++ wondrous-app/components/DiscordNotificationSetup/index.tsx
@@ -101,8 +101,8 @@ const DiscordNotificationSetup = (props) => {
           fontWeight: 'bolder',
         }}
       >
-        After adding the bot (please make sure the bot is in the channel), navigate to the channel you want you want to
-        send notifications to in Discord and enter the command
+        After adding the bot, navigate to the channel you want to send notifications to in Discord. Please make sure the
+        bot is in the channel and that the bot can embed links. Then enter the command
       </SmallerTopParagraph>
       <SmallerTopParagraph
         style={{
