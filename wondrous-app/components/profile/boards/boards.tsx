6851d72f783881f7cac93a565fa86d8a8bab3d23
juniusfree <juniusfree@gmail.com>
2022-03-29 13:53:12 +0800

    refactor: comment out profile boards

--- wondrous-app/components/profile/boards/boards.tsx
+++ wondrous-app/components/profile/boards/boards.tsx
@@ -17,102 +17,104 @@ import Filter from '../../Common/Filter';
 import { ToggleViewButton } from '../../Common/ToggleViewButton';
 import { delQuery } from '../../../utils';
 
-enum ViewType {
-  List = 'list',
-  Grid = 'grid',
-}
+// NOTE: commenting for now because it's not used and it's causing a build error
 
-const Boards = (props) => {
-  const { selectOptions, columns, tasks, onLoadMore, hasMore } = props;
-  const [selected, setSelected] = useState(0);
-  const buttons = ['List', 'Grid'];
-  const router = useRouter();
-  const [view, setView] = useState(null);
+// enum ViewType {
+//   List = 'list',
+//   Grid = 'grid',
+// }
 
-  useEffect(() => {
-    if (router.isReady) {
-      setView((router.query.view || ViewType.Grid) as ViewType);
-    }
-  }, [router.query.view]);
+// const Boards = (props) => {
+//   const { selectOptions, columns, tasks, onLoadMore, hasMore } = props;
+//   const [selected, setSelected] = useState(0);
+//   const buttons = ['List', 'Grid'];
+//   const router = useRouter();
+//   const [view, setView] = useState(null);
 
-  const [filter, setFilter] = useState([]);
-  const filterSchema = [
-    {
-      name: 'Pods',
-      multiChoice: true,
-      items: [
-        { id: 'designPod', name: 'Design Pod', icon: <CreatePodIcon />, count: 12 },
-        { id: 'growthTeam', name: 'Growth Team', icon: <CreatePodIcon />, count: 4 },
-        { id: 'analytics', name: 'Analytics', icon: <CreatePodIcon />, count: 1 },
-        { id: 'dataPod', name: 'Data Pod', icon: <CreatePodIcon />, count: 0 },
-        { id: 'prDreamTeam', name: 'PR Dream Team', icon: <CreatePodIcon />, count: 2 },
-        { id: 'twitterPod', name: 'Twitter Pod', icon: <CreatePodIcon />, count: 10 },
-        { id: 'blogPod', name: 'Blog Pod', icon: <CreatePodIcon />, count: 2 },
-      ],
-    },
-    {
-      name: 'Status',
-      multiChoice: false,
-      items: [
-        { id: 'membershipRequests', name: 'Membership requests', icon: <Done />, count: 4 },
-        { id: 'proposals', name: 'Proposals', icon: <Done />, count: 22 },
-        { id: 'toDo', name: 'To-Do', icon: <ToDo />, count: 8 },
-        { id: 'inProgress', name: 'In Progress', icon: <InProgress />, count: 232 },
-        { id: 'inReview', name: 'In Review', icon: <Done />, count: 324 },
-        { id: 'awaitingPayment', name: 'Awaiting Payment', icon: <Done />, count: 340 },
-        { id: 'completed', name: 'Completed', icon: <Done />, count: 1120 },
-      ],
-    },
-  ];
+//   useEffect(() => {
+//     if (router.isReady) {
+//       setView((router.query.view || ViewType.Grid) as ViewType);
+//     }
+//   }, [router.query.view]);
 
-  const listViewOptions = [
-    {
-      name: 'List',
-      active: view === ViewType.List,
-      action: () => {
-        router.replace(`${delQuery(router.asPath)}?view=${ViewType.List}`);
-      },
-    },
-    {
-      name: 'Grid',
-      active: view === ViewType.Grid,
-      action: () => {
-        router.replace(`${delQuery(router.asPath)}?view=${ViewType.Grid}`);
-      },
-    },
-  ];
+//   const [filter, setFilter] = useState([]);
+//   const filterSchema = [
+//     {
+//       name: 'Pods',
+//       multiChoice: true,
+//       items: [
+//         { id: 'designPod', name: 'Design Pod', icon: <CreatePodIcon />, count: 12 },
+//         { id: 'growthTeam', name: 'Growth Team', icon: <CreatePodIcon />, count: 4 },
+//         { id: 'analytics', name: 'Analytics', icon: <CreatePodIcon />, count: 1 },
+//         { id: 'dataPod', name: 'Data Pod', icon: <CreatePodIcon />, count: 0 },
+//         { id: 'prDreamTeam', name: 'PR Dream Team', icon: <CreatePodIcon />, count: 2 },
+//         { id: 'twitterPod', name: 'Twitter Pod', icon: <CreatePodIcon />, count: 10 },
+//         { id: 'blogPod', name: 'Blog Pod', icon: <CreatePodIcon />, count: 2 },
+//       ],
+//     },
+//     {
+//       name: 'Status',
+//       multiChoice: false,
+//       items: [
+//         { id: 'membershipRequests', name: 'Membership requests', icon: <Done />, count: 4 },
+//         { id: 'proposals', name: 'Proposals', icon: <Done />, count: 22 },
+//         { id: 'toDo', name: 'To-Do', icon: <ToDo />, count: 8 },
+//         { id: 'inProgress', name: 'In Progress', icon: <InProgress />, count: 232 },
+//         { id: 'inReview', name: 'In Review', icon: <Done />, count: 324 },
+//         { id: 'awaitingPayment', name: 'Awaiting Payment', icon: <Done />, count: 340 },
+//         { id: 'completed', name: 'Completed', icon: <Done />, count: 1120 },
+//       ],
+//     },
+//   ];
 
-  return (
-    <Wrapper>
-      <BoardsContainer>
-        <BoardsActivity>
-          <BoardsActivityInput
-            style={{ visibility: 'hidden' }}
-            placeholder="Search people or pods..."
-            InputProps={{
-              startAdornment: (
-                <InputAdornment position="start">
-                  <SearchIcon />
-                </InputAdornment>
-              ),
-            }}
-          />
-          {/*<Filter style={{ visibility: 'hidden' }} filterSchema={filterSchema} filter={filter} setFilter={setFilter} />*/}
-          {view ? <ToggleViewButton options={listViewOptions} /> : null}
-        </BoardsActivity>
+//   const listViewOptions = [
+//     {
+//       name: 'List',
+//       active: view === ViewType.List,
+//       action: () => {
+//         router.replace(`${delQuery(router.asPath)}?view=${ViewType.List}`);
+//       },
+//     },
+//     {
+//       name: 'Grid',
+//       active: view === ViewType.Grid,
+//       action: () => {
+//         router.replace(`${delQuery(router.asPath)}?view=${ViewType.Grid}`);
+//       },
+//     },
+//   ];
 
-        {view ? (
-          <>
-            {view === ViewType.Grid ? (
-              <KanbanBoard columns={columns} onLoadMore={onLoadMore} hasMore={hasMore} />
-            ) : (
-              <Table columns={columns} onLoadMore={onLoadMore} hasMore={hasMore} />
-            )}
-          </>
-        ) : null}
-      </BoardsContainer>
-    </Wrapper>
-  );
-};
+//   return (
+//     <Wrapper>
+//       <BoardsContainer>
+//         <BoardsActivity>
+//           <BoardsActivityInput
+//             style={{ visibility: 'hidden' }}
+//             placeholder="Search people or pods..."
+//             InputProps={{
+//               startAdornment: (
+//                 <InputAdornment position="start">
+//                   <SearchIcon />
+//                 </InputAdornment>
+//               ),
+//             }}
+//           />
+//           {/*<Filter style={{ visibility: 'hidden' }} filterSchema={filterSchema} filter={filter} setFilter={setFilter} />*/}
+//           {view ? <ToggleViewButton options={listViewOptions} /> : null}
+//         </BoardsActivity>
+
+//         {view ? (
+//           <>
+//             {view === ViewType.Grid ? (
+//               <KanbanBoard columns={columns} onLoadMore={onLoadMore} hasMore={hasMore} />
+//             ) : (
+//               <Table columns={columns} onLoadMore={onLoadMore} hasMore={hasMore} />
+//             )}
+//           </>
+//         ) : null}
+//       </BoardsContainer>
+//     </Wrapper>
+//   );
+// };
 
 export default Boards;
