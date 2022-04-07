e5001937906309c6b7488bfda860dd0a614eacc2
juniusfree <juniusfree@gmail.com>
2022-04-07 09:50:19 +0800

    feat: added token gating to settings sidebar

---
+++ wondrous-app/pages/organization/settings/[orgId]/token-gating.tsx
@@ -0,0 +1,7 @@
+import { SettingsWrapper } from 'components/Settings/settingsWrapper';
+
+const TokenGatingPage = () => {
+  return <SettingsWrapper></SettingsWrapper>;
+};
+
+export default TokenGatingPage;
