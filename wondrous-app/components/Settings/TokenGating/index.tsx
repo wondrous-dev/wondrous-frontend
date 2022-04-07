f11f9da52f41d0e7058adee05f34249558014920
juniusfree <juniusfree@gmail.com>
2022-04-07 21:50:04 +0800

    feat: add virtualization to Token selection

--- wondrous-app/components/Settings/TokenGating/index.tsx
+++ wondrous-app/components/Settings/TokenGating/index.tsx
@@ -1,6 +1,7 @@
 import Ethereum from 'components/Icons/ethereum';
 import PolygonIcon from 'components/Icons/polygonMaticLogo.svg';
-import { useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import { FixedSizeList, ListChildComponentProps } from 'react-window';
 import { SettingsWrapper } from '../settingsWrapper';
 import {
   TokenGatingAutocomplete,
@@ -29,7 +30,7 @@ import {
   TokenGatingWrapper,
 } from './styles';
 
-const options = [
+const chainOptions = [
   {
     label: 'Ethereum',
     icon: <Ethereum />,
@@ -42,7 +43,61 @@ const options = [
   },
 ];
 
+const renderRow = (props: ListChildComponentProps) => {
+  const { data, index, style } = props;
+  const dataSet = data[index];
+  console.log(props);
+  return (
+    <TokenGatingAutocompleteListItem {...props} {...dataSet[0]} value={dataSet[1].value} style={style}>
+      <TokenGatingInputImage src={dataSet[1]?.icon} />
+      {dataSet[1].label}
+    </TokenGatingAutocompleteListItem>
+  );
+};
+
+const OuterElementContext = React.createContext({});
+
+const OuterElementType = React.forwardRef<HTMLDivElement>(function OuterElementType(props, ref) {
+  const outerProps = React.useContext(OuterElementContext);
+  return <TokenGatingAutocompleteList ref={ref} {...props} {...outerProps} />;
+});
+
+const ListboxComponent = React.forwardRef<HTMLDivElement, React.HTMLAttributes<HTMLElement>>(function ListboxComponent(
+  props,
+  ref
+) {
+  const { children, ...other } = props;
+  const itemData: React.ReactChild[] = (children as React.ReactChild[]).flatMap(
+    (item: React.ReactChild & { children?: React.ReactChild[] }) => [item, ...(item.children || [])]
+  );
+  const itemCount = itemData.length;
+  const itemSize = 50;
+  const maxNoOfItemsToDisplay = 5;
+  const height = itemCount > maxNoOfItemsToDisplay ? itemSize * maxNoOfItemsToDisplay : itemCount * itemSize;
+  return (
+    <div ref={ref}>
+      <OuterElementContext.Provider value={other}>
+        <FixedSizeList
+          itemData={itemData}
+          height={height}
+          width="100%"
+          outerElementType={OuterElementType}
+          innerElementType="ul"
+          itemSize={itemSize}
+          overscanCount={20}
+          itemCount={itemCount}
+        >
+          {renderRow}
+        </FixedSizeList>
+      </OuterElementContext.Provider>
+    </div>
+  );
+});
+
 const TokenGatingSettings = () => {
+  const [chain, setChain] = useState(chainOptions[0]);
+  const [tokenList, setTokenList] = useState(null);
+  const [selectedToken, setSelectedToken] = useState(null);
   const [minAmount, setMinAmount] = useState(0);
   const handleMinAmountOnChange = (event) => {
     const { value } = event.target;
@@ -52,11 +107,6 @@ const TokenGatingSettings = () => {
   const handleMinAmountOnClick = (change) => {
     setMinAmount(Number(minAmount + change));
   };
-
-  const [chain, setChain] = useState(options[0]);
-
-  const [tokenList, setTokenList] = useState(null);
-  const [selectedToken, setSelectedToken] = useState(null);
 
   useEffect(() => {
     const getTokenList = async () => {
@@ -77,9 +127,9 @@ const TokenGatingSettings = () => {
       setSelectedToken(formatted[0]);
     };
     getTokenList();
-  }, [setTokenList]);
+  }, []);
 
-  console.log(tokenList);
+  console.log(ListboxComponent);
 
   return (
     <SettingsWrapper>
@@ -97,7 +147,7 @@ const TokenGatingSettings = () => {
           <TokenGatingAutocompleteLabel>Chain</TokenGatingAutocompleteLabel>
           <TokenGatingAutocomplete
             disablePortal
-            options={options}
+            options={chainOptions}
             value={chain}
             onChange={(event, newValue) => setChain(newValue)}
             renderInput={(params) => {
@@ -144,14 +194,10 @@ const TokenGatingSettings = () => {
                     />
                   </TokenGatingAutocompleteTextfieldWrapper>
                 )}
-                renderOption={(props, option) => (
-                  <TokenGatingAutocompleteListItem value={option.value} {...props}>
-                    <TokenGatingInputImage src={option?.icon} />
-                    {option.label}
-                  </TokenGatingAutocompleteListItem>
-                )}
-                ListboxComponent={TokenGatingAutocompleteList}
+                ListboxComponent={ListboxComponent}
                 PopperComponent={TokenGatingAutocompletePopper}
+                renderOption={(props, option) => [props, option]}
+                renderGroup={(params) => params}
               />
             </TokenGatingInputWrapper>
             <TokenGatingInputWrapper>
