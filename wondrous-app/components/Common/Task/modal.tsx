da3c3cada0390053ad821ba444f460c91e344ff7
juniusfree <juniusfree@gmail.com>
2022-04-10 12:07:43 +0800

    feat: After archiving, close the task modal

--- wondrous-app/components/Common/Task/modal.tsx
+++ wondrous-app/components/Common/Task/modal.tsx
@@ -773,7 +773,6 @@ export const TaskViewModal = (props: ITaskListModalProps) => {
 
   const [getTaskById] = useLazyQuery(GET_TASK_BY_ID, {
     fetchPolicy: 'network-only',
-    nextFetchPolicy: 'cache-and-network',
     onCompleted: (data) => {
       const taskData = data?.getTaskById;
       if (taskData) {
@@ -857,28 +856,31 @@ export const TaskViewModal = (props: ITaskListModalProps) => {
   );
 
   useEffect(() => {
-    if (!initialStatus) {
-      setInitialStatus(fetchedTask?.status);
-    }
-
-    if (
-      updateTaskStatusMutationData?.updateTaskStatus.status === TASK_STATUS_ARCHIVED ||
-      updateBountyStatusData?.updateBountyStatus.status === TASK_STATUS_ARCHIVED
-    ) {
-      setSnackbarAlertOpen(true);
-      setSnackbarAlertMessage(
-        <>
-          Task archived successfully!{' '}
-          <ArchivedTaskUndo
-            onClick={() => {
-              handleNewStatus(initialStatus);
-              setSnackbarAlertOpen(false);
-            }}
-          >
-            Undo
-          </ArchivedTaskUndo>
-        </>
-      );
+    if (open) {
+      if (initialStatus !== TASK_STATUS_ARCHIVED) {
+        setInitialStatus(fetchedTask?.status);
+      }
+      const archived = [
+        updateTaskStatusMutationData?.updateTaskStatus.status,
+        updateBountyStatusData?.updateBountyStatus.status,
+      ].some((i) => i?.includes(TASK_STATUS_ARCHIVED));
+      if (archived) {
+        handleClose();
+        setSnackbarAlertOpen(true);
+        setSnackbarAlertMessage(
+          <>
+            Task archived successfully!{' '}
+            <ArchivedTaskUndo
+              onClick={() => {
+                handleNewStatus(initialStatus);
+                setSnackbarAlertOpen(false);
+              }}
+            >
+              Undo
+            </ArchivedTaskUndo>
+          </>
+        );
+      }
     }
   }, [
     initialStatus,
@@ -889,7 +891,10 @@ export const TaskViewModal = (props: ITaskListModalProps) => {
     setSnackbarAlertMessage,
     handleNewStatus,
     updateBountyStatusData,
+    handleClose,
+    open,
   ]);
+
   useEffect(() => {
     if (isMilestone) {
       setActiveTab(tabs.tasks);
