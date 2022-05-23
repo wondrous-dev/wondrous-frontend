7be2357198ebd535a8d0c1af2856867b18627434
chao@torii.co <chao@torii.co>
2022-05-23 14:03:04 -0400

    last changes

--- wondrous-app/components/DatepickerContainer/DatepickerContainer.tsx
+++ wondrous-app/components/DatepickerContainer/DatepickerContainer.tsx
@@ -1,43 +1,18 @@
-import { useState } from 'react';
-
 import Box from '@mui/material/Box';
-import { DesktopDatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { LocalizationProvider } from '@mui/x-date-pickers';
 import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
-import TextField from '@mui/material/TextField';
 
-import DateRangePicker from 'components/DateRangePicker';
 import SingleDatePicker from 'components/SingleDatePicker';
 
 import styles from './DatepickerContainerStyles';
 
-const DatepickerContainer = ({}) => {
-  const [value, setValue] = useState(null);
+const DatepickerContainer = ({ }) => (
+  <LocalizationProvider dateAdapter={AdapterDayjs}>
+    <Box sx={styles.container}>
+      <SingleDatePicker sx={styles.datepicker} />
+    </Box>
+  </LocalizationProvider>
+);
 
-  const handleChange = (newValue) => {
-    setValue(newValue);
-  };
-
-  return (
-    <LocalizationProvider dateAdapter={AdapterDayjs}>
-      <Box sx={styles.container}>
-        <Box>
-          <Box>
-            <Box>DISCARDED RANGE OPTION IS INCLUDED IN THE PAID PLAN</Box>
-            <DesktopDatePicker
-              label="Date desktop"
-              inputFormat="MM/dd/yyyy"
-              value={value}
-              onChange={handleChange}
-              renderInput={(params) => <TextField {...params} />}
-            />
-          </Box>
-          <DateRangePicker sx={styles.datepicker} />
-          <Box mt={4} />
-          <SingleDatePicker sx={styles.datepicker} />
-        </Box>
-      </Box>
-    </LocalizationProvider>
-  );
-};
 
 export default DatepickerContainer;
