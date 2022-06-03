d000c8f305108d3a2828e8de341754facf3191ed
juniusfree <juniusfree@gmail.com>
2022-06-03 18:43:53 +0800

    refactor: re-activate the new modal

--- wondrous-app/components/CreateEntity/index.tsx
+++ wondrous-app/components/CreateEntity/index.tsx
@@ -14,13 +14,9 @@ const CreateFormModal = (props) => {
     if (entityType === '') {
       return <ChooseEntityToCreateModal {...props} />;
     }
-    // if (
-    //   entityType === ENTITIES_TYPES.TASK ||
-    //   entityType === ENTITIES_TYPES.MILESTONE ||
-    //   entityType === ENTITIES_TYPES.BOUNTY
-    // ) {
-    //   return <CreateEntityModal {...props} />;
-    // }
+    if ([ENTITIES_TYPES.TASK, ENTITIES_TYPES.MILESTONE, ENTITIES_TYPES.BOUNTY].includes(entityType)) {
+      return <CreateEntityModal {...props} />;
+    }
     return <CreateLayoutBaseModal {...props} />;
   };
 
