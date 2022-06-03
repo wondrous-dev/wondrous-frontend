517aef8f009d3c797cd48c4e2f92d08fa91f7c45
Andros <wong22@hotmail.co.uk>
2022-06-03 12:36:28 -0500

    feat: add github link

--- wondrous-app/components/Settings/Github/index.tsx
+++ wondrous-app/components/Settings/Github/index.tsx
@@ -2,8 +2,23 @@ import { HeaderBlock } from '../headerBlock';
 import { SettingsWrapper } from '../settingsWrapper';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { White } from 'theme/colors';
+import { GithubButton, GithubButtonDiv } from './styles';
+import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
+const GITHUB_BASE_URL = `https://github.com/apps/wonderverse-integration/installations/new`;
+const GITHUB_APP_CLIENT_ID = 'Iv1.64f7faecf13dacf2';
+const GITHUB_REDIRECT_URI = 'http://localhost:3000/github/callback';
 export const GithubIntegration = ({ orgId }) => {
+  const router = useRouter();
+  console.log('router', router);
+  const [githubUrl, setGithubUrl] = useState(null);
+  useEffect(() => {
+    if (typeof window !== 'undefined') {
+      setGithubUrl(`${GITHUB_BASE_URL}?state=${window.location.href}`);
+    }
+  }, []);
+
   return (
     <SettingsWrapper>
       <HeaderBlock
@@ -20,6 +35,16 @@ export const GithubIntegration = ({ orgId }) => {
         title="Github Integration"
         description="Manage Github Integration"
       />
+      <GithubButtonDiv>
+        <GithubButton href={githubUrl}>
+          <GitHubIcon
+            style={{
+              marginRight: '8px',
+            }}
+          />
+          <span>Connect Github Organization</span>
+        </GithubButton>
+      </GithubButtonDiv>
     </SettingsWrapper>
   );
 };
---
+++ wondrous-app/components/Settings/Github/styles.tsx
@@ -0,0 +1,19 @@
+import styled from 'styled-components';
+import { White } from 'theme/colors';
+
+export const GithubButtonDiv = styled.div`
+  margin-top: 32px;
+  margin-left: 48px;
+`;
+export const GithubButton = styled.a`
+  background: #f5f8fa;
+  padding: 8px 12px;
+  text-decoration: none !important;
+  color: black !important;
+  border-radius: 8px;
+  font-weight: bolder;
+  display: flex;
+  width: fit-content;
+  align-items: center;
+  cursor: pointer;
+`;
