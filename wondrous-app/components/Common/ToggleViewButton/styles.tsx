b2b088790e4a12918200a5ab26b4aa54824c1748
terryli0095 <terryli0095@gmail.com>
2022-06-06 12:45:48 -0500

    fix color

--- wondrous-app/components/Common/ToggleViewButton/styles.tsx
+++ wondrous-app/components/Common/ToggleViewButton/styles.tsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { Grey250, White } from '../../../theme/colors';
 
 export const ToggleViewWrapper = styled.div`
   display: flex;
@@ -16,6 +17,7 @@ export const ToggleViewWrapper = styled.div`
 export const ToggleViewOption = styled.div`
   height: 100%;
   display: flex;
+  color: ${White};
   justify-content: center;
   align-items: center;
   &.active {
