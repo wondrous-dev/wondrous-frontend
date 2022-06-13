c115ad55859fe8d88c34a7d149088453f70e2abe
Adrian Mustea <mustea.adrian95@gmail.com>
2022-06-13 17:00:47 +0300

    Added reactour guide

--- wondrous-app/pages/explore/index.tsx
+++ wondrous-app/pages/explore/index.tsx
@@ -1,8 +1,15 @@
 import { withAuth } from 'components/Auth/withAuth';
-import React, { useState } from 'react';
+import React, { useLayoutEffect } from 'react';
 import ExploreComponent from '../../components/Explore';
-
-const Explore = () => {
+import { useTour } from '@reactour/tour';
+const Explore = ({ user }) => {
+  const { setIsOpen, setCurrentStep } = useTour();
+  useLayoutEffect(() => {
+    if (user && !user?.lastCompletedGuide) {
+      setCurrentStep(0);
+      setIsOpen(true);
+    }
+  }, [user]);
   return (
     <>
       <ExploreComponent />
