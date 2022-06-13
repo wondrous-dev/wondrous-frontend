402b72af8a1afa1670a66ce21955febb278cc9d9
juniusfree <juniusfree@gmail.com>
2022-06-13 16:53:46 +0800

    refactor(WIP): use Task component

--- wondrous-app/components/ProfileUserTaskDaos/ProfileUserTaskDaos.tsx
+++ wondrous-app/components/ProfileUserTaskDaos/ProfileUserTaskDaos.tsx
@@ -6,6 +6,12 @@ import TaskCompletedCard from 'components/TaskCompletedCard';
 
 import { ProfileUserTaskDaosContainer, ProfileUserTaskDaosTitle, ProfileUserTaskDaosWrapper } from './styles';
 import WorkingOnCard from 'components/WorkingOnCard';
+import { Task } from 'components/Common/Task';
+
+const ProfileUserTaskCard = (props) => {
+  const { item } = props;
+  return <Task task={item} />;
+};
 
 const ProfileUserTaskDaos = ({ userProfile }) => {
   const { id: userId } = userProfile;
@@ -21,12 +27,12 @@ const ProfileUserTaskDaos = ({ userProfile }) => {
 
       <ProfileUserTaskDaosContainer>
         <ProfileUserTaskDaosTitle>{completedTaskCount} tasks completed</ProfileUserTaskDaosTitle>
-        <ProfileContentGrid data={completedTasksData} Component={TaskCompletedCard} />
+        <ProfileContentGrid data={completedTasksData} Component={ProfileUserTaskCard} />
       </ProfileUserTaskDaosContainer>
 
       <ProfileUserTaskDaosContainer>
         <ProfileUserTaskDaosTitle>{workingTasksData?.length} Currently working on</ProfileUserTaskDaosTitle>
-        <ProfileContentGrid data={workingTasksData} Component={WorkingOnCard} />
+        <ProfileContentGrid data={workingTasksData} Component={ProfileUserTaskCard} />
       </ProfileUserTaskDaosContainer>
     </ProfileUserTaskDaosWrapper>
   );
