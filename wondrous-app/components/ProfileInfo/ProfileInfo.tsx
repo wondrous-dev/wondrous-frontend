c0f25478de21417b85fcbb7632f48026362b6c49
Andros <wong22@hotmail.co.uk>
2022-07-04 11:36:53 -0400

    fix: remove log

--- wondrous-app/components/ProfileInfo/ProfileInfo.tsx
+++ wondrous-app/components/ProfileInfo/ProfileInfo.tsx
@@ -49,7 +49,6 @@ const ProfileInfo = ({ userProfile }) => {
   const { mainLink, social, websites } = parseLinks(links);
   const [createUserInterest] = useMutation(CREATE_USER_INTEREST, { refetchQueries: [GET_USER_INTERESTS] });
   const viewingSelf = user?.username === username;
-  console.log('mainLink', mainLink, links);
   return (
     <ProfileInfoWrapper>
       <UserInterestModal
