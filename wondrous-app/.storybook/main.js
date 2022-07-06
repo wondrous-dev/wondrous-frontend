8ed751dfeb3445671c130f48b277767938f83332
Valentyn Yefimov <valentine.efimov@gmail.com>
2022-07-06 12:27:03 +0300

    feat: remove debug code

--- wondrous-app/.storybook/main.js
+++ wondrous-app/.storybook/main.js
@@ -19,16 +19,6 @@ module.exports = {
 
     config.resolve.alias['next/image'] = require.resolve('./NextImage.js');
 
-    // config.plugins.push(new webpack.DefinePlugin({
-    //   'process.env.__NEXT_IMAGE_OPTS': JSON.stringify({
-    //     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
-    //     imageSizes: [16, 32, 48, 64, 96, 128, 256, 384],
-    //     domains: [],
-    //     path: '/',
-    //     loader: 'default',
-    //   }),
-    // }));
-
     return config;
   },
 };
