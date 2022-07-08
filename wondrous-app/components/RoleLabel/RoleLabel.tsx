1103377fef4d9c94586134807dcb8d6266a6911e
chao@torii.co <chao@torii.co>
2022-07-08 17:38:52 -0400

    Implement Role Label UI

--- wondrous-app/components/RoleLabel/RoleLabel.tsx
+++ wondrous-app/components/RoleLabel/RoleLabel.tsx
@@ -9,15 +9,13 @@ interface RoleLabelProps {
   role: string;
 }
 
-const RoleLabel = ({ onClick, role }: RoleLabelProps) => {
-  return (
-    <Box sx={styles.root}>
-      <Typography sx={styles.whiteText}>Your role:</Typography>
-      <Button onClick={onClick} sx={styles.role}>
-        {role}
-      </Button>
-    </Box>
-  );
-};
+const RoleLabel = ({ onClick, role }: RoleLabelProps) => (
+  <Box sx={styles.root}>
+    <Typography sx={styles.whiteText}>Your role:</Typography>
+    <Button onClick={onClick} sx={styles.role}>
+      {role}
+    </Button>
+  </Box>
+);
 
 export default RoleLabel;
