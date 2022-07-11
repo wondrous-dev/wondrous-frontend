2cb0c196a29d5f0bebd06d304e9cd25e6084103d
Valentyn Yefimov <valentine.efimov@gmail.com>
2022-07-12 01:09:26 +0300

    feat: cache google gcs image

--- wondrous-app/components/Common/Image/index.tsx
+++ wondrous-app/components/Common/Image/index.tsx
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { useLazyQuery } from '@apollo/client';
+import { parseISO, addSeconds } from 'date-fns';
 import { GET_PREVIEW_FILE } from 'graphql/queries/media';
 
 interface SafeImageArgs {
@@ -13,20 +14,44 @@ export const SafeImage = (safeImageArgs: SafeImageArgs) => {
   const { src, style, defaultImage, setImage, className } = safeImageArgs;
   const [getPreviewFile, { data, loading, error }] = useLazyQuery(GET_PREVIEW_FILE, {
     fetchPolicy: 'network-only',
+    onCompleted: (data) => {
+      localStorage.setItem(`safeImage.${src}`, data?.getPreviewFile?.url);
+    },
   });
 
-  const imgUrl = data?.getPreviewFile?.url;
+  let imgUrl = data?.getPreviewFile?.url;
   useEffect(() => {
     if (src && !src.startsWith('http')) {
-      getPreviewFile({
-        variables: {
-          path: src,
-        },
-      });
+      try {
+        const cachedPreviewUrl = localStorage.getItem(`safeImage.${src}`);
+
+        if (cachedPreviewUrl) {
+          // parse query params to get
+          // X-Goog-Date: The date and time the signed URL became usable, in the ISO 8601 basic format YYYYMMDD'T'HHMMSS'Z'.
+          // X-Goog-Expires: The length of time the signed URL remained valid, measured in seconds from the value in X-Goog-Date.
+          const [, signedISODate] = cachedPreviewUrl.match(/X-Goog-Date=([^&]*)/);
+          const [, expiresTime] = cachedPreviewUrl.match(/X-Goog-Expires=([^&]*)/);
+          const signedDate = parseISO(signedISODate).valueOf();
+          const expiredAt = addSeconds(signedDate, Number(expiresTime)).valueOf();
+
+          if (Date.now() < expiredAt) {
+            imgUrl = cachedPreviewUrl;
+          }
+        }
+      } catch (error) {}
+
+      if (!imgUrl) {
+        getPreviewFile({
+          variables: {
+            path: src,
+          },
+        });
+      }
     }
+
     if (imgUrl) {
       if (setImage && !(src?.startsWith('https') || src?.startsWith('file://'))) {
-        setImage(data.getPreviewFile.url);
+        setImage(imgUrl);
       }
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
