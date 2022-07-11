4b352f363d9a850112b8d835885886f2f6d58a6c
juniusfree <juniusfree@gmail.com>
2022-07-11 14:23:59 +0800

    fix: :bug: create/edit task modal is outside the view

--- wondrous-app/components/CreateEntity/styles.tsx
+++ wondrous-app/components/CreateEntity/styles.tsx
@@ -46,7 +46,6 @@ export const CreateFormModalOverlay = styled(Modal)`
   z-index: 1500;
   padding-top: 0;
   height: 100vh;
-  justify-content: center;
 `;
 
 export const CreateLayoutsModal = styled(BaseCard)`
