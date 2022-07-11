3d60640659c611b7e0bff233e22824899dc9b144
Valentyn Yefimov <valentine.efimov@gmail.com>
2022-07-12 01:40:00 +0300

    feat: cache google gcs image

--- wondrous-app/components/Common/Image/index.tsx
+++ wondrous-app/components/Common/Image/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useLazyQuery } from '@apollo/client';
 import { parseISO, addSeconds } from 'date-fns';
 import { GET_PREVIEW_FILE } from 'graphql/queries/media';
@@ -7,69 +7,76 @@ interface SafeImageArgs {
   src: string;
   style?: object;
   defaultImage?: string;
-  setImage?(url: string): void;
+  onPreviewLoaded?(url: string): void;
   className?: string;
+  alt?: string;
 }
 export const SafeImage = (safeImageArgs: SafeImageArgs) => {
-  const { src, style, defaultImage, setImage, className } = safeImageArgs;
-  const [getPreviewFile, { data, loading, error }] = useLazyQuery(GET_PREVIEW_FILE, {
+  const { src, style, defaultImage, className, alt = '', onPreviewLoaded } = safeImageArgs;
+  const [imgUrl, setImageUrl] = useState(null);
+  const [getPreviewFile] = useLazyQuery(GET_PREVIEW_FILE, {
     fetchPolicy: 'network-only',
-    onCompleted: (data) => {
-      localStorage.setItem(`safeImage.${src}`, data?.getPreviewFile?.url);
+    onCompleted: ({ getPreviewFile: { url } }) => {
+      setImageUrl(url);
+      localStorage.setItem(`safeImage.${src}`, url);
+
+      if (onPreviewLoaded) {
+        onPreviewLoaded(url);
+      }
     },
   });
 
-  let imgUrl = data?.getPreviewFile?.url;
   useEffect(() => {
-    if (src && !src.startsWith('http')) {
-      try {
-        const cachedPreviewUrl = localStorage.getItem(`safeImage.${src}`);
+    if (!src || src.startsWith('http') || imgUrl) {
+      return;
+    }
 
-        if (cachedPreviewUrl) {
-          // parse query params to get
-          // X-Goog-Date: The date and time the signed URL became usable, in the ISO 8601 basic format YYYYMMDD'T'HHMMSS'Z'.
-          // X-Goog-Expires: The length of time the signed URL remained valid, measured in seconds from the value in X-Goog-Date.
-          const [, signedISODate] = cachedPreviewUrl.match(/X-Goog-Date=([^&]*)/);
-          const [, expiresTime] = cachedPreviewUrl.match(/X-Goog-Expires=([^&]*)/);
-          const signedDate = parseISO(signedISODate).valueOf();
-          const expiredAt = addSeconds(signedDate, Number(expiresTime)).valueOf();
+    console.log(src, '--------');
 
-          if (Date.now() < expiredAt) {
-            imgUrl = cachedPreviewUrl;
+    try {
+      const cachedPreviewUrl = localStorage.getItem(`safeImage.${src}`);
+
+      if (cachedPreviewUrl) {
+        // parse query params to get
+        // X-Goog-Date: The date and time the signed URL became usable, in the ISO 8601 basic format YYYYMMDD'T'HHMMSS'Z'.
+        // X-Goog-Expires: The length of time the signed URL remained valid, measured in seconds from the value in X-Goog-Date.
+        const [, signedISODate] = cachedPreviewUrl.match(/X-Goog-Date=([^&]*)/);
+        const [, expiresTime] = cachedPreviewUrl.match(/X-Goog-Expires=([^&]*)/);
+        const signedDate = parseISO(signedISODate).valueOf();
+        const expiredAt = addSeconds(signedDate, Number(expiresTime)).valueOf();
+
+        if (Date.now() < expiredAt) {
+          setImageUrl(cachedPreviewUrl);
+
+          if (onPreviewLoaded) {
+            onPreviewLoaded(cachedPreviewUrl);
           }
+          return;
         }
-      } catch (error) {}
-
-      if (!imgUrl) {
-        getPreviewFile({
-          variables: {
-            path: src,
-          },
-        });
       }
-    }
+    } catch (error) {}
 
-    if (imgUrl) {
-      if (setImage && !(src?.startsWith('https') || src?.startsWith('file://'))) {
-        setImage(imgUrl);
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [imgUrl, src, setImage]);
+    getPreviewFile({
+      variables: {
+        path: src,
+      },
+    });
+  }, [imgUrl, src, onPreviewLoaded]);
 
-  if (!src && defaultImage) {
+  if (!imgUrl && defaultImage) {
     // eslint-disable-next-line @next/next/no-img-element
-    return <img style={style} className={className} src={defaultImage} alt="" />;
+    return <img style={style} className={className} src={defaultImage} alt={alt} />;
   }
 
   if (src?.startsWith('https') || src?.startsWith('file://')) {
     // eslint-disable-next-line @next/next/no-img-element
-    return <img style={style} className={className} key={src} src={src} alt="" />;
+    return <img style={style} className={className} key={src} src={src} alt={alt} />;
   } else if (imgUrl) {
     return (
       // eslint-disable-next-line @next/next/no-img-element
-      <img style={style} className={className} key={src} src={imgUrl} alt="" />
+      <img style={style} className={className} key={src} src={imgUrl} alt={alt} />
     );
   }
+
   return null;
 };
