1137766c09e63ab0c482207068a651401fc0164b
juniusfree <juniusfree@gmail.com>
2022-07-11 16:01:39 +0800

    fix: subtasks data fetching

--- wondrous-app/components/Common/TaskSubtask/TaskSubtaskList/TaskSubtaskList.tsx
+++ wondrous-app/components/Common/TaskSubtask/TaskSubtaskList/TaskSubtaskList.tsx
@@ -1,55 +1,47 @@
 import { useQuery } from '@apollo/client';
+import { GET_SUBTASKS_FOR_TASK } from 'graphql/queries';
 import { useEffect, useState } from 'react';
 import { useInView } from 'react-intersection-observer';
-import { GET_SUBTASKS_FOR_TASK } from 'graphql/queries';
 import { TASK_STATUS_ARCHIVED } from 'utils/constants';
 import { SubtaskTaskItem, SubtaskTaskListHasMore } from './styles';
 
-export const TaskSubtaskList = ({ taskId }) => {
-  const [ref, inView] = useInView({});
+const LIMIT = 5;
+const useGetSubtasksForTask = ({ taskId, inView }) => {
   const [hasMore, setHasMore] = useState(true);
-  const {
-    data: getSubtasksForTaskData,
-    fetchMore,
-    loading,
-  } = useQuery(GET_SUBTASKS_FOR_TASK, {
+  const { data, fetchMore, loading } = useQuery(GET_SUBTASKS_FOR_TASK, {
+    fetchPolicy: 'cache-and-network',
     variables: {
       taskId,
-      limit: 5,
+      limit: LIMIT,
       offset: 0,
     },
   });
 
   useEffect(() => {
-    if (inView) {
+    if (inView && hasMore && !loading) {
       fetchMore({
         variables: {
-          offset: getSubtasksForTaskData?.getSubtasksForTask.length,
+          offset: data?.getSubtasksForTask.length,
         },
-        updateQuery: (prev: any, { fetchMoreResult }: any) => {
-          if (!fetchMoreResult) {
-            setHasMore(false);
-            return prev;
-          }
-          if (fetchMoreResult?.getSubtasksForTask.length === 0) {
-            setHasMore(false);
-            return prev;
-          }
-          return {
-            getSubtasksForTask: [...prev?.getSubtasksForTask, ...fetchMoreResult?.getSubtasksForTask],
-          };
-        },
-      });
+      }).then(({ data }) => setHasMore(data?.getSubtasksForTask.length >= LIMIT));
     }
-  }, [inView, fetchMore, getSubtasksForTaskData]);
+  }, [inView, fetchMore, data?.getSubtasksForTask, hasMore, loading]);
+  return {
+    data: data?.getSubtasksForTask.filter((i) => i.status !== TASK_STATUS_ARCHIVED),
+    loading,
+    hasMore,
+  };
+};
 
+export const TaskSubtaskList = ({ taskId }) => {
+  const [ref, inView] = useInView({});
+  const { hasMore, data, loading } = useGetSubtasksForTask({ taskId, inView });
+  if (!data) return null;
   return (
     <>
-      {getSubtasksForTaskData?.getSubtasksForTask
-        .filter((i) => i.status !== TASK_STATUS_ARCHIVED)
-        .map((subtask) => (
-          <SubtaskTaskItem key={subtask.id} task={subtask} />
-        ))}
+      {data.map((subtask) => (
+        <SubtaskTaskItem key={subtask.id} task={subtask} />
+      ))}
       {hasMore && !loading && <SubtaskTaskListHasMore ref={ref} />}
     </>
   );
