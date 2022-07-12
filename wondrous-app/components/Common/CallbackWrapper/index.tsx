1dd08e435034d32e40f5fa1b8b0d0a9630251a80
terryli0095 <terryli0095@gmail.com>
2022-07-12 15:23:21 +0200

    restyle discord callbackpage

---
+++ wondrous-app/components/Common/CallbackWrapper/index.tsx
@@ -0,0 +1,33 @@
+import { Typography } from '@mui/material';
+import styled from 'styled-components';
+import palette from 'theme/palette';
+
+export const CallbackBackground = styled.div`
+  background-image: url(/images/404/404-background.webp);
+  background-repeat: no-repeat;
+  background-position: center;
+  background-size: 1000px;
+  width: 100vw;
+  height: 573px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  position: relative;
+  padding-top: 100px;
+`;
+
+export const CallbackWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const CallbackHeading = styled(Typography)`
+  && {
+    font-family: 'Space Grotesk';
+    font-size: 32px;
+    font-weight: 500;
+    color: ${palette.white};
+  }
+`;
