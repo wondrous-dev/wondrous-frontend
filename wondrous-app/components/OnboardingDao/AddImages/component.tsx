f47bc7a9e00809d1fab2e355dd593bccee22288d
juniusfree <juniusfree@gmail.com>
2022-07-18 15:27:02 +0800

    feat: upload logo

--- wondrous-app/components/OnboardingDao/AddImages/component.tsx
+++ wondrous-app/components/OnboardingDao/AddImages/component.tsx
@@ -1,18 +1,35 @@
 import AddAPhotoIcon from 'components/Icons/addAPhoto.svg';
 import { ComponentFieldWrapper, FieldLabel, FieldWrapper } from 'components/OnboardingDao/styles';
-import { AddPhotoIconWrapper, HeaderText, HeaderWrapper, LogoWrapper } from './styles';
+import { ImageUpload } from 'components/Settings/imageUpload';
+import { useField, useFormikContext } from 'formik';
+import { AddPhotoIconWrapper, HeaderText, HeaderWrapper, ImageWrapper, LogoUpload } from './styles';
 
-const AddImages = () => {
+const ProfilePicture = (props) => {
+  const [field] = useField(props.name);
+  const { setFieldValue } = useFormikContext();
+  const withImage = Boolean(field.value);
+  return (
+    <FieldWrapper>
+      <FieldLabel>Logo</FieldLabel>
+      <ImageUpload
+        updateFilesCb={(file) => setFieldValue(props.name, file)}
+        LabelComponent={(props) => (
+          <LogoUpload {...props}>
+            <ImageWrapper withImage={withImage} src={URL?.createObjectURL(field.value)} />
+            <AddPhotoIconWrapper withImage={withImage}>
+              <AddAPhotoIcon />
+            </AddPhotoIconWrapper>
+          </LogoUpload>
+        )}
+      />
+    </FieldWrapper>
+  );
+};
+
+const AddImages = (props) => {
   return (
     <ComponentFieldWrapper>
-      <FieldWrapper>
-        <FieldLabel>Logo</FieldLabel>
-        <LogoWrapper>
-          <AddPhotoIconWrapper>
-            <AddAPhotoIcon />
-          </AddPhotoIconWrapper>
-        </LogoWrapper>
-      </FieldWrapper>
+      <ProfilePicture {...props.fields.profilePicture} />
       <FieldWrapper>
         <FieldLabel>Header</FieldLabel>
         <HeaderWrapper>
