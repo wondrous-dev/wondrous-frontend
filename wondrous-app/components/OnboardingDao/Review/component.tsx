203f6dfda2c5f4d3cc55d0f2e8493cdaa78a5078
juniusfree <juniusfree@gmail.com>
2022-07-18 16:56:37 +0800

    feat: updated some data on Review

--- wondrous-app/components/OnboardingDao/Review/component.tsx
+++ wondrous-app/components/OnboardingDao/Review/component.tsx
@@ -1,3 +1,5 @@
+import { FormikValues, useFormikContext } from 'formik';
+import Image from 'next/image';
 import { ItemWrapper, LabelText, LabelWrapper, Text, Wrapper } from './styles';
 
 const Item = ({ label, children }) => {
@@ -12,19 +14,25 @@ const Item = ({ label, children }) => {
 };
 
 const Review = () => {
+  const { values }: FormikValues = useFormikContext();
+  const { name, description, profilePicture, category } = values;
   return (
     <Wrapper>
       <Item label="DAO Name">
-        <Text>Sample DAO</Text>
+        <Text>{name}</Text>
       </Item>
-      <Item label="DAO Logo">Sample DAO</Item>
+      <Item label="DAO Logo">
+        <Image alt="Profile" width="26px" height="26px" src={URL?.createObjectURL(profilePicture)} />
+      </Item>
       <Item label="DAO username">
         <Text>@sampleDAO</Text>
       </Item>
       <Item label="Description">
-        <Text>SampleDAO empowers anyone to create the best web2/web3s creative samples.</Text>
+        <Text>{description}</Text>
       </Item>
-      <Item label="Goals">Sample DAO</Item>
+      <Item label="Goals">
+        <Text>{category}</Text>
+      </Item>
       <Item label="Task import file">Sample DAO</Item>
       <Item label="Community import file">Sample DAO</Item>
     </Wrapper>
