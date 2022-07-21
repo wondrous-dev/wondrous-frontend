986bb787aadaa896cf49668e154576185d85684a
juniusfree <juniusfree@gmail.com>
2022-07-21 13:49:45 +0800

    feat: edit dao name on review

--- wondrous-app/components/OnboardingDao/Review/component.tsx
+++ wondrous-app/components/OnboardingDao/Review/component.tsx
@@ -1,5 +1,22 @@
-import { FormikValues, useFormikContext } from 'formik';
-import { Category, ChildrenWrapper, ItemWrapper, LabelText, LabelWrapper, Logo, Text, Wrapper } from './styles';
+import { ClickAwayListener } from '@mui/material';
+import EditIcon from 'components/Icons/edit.svg';
+import { FormikValues, useField, useFormikContext } from 'formik';
+import { useState } from 'react';
+import {
+  Category,
+  ChildrenWrapper,
+  DaoNameWrapper,
+  EditButton,
+  EditButtonText,
+  EditInput,
+  EditInputWrapper,
+  ItemWrapper,
+  LabelText,
+  LabelWrapper,
+  Logo,
+  Text,
+  Wrapper,
+} from './styles';
 
 const Item = ({ label, children }) => {
   return (
@@ -12,26 +29,70 @@ const Item = ({ label, children }) => {
   );
 };
 
+const Edit = ({ onClick }) => {
+  return (
+    <EditButton onClick={onClick}>
+      <EditIcon /> <EditButtonText>Edit</EditButtonText>
+    </EditButton>
+  );
+};
+
+const EditDaoName = ({ name, setIsEditing }) => {
+  const [value, setValue] = useState(name);
+  const [_, __, helpers] = useField('name');
+  return (
+    <ClickAwayListener
+      onClickAway={() => {
+        setIsEditing(false);
+        setValue(name);
+      }}
+    >
+      <EditInputWrapper>
+        <EditInput value={value} onChange={(e) => setValue(e.target.value)} />
+        <Edit
+          onClick={() => {
+            setIsEditing(false);
+            helpers.setValue(value);
+          }}
+        />
+      </EditInputWrapper>
+    </ClickAwayListener>
+  );
+};
+
+const DaoName = ({ name }) => {
+  const [isEditing, setIsEditing] = useState(false);
+  return (
+    <Item label="DAO Name">
+      <DaoNameWrapper>
+        {isEditing ? (
+          <EditDaoName name={name} setIsEditing={setIsEditing} />
+        ) : (
+          <Text onClick={() => setIsEditing(true)}>{name}</Text>
+        )}
+      </DaoNameWrapper>
+    </Item>
+  );
+};
+
 const Review = () => {
-  const { values }: FormikValues = useFormikContext();
-  const { name, description, profilePicture, category } = values;
+  const { values } = useFormikContext();
+  const { name, description, profilePicture, category }: FormikValues = values;
   return (
     <Wrapper>
-      <Item label="DAO Name">
-        <Text>{name}</Text>
+      <DaoName name={name} />
+      <Item label="DAO Logo">
+        {profilePicture && <Logo alt="Profile" width="26px" height="26px" src={URL?.createObjectURL(profilePicture)} />}
       </Item>
-      {profilePicture && (
-        <Item label="DAO Logo">
-          <Logo alt="Profile" width="26px" height="26px" src={URL?.createObjectURL(profilePicture)} />
-        </Item>
-      )}
       <Item label="Description">
         <Text>{description}</Text>
       </Item>
       <Item label="Goals">
-        <Category>
-          <Text>{category}</Text>
-        </Category>
+        {category && (
+          <Category>
+            <Text>{category}</Text>
+          </Category>
+        )}
       </Item>
       <Item label="Task import file">Sample DAO</Item>
       <Item label="Community import file">Sample DAO</Item>
--- wondrous-app/components/OnboardingDao/Review/styles.tsx
+++ wondrous-app/components/OnboardingDao/Review/styles.tsx
@@ -1,4 +1,5 @@
-import { Typography } from '@mui/material';
+import { InputUnstyled } from '@mui/base';
+import { ButtonBase, Typography } from '@mui/material';
 import Image from 'next/image';
 import styled from 'styled-components';
 
@@ -36,6 +37,7 @@ export const LabelText = styled.div`
 export const ChildrenWrapper = styled.div`
   display: flex;
   align-items: center;
+  gap: 12px;
 `;
 
 export const Text = styled(Typography)`
@@ -47,6 +49,13 @@ export const Text = styled(Typography)`
     font-size: 14px;
     font-weight: 500;
   }
+`;
+
+export const DaoNameWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+  cursor: pointer;
 `;
 
 export const Logo = styled(Image)`
@@ -63,4 +72,57 @@ export const Category = styled.div`
   background: #141414;
   border-radius: 4px;
   padding: 4px 9px;
+`;
+
+export const EditButton = styled(ButtonBase)`
+  && {
+    align-items: center;
+    background: #282828;
+    border-radius: 4px;
+    gap: 6px;
+    height: 26px;
+    justify-content: center;
+    padding: 10px 7px;
+    display: flex;
+  }
+`;
+
+export const EditButtonText = styled(Typography)`
+  && {
+    align-items: center;
+    color: ${({ theme }) => theme.palette.highlightBlue};
+    display: flex;
+    font-family: 'Space Grotesk';
+    font-size: 13px;
+    font-weight: 500;
+    line-height: 0;
+  }
+`;
+
+export const EditInputWrapper = styled.div`
+  display: flex;
+  align-items: center;
+  gap: 12px;
+`;
+
+export const EditInput = styled(InputUnstyled)`
+  && {
+    width: 70%;
+    height: 26px;
+    .MuiInput-input {
+      background: transparent;
+      border-radius: 4px;
+      border: 1px solid #282828;
+      color: ${({ theme }) => theme.palette.white};
+      font-family: 'Space Grotesk';
+      font-size: 14px;
+      font-weight: 500;
+      height: 100%;
+      width: 100%;
+      padding: 4px 7px;
+      :focus-visible {
+        outline: none;
+      }
+    }
+  }
 `;
