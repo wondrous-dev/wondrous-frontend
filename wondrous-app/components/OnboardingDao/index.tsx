456bc8fb6fb05ddf73887eb584de52bba03d8341
juniusfree <juniusfree@gmail.com>
2022-07-29 13:43:23 +0800

    refactor: move component to index files

--- wondrous-app/components/OnboardingDao/index.tsx
+++ wondrous-app/components/OnboardingDao/index.tsx
@@ -3,5 +3,5 @@ export { default as CreateDao } from './CreateDao';
 export { default as DaoCategory } from './DaoCategory';
 export { default as ImportTasks } from './ImportTasks';
 export { default as InviteCommunity } from './InviteCommunity';
-export { default as StepWrapper } from './StepWrapper';
 export { default as Review } from './Review';
+export { default as StepWrapper } from './StepWrapper';
