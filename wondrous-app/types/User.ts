d529385d27e4eaf226cab2551cc5471ecbbc4a96
Valentyn Yefimov <valentine.efimov@gmail.com>
2022-08-08 01:31:01 +0200

    feat: use memo for header

---
+++ wondrous-app/types/User.ts
@@ -0,0 +1,22 @@
+export interface UserInfo {
+  __typename: 'UserInfo';
+  email: string;
+  discordUsername: string;
+  twitterUsername?: any;
+  promotionTweet?: any;
+}
+
+export interface User {
+  __typename: 'User';
+  id: string;
+  username: string;
+  bio: string;
+  activeEthAddress: string;
+  profilePicture: string;
+  headerPicture?: any;
+  thumbnailPicture: string;
+  userInfo: UserInfo;
+  signupCompleted?: any;
+  lastCompletedGuide: string;
+  links?: any;
+}
