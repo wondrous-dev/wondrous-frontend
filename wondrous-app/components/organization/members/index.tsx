2b3701f74d2bc39304ecd8a6824ad918132c3ecb
SankhaChak <anonproductionspro@gmail.com>
2022-08-12 19:37:42 +0530

    fix: remove console logs and unused functions

--- wondrous-app/components/organization/members/index.tsx
+++ wondrous-app/components/organization/members/index.tsx
@@ -28,29 +28,25 @@ const QUERY_LIMIT = 20;
 const useGetOrgMemberRequests = (orgId) => {
   const [hasMore, setHasMore] = useState(false);
   const [isInitialFetchForThePage, setIsInitialFetchForThePage] = useState(true); // this state is used to determine if the fetch is from a fetchMore or from route change
-  const [getOrgUserMembershipRequests, { data, fetchMore, previousData, refetch }] = useLazyQuery(
-    GET_ORG_MEMBERSHIP_REQUEST,
-    {
-      fetchPolicy: 'cache-and-network',
-      nextFetchPolicy: 'cache-first',
-      // set notifyOnNetworkStatusChange to true if you want to trigger a rerender whenever the request status updates
-      notifyOnNetworkStatusChange: true,
-      onCompleted: ({ getOrgMembershipRequest }) => {
-        const isPreviousDataValid = previousData && previousData?.getOrgMembershipRequest?.length > 1; // if length of previous data is 1, it is likely a refetch;
+  const [getOrgUserMembershipRequests, { data, fetchMore, previousData }] = useLazyQuery(GET_ORG_MEMBERSHIP_REQUEST, {
+    fetchPolicy: 'cache-and-network',
+    nextFetchPolicy: 'cache-first',
+    // set notifyOnNetworkStatusChange to true if you want to trigger a rerender whenever the request status updates
+    notifyOnNetworkStatusChange: true,
+    onCompleted: ({ getOrgMembershipRequest }) => {
+      const isPreviousDataValid = previousData && previousData?.getOrgMembershipRequest?.length > 1; // if length of previous data is 1, it is likely a refetch;
 
-        const limitToRefer = QUERY_LIMIT;
-        const previousDataLength = previousData?.getOrgMembershipRequest?.length;
-        const currentDataLength = getOrgMembershipRequest?.length;
-        const updatedDataLength = isPreviousDataValid ? currentDataLength - previousDataLength : currentDataLength;
-        if (isInitialFetchForThePage) {
-          setHasMore(currentDataLength >= limitToRefer);
-        } else {
-          updatedDataLength >= 0 && setHasMore(updatedDataLength >= limitToRefer); // updatedDataLength >= 0 means it's not a refetch
-        }
-        console.log({ updatedDataLength, previousDataLength, currentDataLength });
-      },
-    }
-  );
+      const limitToRefer = QUERY_LIMIT;
+      const previousDataLength = previousData?.getOrgMembershipRequest?.length;
+      const currentDataLength = getOrgMembershipRequest?.length;
+      const updatedDataLength = isPreviousDataValid ? currentDataLength - previousDataLength : currentDataLength;
+      if (isInitialFetchForThePage) {
+        setHasMore(currentDataLength >= limitToRefer);
+      } else {
+        updatedDataLength >= 0 && setHasMore(updatedDataLength >= limitToRefer); // updatedDataLength >= 0 means it's not a refetch
+      }
+    },
+  });
   useEffect(() => {
     if (orgId) {
       getOrgUserMembershipRequests({
