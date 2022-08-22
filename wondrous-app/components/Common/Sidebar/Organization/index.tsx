a2427ac98660a9305394e38e037ec39e957be76b
juniusfree <juniusfree@gmail.com>
2022-08-22 08:40:19 +0800

    refactor: extract wrappers

--- wondrous-app/components/Common/Sidebar/Organization/index.tsx
+++ wondrous-app/components/Common/Sidebar/Organization/index.tsx
@@ -1,45 +1,19 @@
-import ScrollBarStyles from 'components/Common/ScrollbarStyles';
+import { ChildrenWrapper, SidebarWrapper, Wrapper } from 'components/Common/Sidebar/Common/styles';
 import useSideBar from 'hooks/useSideBar';
-import styled from 'styled-components';
 
 import AboutEntity from './AboutEntity';
 import List from './List';
 
-const Wrapper = styled.div`
-  background: #232323;
-  flex-direction: column;
-  gap: 28px;
-  height: 100%;
-  overflow-y: auto;
-  padding: 24px;
-  position: fixed;
-  width: 260px;
-  display: flex;
-  ${({ minimized }) => minimized && `left: -100%`};
-  ${ScrollBarStyles}
-`;
-
-const DaoSidebarWrapper = styled.div`
-  display: flex;
-  width: 100%;
-  position: relative;
-`;
-
-const ChildrenWrapper = styled.div`
-  margin-left: ${({ minimized }) => (minimized ? '0px' : '280px')};
-  width: 100%;
-`;
-
 const DaoSidebar = ({ children }) => {
   const { minimized } = useSideBar();
   return (
-    <DaoSidebarWrapper>
-      <Wrapper minimized={minimized}>
+    <Wrapper>
+      <SidebarWrapper minimized={minimized}>
         <AboutEntity />
         <List />
-      </Wrapper>
+      </SidebarWrapper>
       <ChildrenWrapper minimized={minimized}>{children}</ChildrenWrapper>
-    </DaoSidebarWrapper>
+    </Wrapper>
   );
 };
 
