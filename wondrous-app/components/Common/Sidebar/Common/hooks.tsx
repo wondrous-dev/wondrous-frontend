d97baac42bdc7ee94c9c29b7b785bd3c627c5bed
juniusfree <juniusfree@gmail.com>
2022-08-25 08:24:48 +0800

    refactor: use useCreateEntityContext for permissions

--- wondrous-app/components/Common/Sidebar/Common/hooks.tsx
+++ wondrous-app/components/Common/Sidebar/Common/hooks.tsx
@@ -1,11 +1,12 @@
 import { PERMISSIONS } from 'utils/constants';
 import { parseUserPermissionContext } from 'utils/helpers';
-import { useBoards } from 'utils/hooks';
+import { useBoards, useCreateEntityContext } from 'utils/hooks';
 
 export const useCanManage = () => {
   const { board } = useBoards();
+  const { userPermissionsContext } = useCreateEntityContext();
   const permissions = parseUserPermissionContext({
-    userPermissionsContext: board?.userPermissionsContext,
+    userPermissionsContext,
     orgId: board?.orgId,
     podId: board?.podId,
   });
@@ -18,8 +19,9 @@ export const useCanManage = () => {
 
 export const useCanEdit = () => {
   const { board } = useBoards();
+  const { userPermissionsContext } = useCreateEntityContext();
   const permissions = parseUserPermissionContext({
-    userPermissionsContext: board?.userPermissionsContext,
+    userPermissionsContext,
     orgId: board?.orgId,
     podId: board?.podId,
   });
