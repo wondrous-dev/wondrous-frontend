a7c3f4d56c27a50767a508e6693bd3a732dc5982
juniusfree <juniusfree@gmail.com>
2022-08-29 18:13:46 +0800

    fix: re-render when navigating workspaces

--- wondrous-app/components/Common/Sidebar/Entity/List.tsx
+++ wondrous-app/components/Common/Sidebar/Entity/List.tsx
@@ -34,100 +34,124 @@ const usePerTypeTaskCountForBoard = () => {
 
 const useSidebarData = () => {
   const { board, orgBoard } = useBoards();
+  const { setEntityType } = board || {};
   const router = useRouter();
+  const handleOnClick =
+    (link, type = null) =>
+    () => {
+      if (type && setEntityType) {
+        setEntityType(type);
+        return;
+      }
+      router.push(link);
+    };
   const link = orgBoard ? `/organization/${board?.orgData?.username}` : `/pod/${board?.podId}`;
   const taskCount = usePerTypeTaskCountForBoard();
-  return [
-    {
-      label: 'Workspaces',
-      items: [
-        {
-          text: 'Tasks',
-          Icon: CheckBoxIcon,
-          link: `${link}/boards?entity=${ENTITIES_TYPES.TASK}`,
-          count: taskCount.taskCount,
-        },
-        {
-          text: 'Bounties',
-          Icon: StartIcon,
-          link: `${link}/boards?entity=${ENTITIES_TYPES.BOUNTY}`,
-          count: taskCount.bountyCount,
-        },
-        {
-          text: 'Milestones',
-          Icon: FlagIcon,
-          link: `${link}/boards?entity=${ENTITIES_TYPES.MILESTONE}`,
-          count: taskCount.milestoneCount,
-        },
-        {
-          text: 'Proposals',
-          Icon: ContentPaste,
-          link: `${link}/boards?entity=${ENTITIES_TYPES.PROPOSAL}`,
-          count: taskCount.proposalCount,
-        },
-        // {
-        //   text: 'Pods',
-        //   Icon: PodIcon,
-        //   link: null, // link: not sure yet
-        // },
-      ],
-    },
-    {
-      label: 'Community',
-      items: [
-        {
-          text: 'Analytics',
-          Icon: PieChartIcon,
-          link: `${link}/analytics`,
-        },
-        {
-          text: 'Activity',
-          Icon: ShowChartIcon,
-          link: `${link}/activities`,
-        },
-        {
-          text: 'Members',
-          Icon: GroupIcon,
-          link: `${link}/members`,
-        },
-        {
-          text: 'Roles',
-          Icon: StackIcon,
-          link: {
-            pathname: router.pathname,
-            query: {
-              ...router.query,
-              roles: true,
+  return {
+    handleOnClick,
+    data: [
+      {
+        label: 'Workspaces',
+        items: [
+          {
+            text: 'Tasks',
+            Icon: CheckBoxIcon,
+            link: `${link}/boards?entity=${ENTITIES_TYPES.TASK}`,
+            count: taskCount.taskCount,
+            entityType: ENTITIES_TYPES.TASK,
+          },
+          {
+            text: 'Bounties',
+            Icon: StartIcon,
+            link: `${link}/boards?entity=${ENTITIES_TYPES.BOUNTY}`,
+            count: taskCount.bountyCount,
+            entityType: ENTITIES_TYPES.BOUNTY,
+          },
+          {
+            text: 'Milestones',
+            Icon: FlagIcon,
+            link: `${link}/boards?entity=${ENTITIES_TYPES.MILESTONE}`,
+            count: taskCount.milestoneCount,
+            entityType: ENTITIES_TYPES.MILESTONE,
+          },
+          {
+            text: 'Proposals',
+            Icon: ContentPaste,
+            link: `${link}/boards?entity=${ENTITIES_TYPES.PROPOSAL}`,
+            count: taskCount.proposalCount,
+            entityType: ENTITIES_TYPES.PROPOSAL,
+          },
+          // {
+          //   text: 'Pods',
+          //   Icon: PodIcon,
+          //   link: null, // link: not sure yet
+          // },
+        ],
+      },
+      {
+        label: 'Community',
+        items: [
+          {
+            text: 'Analytics',
+            Icon: PieChartIcon,
+            link: `${link}/analytics`,
+          },
+          {
+            text: 'Activity',
+            Icon: ShowChartIcon,
+            link: `${link}/activities`,
+          },
+          {
+            text: 'Members',
+            Icon: GroupIcon,
+            link: `${link}/members`,
+          },
+          {
+            text: 'Roles',
+            Icon: StackIcon,
+            link: {
+              pathname: router.pathname,
+              query: {
+                ...router.query,
+                roles: true,
+              },
             },
           },
-        },
 
-        {
-          text: 'Resources',
-          Icon: FolderIcon,
-          link: `${link}/docs`,
-        },
-      ],
-    },
-  ];
+          {
+            text: 'Resources',
+            Icon: FolderIcon,
+            link: `${link}/docs`,
+          },
+        ],
+      },
+    ],
+  };
 };
 
 const List = () => {
-  const sidebarData = useSidebarData();
+  const { data, handleOnClick } = useSidebarData();
   const router = useRouter();
-  const routerPush = (params) => () => router.push(params);
-  const isActive = (link) => router.asPath.includes(link);
   return (
     <ListWrapper>
-      {sidebarData?.map(({ label, items }) => (
+      {data?.map(({ label, items }) => (
         <ListWrapper key={label}>
           <Label>{label}</Label>
           <ListWrapper>
-            {items.map(({ text, link, Icon, count }) => (
-              <Item key={text} onClick={routerPush(link)} Icon={Icon} isActive={isActive(link)} count={count}>
-                {text}
-              </Item>
-            ))}
+            {items.map(({ text, link, Icon, count, entityType = null }) => {
+              const isActive = entityType ? window.location.toString().includes(link) : router.asPath.includes(link);
+              return (
+                <Item
+                  key={text}
+                  onClick={handleOnClick(link, entityType)}
+                  Icon={Icon}
+                  isActive={isActive}
+                  count={count}
+                >
+                  {text}
+                </Item>
+              );
+            })}
           </ListWrapper>
         </ListWrapper>
       ))}
