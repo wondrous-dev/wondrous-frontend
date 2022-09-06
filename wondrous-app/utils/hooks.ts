7829ef8528f170457bcfc0418b5d8df6b491fc58
Valentyn Yefimov <valentine.efimov@gmail.com>
2022-09-06 21:36:21 +0200

    Guild token gating (#927)
    * feat: import styles for token gatin
    
    * feat: guild roles
    
    * feat: guild token gating
    
    * feat: guild token gating
    
    * fix: ci
    
    * fix: ci
    
    * feat: change schema for guild
    
    * feat: fix guild for role
    
    * feat: re-use components
    
    * feat: re-use components
    
    * feat: re-use components
    
    * feat: add tabs to the token-gating page
    
    * feat: add tabs to the roles page
    
    * fix minor ui bug
    
    * fix: tabs spacing
    
    Co-authored-by: terryli0095 <terryli0095@gmail.com>

--- wondrous-app/utils/hooks.ts
+++ wondrous-app/utils/hooks.ts
@@ -1,6 +1,7 @@
 import { NextRouter, useRouter } from 'next/router';
 import { useContext, useState, useEffect, useRef, Dispatch, SetStateAction, useMemo } from 'react';
 import apollo from 'services/apollo';
+import { TokenGatingCondition } from 'types/TokenGating';
 import { PRIVACY_LEVEL, TASK_TYPE, PERMISSIONS, BOUNTY_TYPE, MILESTONE_TYPE } from 'utils/constants';
 import {
   GET_PER_STATUS_TASK_COUNT_FOR_USER_BOARD,
@@ -23,10 +24,10 @@ import {
   ApprovedSubmissionContext,
   PaymentModalContext,
   SelectMembershipContext,
-  EditTokenGatingConditionContext,
   UserProfileContext,
   GlobalContext,
   HotkeyContext,
+  TokenGatingContext,
 } from './contexts';
 
 export const useIsMobile = () => useContext(IsMobileContext);
@@ -108,7 +109,13 @@ export const usePaymentModal = () => useContext(PaymentModalContext);
 
 export const useSelectMembership = () => useContext(SelectMembershipContext);
 
-export const useEditTokenGatingCondition = () => useContext(EditTokenGatingConditionContext);
+export const useTokenGatingCondition = (): {
+  editTokenGating: (tokenGatingCondition: TokenGatingCondition) => void;
+  deleteTokenGating: (tokenGatingCondition: TokenGatingCondition) => void;
+  closeTokenGatingModal: () => void;
+  selectedTokenGatingCondition: TokenGatingCondition;
+} => useContext(TokenGatingContext);
+
 /**
  * Hook that alerts clicks outside of the passed ref
  */
