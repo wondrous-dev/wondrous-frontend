9fce90a919821f539413b7503deab0f5c5305b05
Cristobal Chao <chao@torii.co>
2022-09-07 12:50:18 -0400

    Cleanup

--- wondrous-app/cypress.config.ts
+++ wondrous-app/cypress.config.ts
@@ -3,7 +3,7 @@
 import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  projectId: 'xw5k6b', // TODO: We need to create a new `projectId`
+  projectId: 'wonder', // TODO: We need to create a new `projectId`
   e2e: {
     setupNodeEvents(on, config) {
       require('cypress-localstorage-commands/plugin')(on, config);
