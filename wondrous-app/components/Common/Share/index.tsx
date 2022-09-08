068c972997c28fdbe3bd600e67389f5fbc94a8b1
Junius Free B. Fontamillas <juniusfree@users.noreply.github.com>
2022-09-09 01:54:46 +0800

    fix: Share link not working, refactor Share comp (#970)

--- wondrous-app/components/Common/Share/index.tsx
+++ wondrous-app/components/Common/Share/index.tsx
@@ -1,36 +1,37 @@
-import { useContext } from 'react';
-import { ENTITIES_TYPES, TASK_TYPE } from 'utils/constants';
-import { capitalize } from 'utils/common';
-import Tooltip from 'components/Tooltip';
+import { StyledShare } from 'components/Common/Share/styles';
+import { SnackbarAlertContext } from 'components/Common/SnackbarAlert';
 import { TaskShareIcon } from 'components/Icons/taskShare';
-import { SnackbarAlertContext } from '../SnackbarAlert';
-import { StyledShare } from './styles';
+import Tooltip from 'components/Tooltip';
+import { useContext } from 'react';
+import { capitalize } from 'utils/common';
+import { ENTITIES_TYPES, LINK } from 'utils/constants';
 
 interface IShareProps {
-  url: string;
-  entityType?: string;
+  fetchedTask: {
+    orgUsername: string;
+    parentTaskId?: string;
+    id: string;
+    type?: string;
+  };
 }
 
-const Share = ({ url, entityType = TASK_TYPE }: IShareProps) => {
+const Share = ({ fetchedTask }: IShareProps) => {
   const { setSnackbarAlertOpen, setSnackbarAlertMessage } = useContext(SnackbarAlertContext);
-  const snackbarAlertMessage = `${capitalize(entityType)} link copied`;
+  const { id, parentTaskId, orgUsername, type } = fetchedTask;
+  const entityType = type || ENTITIES_TYPES.PROPOSAL;
+  const taskTypeQuery = type ? `task` : `taskProposal`;
+  const taskId = parentTaskId || id;
   const handleOnClick = (e) => {
     e.preventDefault();
     e.stopPropagation();
-    navigator.clipboard.writeText(url);
-    setSnackbarAlertMessage(snackbarAlertMessage);
+    navigator.clipboard.writeText(
+      `${LINK}/organization/${orgUsername}/boards?${taskTypeQuery}=${taskId}&entity=${entityType}`
+    );
+    setSnackbarAlertMessage(`${capitalize(entityType)} link copied`);
     setSnackbarAlertOpen(true);
   };
-
-  const getTitle = (type) => {
-    if (type === ENTITIES_TYPES.PROPOSAL) return 'Share Proposal';
-    if (type === ENTITIES_TYPES.BOUNTY) return 'Share Bounty';
-
-    return 'Share Task';
-  };
-
   return (
-    <Tooltip title={getTitle(entityType)} placement="top">
+    <Tooltip title={`Share ${capitalize(entityType)}`} placement="top">
       <StyledShare onClick={handleOnClick}>
         <TaskShareIcon />
       </StyledShare>
