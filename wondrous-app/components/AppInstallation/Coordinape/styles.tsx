5fefff9a1f29a29a980062671c1f1ca09191efc1
SankhaChak <anonproductionspro@gmail.com>
2022-09-14 22:00:27 +0530

    fix: background image

--- wondrous-app/components/AppInstallation/Coordinape/styles.tsx
+++ wondrous-app/components/AppInstallation/Coordinape/styles.tsx
@@ -2,7 +2,8 @@ import styled from 'styled-components';
 
 export const CoordinapePageWrapper = styled.div`
   min-height: 100vh;
-  background: url('/images/app-installation/animated-background.svg') no-repeat center center;
+  background: url('/images/onboarding/background.png') no-repeat;
+  background-size: cover;
   display: flex;
   align-items: center;
   justify-content: center;
