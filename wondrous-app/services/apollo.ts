dbb8c26dfbf581e28b9f2407d09588f186af4822
Adrian Mustea <mustea.adrian95@gmail.com>
2022-09-20 12:25:49 +0300

    added key args

--- wondrous-app/services/apollo.ts
+++ wondrous-app/services/apollo.ts
@@ -121,7 +121,7 @@ const cache = new InMemoryCache({
           keyArgs: ['input', ['orgId', 'podIds', 'date', 'statuses']],
           merge: offsetLimitPaginationInput,
         },
-        getOrgUsers: offsetLimitPagination(['orgId', 'searchString', 'filterByRoles']),
+        getOrgUsers: offsetLimitPagination(['orgId', 'searchString', 'roleIds', 'limit', 'offset']),
       },
     },
   },
