1003398a7a23a3b0cb9209f76e78f87bb642ea82
terryli0095 <terryli0095@gmail.com>
2022-09-24 23:47:33 -0700

    clean up create pod modal

--- wondrous-app/components/Pod/analytics/index.tsx
+++ wondrous-app/components/Pod/analytics/index.tsx
@@ -40,9 +40,13 @@ import {
   StyledChip,
 } from 'components/CreateEntity/styles';
 import palette from 'theme/palette';
-import { filterOrgUsers } from 'components/CreateEntity/CreatePodModal';
 import CSVModal from 'components/organization/analytics/CSVModal';
-import { calculateCount, exportContributorTaskCSV, getContributorTaskData } from 'components/organization/analytics';
+import {
+  calculateCount,
+  exportContributorTaskCSV,
+  getContributorTaskData,
+  filterOrgUsers,
+} from 'components/organization/analytics';
 import { BOUNTY_TYPE, PRIVATE_TASK_TITLE } from 'utils/constants';
 import Wrapper from '../wrapper';
 import { Post } from '../../Common/Post';
