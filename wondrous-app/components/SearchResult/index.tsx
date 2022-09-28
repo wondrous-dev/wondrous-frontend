0d1ba504331c4c25560867d50fa2e13d985c7322
juniusfree <8164667+juniusfree@users.noreply.github.com>
2022-09-28 17:06:05 +0800

    feat: dnd task items

--- wondrous-app/components/SearchResult/index.tsx
+++ wondrous-app/components/SearchResult/index.tsx
@@ -2,40 +2,64 @@ import { useQuery } from '@apollo/client';
 import ListView from 'components/ListView';
 import { GET_PER_STATUS_TASK_COUNT_FOR_USER_CREATED_TASK, SEARCH_USER_CREATED_TASKS } from 'graphql/queries';
 import groupBy from 'lodash/groupBy';
+import { useMemo, useReducer } from 'react';
 import { generateColumns } from 'services/board';
-import { COLUMNS_CONFIGURATION } from 'utils/constants';
+import { COLUMNS_CONFIGURATION, ENTITIES_TYPES } from 'utils/constants';
+import { UserBoardContext } from 'utils/contexts';
 
 import { Title, Wrapper } from './styles';
 
-const COLUMNS_SNAKE_TO_CAMEL_CASE = {
-  created: 'created',
-  in_progress: 'inProgress',
-  in_review: 'inReview',
-  completed: 'completed',
-};
-
-const ORG_POD_COLUMNS = generateColumns(false, COLUMNS_CONFIGURATION.ORG);
-
-const SearchResult = () => {
-  const { data: userCreatedTasksData } = useQuery(SEARCH_USER_CREATED_TASKS, {
+const useSearchUserCreatedTasks = () => {
+  const ORG_POD_COLUMNS = generateColumns(false, COLUMNS_CONFIGURATION.ORG);
+  const handleSetColumns = (columns, { key, val }) =>
+    columns.map(({ status, ...column }) => ({
+      ...column,
+      status,
+      [key]: val(status),
+    }));
+  const [columns, setColumns] = useReducer(handleSetColumns, ORG_POD_COLUMNS);
+  useQuery(SEARCH_USER_CREATED_TASKS, {
     variables: {
       statuses: ['created', 'in_progress', 'in_review', 'completed'],
     },
+    onCompleted: ({ searchUserCreatedTask }) => {
+      const taskByStatus = groupBy(searchUserCreatedTask, 'status');
+      setColumns({ key: 'tasks', val: (i) => taskByStatus[i] });
+    },
   });
-  const userCreatedTasks = userCreatedTasksData?.searchUserCreatedTask;
-  const { data: perStatusCountData } = useQuery(GET_PER_STATUS_TASK_COUNT_FOR_USER_CREATED_TASK);
-  const perStatusCount = perStatusCountData?.getPerStatusTaskCountForUserCreatedTask;
-  const taskByStatus = groupBy(userCreatedTasks, 'status');
-  const columns = ORG_POD_COLUMNS.map(({ status }) => ({
-    status,
-    tasks: taskByStatus[status],
-    count: perStatusCount?.[COLUMNS_SNAKE_TO_CAMEL_CASE?.[status]] || 0,
-  }));
+  useQuery(GET_PER_STATUS_TASK_COUNT_FOR_USER_CREATED_TASK, {
+    onCompleted: ({ getPerStatusTaskCountForUserCreatedTask }) => {
+      const COLUMNS_SNAKE_TO_CAMEL_CASE = {
+        created: 'created',
+        in_progress: 'inProgress',
+        in_review: 'inReview',
+        completed: 'completed',
+      };
+      setColumns({
+        key: 'count',
+        val: (i) => getPerStatusTaskCountForUserCreatedTask?.[COLUMNS_SNAKE_TO_CAMEL_CASE?.[i]] || 0,
+      });
+    },
+  });
+  return { columns, setColumns };
+};
+
+const SearchResult = () => {
+  const { columns, setColumns } = useSearchUserCreatedTasks();
+  const value = useMemo(
+    () => ({
+      entityType: ENTITIES_TYPES.TASK,
+      setColumns,
+    }),
+    [setColumns]
+  );
   return (
-    <Wrapper>
-      <Title>Tasks I've Created</Title>
-      <ListView columns={columns} onLoadMore={() => null} hasMore={false} />
-    </Wrapper>
+    <UserBoardContext.Provider value={value}>
+      <Wrapper>
+        <Title>Tasks I've Created</Title>
+        <ListView columns={columns} onLoadMore={() => null} hasMore={false} />
+      </Wrapper>
+    </UserBoardContext.Provider>
   );
 };
 
--- wondrous-app/pages/search-result/index.tsx
+++ wondrous-app/pages/search-result/index.tsx
@@ -1,21 +1,6 @@
 import { withAuth } from 'components/Auth/withAuth';
 import SearchResult from 'components/SearchResult';
-import React from 'react';
-import { ENTITIES_TYPES } from 'utils/constants';
-import { UserBoardContext } from 'utils/contexts';
 
-const SearchResultPage = () => {
-  const value = React.useMemo(
-    () => ({
-      entityType: ENTITIES_TYPES.TASK,
-    }),
-    []
-  );
-  return (
-    <UserBoardContext.Provider value={value}>
-      <SearchResult />;
-    </UserBoardContext.Provider>
-  );
-};
+const SearchResultPage = () => <SearchResult />;
 
 export default withAuth(SearchResultPage);
