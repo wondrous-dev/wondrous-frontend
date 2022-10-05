7b6aef6188a24d029b95bd264b201ac6e619c60b
juniusfree <8164667+juniusfree@users.noreply.github.com>
2022-10-06 07:57:55 +0800

    refactor: use the existing modal
    Co-Authored-By: Valentyn Yefimov <valentine.efimov@gmail.com>

--- wondrous-app/components/CreateEntityDiscardTask/index.tsx
+++ wondrous-app/components/CreateEntityDiscardTask/index.tsx
@@ -1,29 +1,42 @@
-import CloseModalIcon from 'components/Icons/closeModal';
-import { Body, Cancel, CloseHeaderIcon, Discard, Footer, Header, HeaderText, StyledModal, Wrapper } from './styles';
+import Typography from '@mui/material/Typography';
 
-const CreateEntityDiscardTask = ({ open, onClose, onCloseFormModal, entityType }) => (
-  <StyledModal open={open} onClose={() => onClose(false)}>
-    <Wrapper>
-      <Header>
-        <HeaderText>Discard {entityType} data?</HeaderText>
-        <CloseHeaderIcon onClick={() => onClose(false)}>
-          <CloseModalIcon />
-        </CloseHeaderIcon>
-      </Header>
-      <Body>Are you sure you want to discard your current {entityType}?</Body>
-      <Footer>
-        <Cancel onClick={() => onClose(false)}>Cancel</Cancel>
-        <Discard
-          onClick={() => {
-            onCloseFormModal();
-            onClose(false);
-          }}
-        >
-          Discard {entityType}
-        </Discard>
-      </Footer>
-    </Wrapper>
-  </StyledModal>
-);
+import WonderButton from 'components/Button';
+import WonderModal from 'components/Modal';
+
+const CreateEntityDiscardTask = ({ open, onClose, onCloseFormModal, entityType }) => {
+  const footerRight = (
+    <>
+      <WonderButton color="grey" onClick={() => onClose(false)}>
+        Cancel
+      </WonderButton>
+      <WonderButton
+        color="primary"
+        onClick={() => {
+          onCloseFormModal();
+          onClose(false);
+        }}
+        buttonTheme={{
+          background: '#0f0f0f',
+        }}
+      >
+        Discard {entityType}
+      </WonderButton>
+    </>
+  );
+
+  return (
+    <WonderModal
+      title={`Discard ${entityType} data?`}
+      onClose={() => onClose(false)}
+      footerRight={footerRight}
+      maxWidth={560}
+      open={open}
+    >
+      <Typography sx={{ color: 'white', textAlign: 'center' }}>
+        Are you sure you want to discard your current {entityType}?
+      </Typography>
+    </WonderModal>
+  );
+};
 
 export default CreateEntityDiscardTask;
