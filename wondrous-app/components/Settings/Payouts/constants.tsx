d3e6a402824d33994037d03cb269dd9852408cbf
SankhaChak <anonproductionspro@gmail.com>
2022-10-13 03:45:21 +0530

    wip: payment-ledger-revamp [selection]

--- wondrous-app/components/Settings/Payouts/constants.tsx
+++ wondrous-app/components/Settings/Payouts/constants.tsx
@@ -1,1 +1,7 @@
 export const INITIAL_SELECTION_OPTIONS = [{ label: 'Select all payments', value: 'all' }];
+
+export const PAYMENT_TYPES = {
+  PAID: 'paid',
+  PROCESSING: 'processing',
+  UNPAID: 'unpaid',
+};
