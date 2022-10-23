ab17213b6cf4e857cb993f030d2d3d8cb114b3c2
Adrian <mustea.adrian95@gmail.com>
2022-10-23 09:14:16 +0300

    extend search org users to accept array of org ids (#1138)

--- wondrous-app/components/CreateCollaborationModal/Steps/AddTeamMembers/index.tsx
+++ wondrous-app/components/CreateCollaborationModal/Steps/AddTeamMembers/index.tsx
@@ -44,7 +44,7 @@ import {
 } from './styles';
 
 type Props = {
-  org: Org;
+  parentOrgIds: string[];
   onCancel: () => void;
   onSubmit: () => void | Promise<any>;
   footerRef: React.RefObject<HTMLDivElement>;
@@ -55,7 +55,7 @@ type Props = {
 };
 
 const AddTeamMembers = ({
-  org,
+  parentOrgIds,
   onSubmit,
   onCancel,
   footerRef,
@@ -116,7 +116,7 @@ const AddTeamMembers = ({
 
   const handleInputChange = (e) => {
     setInputValue(e.target.value);
-    if (e.target.value) search({ variables: { searchString: e.target.value, orgId: org.id } });
+    if (e.target.value) search({ variables: { searchString: e.target.value, orgsIds: parentOrgIds } });
   };
 
   const handleChange = (key, event, users) => setUsers({ ...selectedUsers, [key]: users });
