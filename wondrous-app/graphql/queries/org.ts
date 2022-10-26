523bc4024771c31d194258c326b2fd60d8c58899
terryli0095 <terryli0095@gmail.com>
2022-10-26 16:10:41 -0700

    fix org ids

--- wondrous-app/graphql/queries/org.ts
+++ wondrous-app/graphql/queries/org.ts
@@ -195,8 +195,8 @@ export const GET_ORG_PODS = gql`
 `;
 
 export const SEARCH_ORG_USERS = gql`
-  query searchOrgUsers($orgsIds: [ID], $searchString: String!, $limit: Int, $offset: Int) {
-    searchOrgUsers(orgsIds: $orgsIds, searchString: $searchString, limit: $limit, offset: $offset) {
+  query searchOrgUsers($orgIds: [ID], $searchString: String!, $limit: Int, $offset: Int) {
+    searchOrgUsers(orgIds: $orgIds, searchString: $searchString, limit: $limit, offset: $offset) {
       id
       username
       profilePicture
