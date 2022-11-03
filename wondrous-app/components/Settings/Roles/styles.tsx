27044bdde9db129de6b3f79d934aa87428983403
Sankha <61096193+SankhaChak@users.noreply.github.com>
2022-11-03 13:22:04 +0530

    feat: role page revamp (#1194)
    * feat: role page revamp
    
    * fix: pod page select permissions accordion margin

--- wondrous-app/components/Settings/Roles/styles.tsx
+++ wondrous-app/components/Settings/Roles/styles.tsx
@@ -5,13 +5,15 @@ import { InputBase, Switch, Typography } from '@mui/material';
 import SnackbarComp from '@mui/material/Snackbar';
 
 import { newDropdownStyles } from 'components/Common/DropdownSelect/styles';
+import { BaseCard } from 'components/Common/card';
+import { Button } from 'components/Common/button';
+import Accordion from 'components/Common/Accordion';
+
 import palette from 'theme/palette';
-import { Button } from '../../Common/button';
-import { BaseCard } from '../../Common/card';
+import typography from 'theme/typography';
 
 export const RolesContainer = styled.div`
-  width: 100%;
-  max-width: 765px;
+  width: 780px;
 
   ${newDropdownStyles}
 `;
@@ -30,28 +32,55 @@ export const RoleNameBlock = styled.div`
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  margin-bottom: 28px;
 `;
 
 export const CreateRole = styled.div`
   display: flex;
+  align-items: center;
+  gap: 12px;
 `;
 
 export const CreateRoleButton = styled(Button)`
-  width: 160px;
-  margin-left: 22px;
-  height: 36px;
-  box-sizing: content-box;
-  min-height: 36px;
+  && {
+    background: linear-gradient(
+      270deg,
+      ${palette.blue20} -5.62%,
+      ${palette.highlightPurple} 45.92%,
+      ${palette.highlightBlue} 103.12%
+    );
+    min-width: fit-content;
+    padding: 1px;
+
+    button {
+      font-family: ${typography.fontFamily};
+      font-weight: 600;
+      padding: 9.5px 24px;
+      min-width: min-content;
+      ${({ theme }) => `
+        background: ${theme.palette.background.default};
+    `}
+      transition: background 0.3s ease;
+
+      &:hover {
+        background: transparent;
+      }
+    }
+  }
 `;
 
-export const DeleteButton = styled(Button).attrs({ variant: 'outlined', color: 'error' })`
-  && {
-    border: 1px solid #cb3340;
-    color: white;
-    width: 150px;
-    height: 36px;
-    min-height: 36px;
+export const DeleteButton = styled(Button)`
+  && button {
+    color: ${palette.red300};
+    background: ${palette.grey950};
+    font-family: ${typography.fontFamily};
+    font-weight: 600;
+    font-size: 16px;
+    padding: 9px 24px;
+    transition: background 0.3s ease;
+
+    &:hover {
+      background: ${palette.grey920};
+    }
   }
 `;
 
@@ -59,16 +88,19 @@ export const Permissions = styled.div`
   padding: 0 24px;
 `;
 
+export const RolePermissionsList = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+  column-gap: 28px;
+`;
+
 export const Permission = styled.div`
   color: white;
   display: flex;
   justify-content: space-between;
   align-items: center;
-  height: 85px;
-
-  &:not(:first-child) {
-    border-top: 1px solid #4b4b4b;
-  }
+  padding: 12px 0;
+  border-bottom: ${({ showBorder }) => (showBorder ? `1px solid ${palette.black91}` : 'none')};
 `;
 
 export const RoleTokenGatingWrapper = styled.div`
@@ -78,16 +110,32 @@ export const RoleTokenGatingWrapper = styled.div`
   height: 85px;
 
   &:not(:first-child) {
-    border-top: 1px solid #4b4b4b;
+    border-top: 1px solid ${palette.black91};
   }
 `;
 
 export const PermissionTitle = styled.h5`
   color: ${palette.white};
   font-size: 16px;
-  margin-bottom: 7px;
-  margin-top: 14px;
+  margin: 0;
   font-family: Space Grotesk;
+
+  display: flex;
+  align-items: center;
+  gap: 10px;
+`;
+
+export const PermissionTitleLabel = styled(Typography)`
+  && {
+    font-size: 13px !important;
+    font-weight: 600 !important;
+    color: ${({ isInactive, isNone }) =>
+      isInactive ? palette.red300 : isNone ? palette.grey250 : palette.green800} !important;
+    background: ${({ isInactive, isNone }) =>
+      isInactive ? palette.red850 : isNone ? palette.grey88 : palette.green720};
+    padding: 4px;
+    border-radius: 6px;
+  }
 `;
 
 export const PermissionSubtitle = styled.div`
@@ -96,8 +144,8 @@ export const PermissionSubtitle = styled.div`
 `;
 
 export const PermissionFooter = styled.div`
-  border-top: 1px solid #4b4b4b;
-  padding: 50px 0 30px;
+  background: ${palette.black97};
+  padding: 24px;
   display: flex;
   justify-content: flex-end;
   font-family: Space Grotesk;
@@ -106,29 +154,57 @@ export const PermissionFooter = styled.div`
 export const RoleNameInput = styled(InputBase)`
   && {
     width: 100%;
-    height: 40px;
-    border: 1px solid #4b4b4b;
+    background: ${palette.black101};
+    color: ${palette.white};
+    padding: 12px;
     border-radius: 6px;
+    font-size: 15px;
+    font-family: ${typography.fontFamily};
+    max-height: 42px;
 
-    font-size: 14px;
-    line-height: 21px;
-    letter-spacing: 0.01em;
-    color: #c4c4c4;
-    padding: 10px 15px;
-    font-family: Space Grotesk;
+    input {
+      padding: 0;
+    }
   }
 `;
 
 export const LabelBlock = styled(Typography)`
   && {
     ${space};
-    margin-left: 20px;
     font-weight: 500;
     font-size: 14px;
     line-height: 18px;
     letter-spacing: 0.01em;
     color: ${palette.blue20};
     margin-bottom: 10px;
+  }
+`;
+
+export const RoleAccordion = styled(Accordion)`
+  && {
+    border-radius: 6px;
+    border: none !important;
+    background: ${palette.black101};
+    padding: 1px;
+    opacity: ${({ disabled }) => (disabled ? 0.5 : 1)};
+    transition: opacity 0.3s ease;
+    margin-bottom: ${({ hasMarginBottom }) => (hasMarginBottom ? '28px' : '0')};
+
+    p {
+      color: ${palette.white};
+      font-size: 15px;
+      font-family: ${typography.fontFamily};
+      font-weight: 400;
+    }
+
+    svg {
+      background: ${palette.grey87};
+      width: 24px;
+      height: 24px;
+      padding: 8.95px 6.5px;
+      border-radius: 4px;
+      stroke: ${palette.blue20};
+    }
   }
 `;
 
@@ -144,17 +220,41 @@ export const Error = styled.h3`
   margin: 30px 0;
 `;
 
-export const TokenGatingButton = styled(Button)`
-  min-width: 87px;
-  max-width: 166px;
-  height: 40px;
-  min-height: 40px;
-`;
+export const TokenGatingButton = styled(CreateRoleButton)``;
 
-export const ImportDiscordRoleButton = styled(Button)`
+export const ImportDiscordRoleButton = styled(CreateRoleButton)`
   && {
-    max-width: 400px;
-    margin-top: 10px;
+    width: fit-content;
+    padding: 1px;
+    margin-bottom: 28px;
+    margin-top: 12px;
+
+    button {
+      padding: 8px 14px;
+      ${({ theme }) => `
+        background: ${theme.palette.background.default};
+    `}
+      transition: background 0.3s ease;
+      display: flex;
+      align-items: center;
+      gap: 10px;
+
+      &:hover {
+        background: transparent;
+
+        svg path {
+          fill: ${palette.white};
+        }
+      }
+
+      svg {
+        margin-bottom: -2px;
+
+        path {
+          transition: fill 0.3s ease;
+        }
+      }
+    }
   }
 `;
 
@@ -172,6 +272,12 @@ export const TitleLockIconWrapper = styled.div`
   align-items: center;
   > * {
     margin-right: 10px;
+  }
+
+  svg {
+    width: auto !important;
+    height: auto !important;
+    background: transparent !important;
   }
 `;
 
