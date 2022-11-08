836b961ae64742b720866a4e4607428eb6dc3bff
juniusfree <8164667+juniusfree@users.noreply.github.com>
2022-11-08 14:27:20 +0800

    feat: add menu to bounty card

--- wondrous-app/components/Common/BountyBoard/index.tsx
+++ wondrous-app/components/Common/BountyBoard/index.tsx
@@ -1,6 +1,8 @@
+import { useMutation } from '@apollo/client';
 import Box from '@mui/material/Box';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
+import { useMe } from 'components/Auth/withAuth';
 import {
   BoardsCardBody,
   BoardsCardBodyDescription,
@@ -16,19 +18,27 @@ import GR15DEIModal from 'components/Common/IntiativesModal/GR15DEIModal';
 import { GR15DEILogo } from 'components/Common/IntiativesModal/GR15DEIModal/GR15DEILogo';
 import { ToggleBoardPrivacyIcon } from 'components/Common/PrivateBoardIcon';
 import { PodName, PodWrapper } from 'components/Common/Task/styles';
+import TaskCardDate from 'components/Common/TaskCardDate';
+import TaskCardStatus from 'components/Common/TaskCardStatuts';
 import TaskPriority from 'components/Common/TaskPriority';
 import { hasGR15DEIIntiative } from 'components/Common/TaskViewModal/utils';
+import { CreateEntity } from 'components/CreateEntity';
 import CommentsIcon from 'components/Icons/comments';
 import { DAOIcon } from 'components/Icons/dao';
 import PodIcon from 'components/Icons/podIcon';
 import { RichTextViewer } from 'components/RichText';
+import { ARCHIVE_TASK } from 'graphql/mutations';
+import { SEARCH_USER_CREATED_TASKS } from 'graphql/queries';
 import { useRouter } from 'next/router';
-import { Fragment, useState } from 'react';
+import { Fragment, useCallback, useContext, useState } from 'react';
 import palette from 'theme/palette';
-import { PRIVACY_LEVEL } from 'utils/constants';
-import TaskCardDate from 'components/Common/TaskCardDate';
-import TaskCardStatus from 'components/Common/TaskCardStatuts';
-import { BountyBoardEmpty, BountyCardWrapper } from './styles';
+import { PERMISSIONS, PRIVACY_LEVEL } from 'utils/constants';
+import { parseUserPermissionContext } from 'utils/helpers';
+import { useGlobalContext } from 'utils/hooks';
+import { SnackbarAlertContext } from '../SnackbarAlert';
+import ActionModals from '../TaskViewModal/actionModals';
+import { Menu } from '../TaskViewModal/helpers';
+import { BountyBoardEmpty, BountyCardWrapper, MenuWrapper } from './styles';
 
 export function SubmissionsCount({ total, approved }) {
   const config = [
@@ -73,8 +83,33 @@ export function SubmissionsCount({ total, approved }) {
   );
 }
 
-export default function Board({ tasks, handleCardClick = (bounty) => {}, displayOrg = false, Container = Fragment }) {
+const usePermissions = (entity) => {
+  const globalContext = useGlobalContext();
+  const user = useMe();
+  const getUserPermissionContext = useCallback(() => globalContext?.userPermissionsContext, [globalContext]);
+  const userPermissionsContext = getUserPermissionContext();
+  const permissions = parseUserPermissionContext({
+    userPermissionsContext,
+    orgId: entity?.orgId,
+    podId: entity?.podId,
+  });
+  const canEdit =
+    permissions.includes(PERMISSIONS.FULL_ACCESS) ||
+    permissions.includes(PERMISSIONS.EDIT_TASK) ||
+    entity?.createdBy === user?.id ||
+    (entity?.assigneeId && entity?.assigneeId === user?.id);
+  const canArchive =
+    permissions.includes(PERMISSIONS.MANAGE_BOARD) ||
+    permissions.includes(PERMISSIONS.FULL_ACCESS) ||
+    entity?.createdBy === user?.id;
+  return { canEdit, canArchive };
+};
+
+const BountyItem = ({ bounty, handleCardClick, displayOrg }) => {
   const router = useRouter();
+  const { canEdit, canArchive } = usePermissions(bounty);
+  const { setSnackbarAlertOpen, setSnackbarAlertMessage } = useContext(SnackbarAlertContext);
+  const hasGR15 = hasGR15DEIIntiative(bounty?.categories);
   const goToPod = (podId) => {
     router.push(`/pod/${podId}/boards`, undefined, {
       shallow: true,
@@ -82,128 +117,208 @@ export default function Board({ tasks, handleCardClick = (bounty) => {}, display
   };
   const [openGR15Modal, setOpenGR15Modal] = useState(false);
   const goToOrg = (orgUsername) => router.push(`/organization/${orgUsername}/boards`, undefined, { shallow: true });
+
+  const [anchorEl, setAnchorEl] = useState();
+  const [editTask, setEditTask] = useState(false);
+  const [archiveTask, setArchiveTask] = useState(false);
+  const [completeModal, setCompleteModal] = useState(false);
+
+  const handleCloseModal = () => {
+    setAnchorEl(null);
+    setEditTask(false);
+  };
+
+  const [archiveTaskMutation] = useMutation(ARCHIVE_TASK, {
+    refetchQueries: [
+      'getTaskById',
+      'getUserTaskBoardTasks',
+      'getPerStatusTaskCountForUserBoard',
+      'getOrgTaskBoardTasks',
+      'getPerStatusTaskCountForOrgBoard',
+      'getPodTaskBoardTasks',
+      'getPerStatusTaskCountForPodBoard',
+      SEARCH_USER_CREATED_TASKS,
+    ],
+    onError: () => {
+      console.error('Something went wrong with archiving tasks');
+    },
+  });
+
+  const handleOnCloseArchiveTaskModal = () => setArchiveTask(false);
+
+  return (
+    <>
+      {editTask && (
+        <CreateEntity
+          open={editTask}
+          handleCloseModal={() => {
+            setEditTask(false);
+            handleCloseModal();
+          }}
+          entityType={bounty?.type}
+          handleClose={() => {
+            setEditTask(false);
+            handleCloseModal();
+          }}
+          cancel={() => setEditTask(false)}
+          existingTask={
+            bounty?.id && {
+              ...bounty,
+              // reviewers: reviewerData?.getTaskReviewers || [],
+            }
+          }
+        />
+      )}
+      <ActionModals
+        completeModal={completeModal}
+        setCompleteModal={setCompleteModal}
+        taskType={bounty?.type}
+        fetchedTask={bounty}
+        archiveTask={archiveTask}
+        archiveTaskMutation={archiveTaskMutation}
+        handleOnCloseArchiveTaskModal={handleOnCloseArchiveTaskModal}
+        setSnackbarAlertOpen={setSnackbarAlertOpen}
+        setSnackbarAlertMessage={setSnackbarAlertMessage}
+      />
+      <BountyCardWrapper onClick={() => handleCardClick(bounty)} key={bounty.id}>
+        <BoardsCardHeader>
+          <BoardsCardSubheader>
+            <TaskCardStatus type={bounty?.type} orgId={bounty?.orgId} status={bounty?.status} />
+            {hasGR15 && (
+              <>
+                <GR15DEIModal open={openGR15Modal} onClose={() => setOpenGR15Modal(false)} />
+                <GR15DEILogo width="28" height="28" onClick={() => setOpenGR15Modal(true)} />
+              </>
+            )}
+            {bounty?.privacyLevel !== PRIVACY_LEVEL.public && (
+              <ToggleBoardPrivacyIcon
+                style={{
+                  width: '28px',
+                  height: '28px',
+                  marginRight: '0',
+                }}
+                isPrivate={bounty?.privacyLevel !== PRIVACY_LEVEL.public}
+                tooltipTitle={bounty?.privacyLevel !== PRIVACY_LEVEL.public ? 'Private' : 'Public'}
+              />
+            )}
+          </BoardsCardSubheader>
+
+          <Grid container width="fit-content" flexGrow="1" justifyContent="flex-end" gap="6px">
+            <TaskCardDate date={bounty?.dueDate} />
+            {bounty?.rewards && bounty?.rewards?.length > 0 && <Compensation rewards={bounty?.rewards} />}
+          </Grid>
+        </BoardsCardHeader>
+        <BoardsCardBody>
+          <BoardsCardBodyTitle>{bounty.title}</BoardsCardBodyTitle>
+          {bounty?.priority ? (
+            <Box>
+              <TaskPriority value={bounty?.priority} />
+            </Box>
+          ) : null}
+          <BoardsCardBodyDescription>
+            <RichTextViewer text={bounty.description} />
+          </BoardsCardBodyDescription>
+          <SubmissionsCount total={bounty.totalSubmissionsCount} approved={bounty.approvedSubmissionsCount} />
+          {bounty?.media?.[0] && bounty?.media?.[0]?.type === 'image' ? (
+            <BoardsCardMedia>
+              <SafeImage
+                useNextImage={false}
+                style={{
+                  width: '100%',
+                  height: '100%',
+                  objectFit: 'cover',
+                  objectPosition: 'center',
+                  borderRadius: '6px',
+                }}
+                src={bounty?.media[0].slug}
+              />
+            </BoardsCardMedia>
+          ) : null}
+        </BoardsCardBody>
+        <BoardsCardFooter>
+          {bounty?.podName && !displayOrg && (
+            <PodWrapper
+              style={{ marginTop: '0' }}
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                goToPod(bounty?.podId);
+              }}
+            >
+              <PodIcon
+                color={bounty?.podColor || palette.grey900}
+                style={{
+                  width: '26px',
+                  height: '26px',
+                }}
+              />
+              <PodName>{bounty?.podName}</PodName>
+            </PodWrapper>
+          )}
+          {displayOrg && (
+            <PodWrapper
+              style={{ marginTop: '0', alignItems: 'center' }}
+              onClick={(e) => {
+                e.preventDefault();
+                e.stopPropagation();
+                goToOrg(bounty?.orgUsername);
+              }}
+            >
+              {bounty?.orgProfilePicture ? (
+                <SafeImage
+                  src={bounty.orgProfilePicture}
+                  style={{
+                    width: '26px',
+                    height: '26px',
+                    borderRadius: '4px',
+                    marginRight: '8px',
+                  }}
+                />
+              ) : (
+                <DAOIcon />
+              )}
+
+              <PodName>{bounty?.orgName}</PodName>
+            </PodWrapper>
+          )}
+          <div
+            style={{
+              flex: 1,
+            }}
+          />
+          <Grid item container width="fit-content" gap="12px">
+            <Grid item container gap="10px" width="fit-content" lineHeight="0" alignItems="center">
+              <CommentsIcon />
+              {bounty.commentCount || 0}
+            </Grid>
+            <MenuWrapper open={Boolean(anchorEl)}>
+              <Menu
+                canArchive={canArchive}
+                canEdit={canEdit}
+                isBounty
+                setArchiveTask={setArchiveTask}
+                setCompleteModal={setCompleteModal}
+                setEditTask={setEditTask}
+                taskType={bounty?.type}
+                style={{ height: '28px' }}
+                setAnchorElParent={setAnchorEl}
+                anchorElParent={anchorEl}
+              />
+            </MenuWrapper>
+          </Grid>
+        </BoardsCardFooter>
+      </BountyCardWrapper>
+    </>
+  );
+};
+
+export default function Board({ tasks, handleCardClick = (bounty) => {}, displayOrg = false, Container = Fragment }) {
   return (
     <Container>
       {tasks?.length ? (
-        tasks.map((bounty) => {
-          const hasGR15 = hasGR15DEIIntiative(bounty?.categories);
-          return (
-            <BountyCardWrapper onClick={() => handleCardClick(bounty)} key={bounty.id}>
-              <BoardsCardHeader>
-                <BoardsCardSubheader>
-                  <TaskCardStatus type={bounty?.type} orgId={bounty?.orgId} status={bounty?.status} />
-                  {hasGR15 && (
-                    <>
-                      <GR15DEIModal open={openGR15Modal} onClose={() => setOpenGR15Modal(false)} />
-                      <GR15DEILogo width="28" height="28" onClick={() => setOpenGR15Modal(true)} />
-                    </>
-                  )}
-                  {bounty?.privacyLevel !== PRIVACY_LEVEL.public && (
-                    <ToggleBoardPrivacyIcon
-                      style={{
-                        width: '28px',
-                        height: '28px',
-                        marginRight: '0',
-                      }}
-                      isPrivate={bounty?.privacyLevel !== PRIVACY_LEVEL.public}
-                      tooltipTitle={bounty?.privacyLevel !== PRIVACY_LEVEL.public ? 'Private' : 'Public'}
-                    />
-                  )}
-                </BoardsCardSubheader>
-
-                <Grid container width="fit-content" flexGrow="1" justifyContent="flex-end" gap="6px">
-                  <TaskCardDate date={bounty?.dueDate} />
-                  {bounty?.rewards && bounty?.rewards?.length > 0 && <Compensation rewards={bounty?.rewards} />}
-                </Grid>
-              </BoardsCardHeader>
-              <BoardsCardBody>
-                <BoardsCardBodyTitle>{bounty.title}</BoardsCardBodyTitle>
-                {bounty?.priority ? (
-                  <Box>
-                    <TaskPriority value={bounty?.priority} />
-                  </Box>
-                ) : null}
-                <BoardsCardBodyDescription>
-                  <RichTextViewer text={bounty.description} />
-                </BoardsCardBodyDescription>
-                <SubmissionsCount total={bounty.totalSubmissionsCount} approved={bounty.approvedSubmissionsCount} />
-                {bounty?.media?.[0] && bounty?.media?.[0]?.type === 'image' ? (
-                  <BoardsCardMedia>
-                    <SafeImage
-                      useNextImage={false}
-                      style={{
-                        width: '100%',
-                        height: '100%',
-                        objectFit: 'cover',
-                        objectPosition: 'center',
-                        borderRadius: '6px',
-                      }}
-                      src={bounty?.media[0].slug}
-                    />
-                  </BoardsCardMedia>
-                ) : null}
-              </BoardsCardBody>
-              <BoardsCardFooter>
-                {bounty?.podName && !displayOrg && (
-                  <PodWrapper
-                    style={{ marginTop: '0' }}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
-                      goToPod(bounty?.podId);
-                    }}
-                  >
-                    <PodIcon
-                      color={bounty?.podColor || palette.grey900}
-                      style={{
-                        width: '26px',
-                        height: '26px',
-                      }}
-                    />
-                    <PodName>{bounty?.podName}</PodName>
-                  </PodWrapper>
-                )}
-                {displayOrg && (
-                  <PodWrapper
-                    style={{ marginTop: '0', alignItems: 'center' }}
-                    onClick={(e) => {
-                      e.preventDefault();
-                      e.stopPropagation();
-                      goToOrg(bounty?.orgUsername);
-                    }}
-                  >
-                    {bounty?.orgProfilePicture ? (
-                      <SafeImage
-                        src={bounty.orgProfilePicture}
-                        style={{
-                          width: '26px',
-                          height: '26px',
-                          borderRadius: '4px',
-                          marginRight: '8px',
-                        }}
-                      />
-                    ) : (
-                      <DAOIcon />
-                    )}
-
-                    <PodName>{bounty?.orgName}</PodName>
-                  </PodWrapper>
-                )}
-                <div
-                  style={{
-                    flex: 1,
-                  }}
-                />
-                <Grid item container width="fit-content" gap="12px">
-                  <Grid item container gap="10px" width="fit-content" lineHeight="0" alignItems="center">
-                    <CommentsIcon />
-                    {bounty.commentCount || 0}
-                  </Grid>
-                </Grid>
-              </BoardsCardFooter>
-            </BountyCardWrapper>
-          );
-        })
+        tasks.map((bounty) => (
+          <BountyItem key={bounty?.id} bounty={bounty} handleCardClick={handleCardClick} displayOrg={displayOrg} />
+        ))
       ) : (
         <BountyBoardEmpty />
       )}
--- wondrous-app/components/Common/TaskViewModal/styles.tsx
+++ wondrous-app/components/Common/TaskViewModal/styles.tsx
@@ -251,6 +251,7 @@ export const TaskModalHeaderMenu = styled(Menu)`
 
 export const TaskModalHeaderMenuItem = styled(MenuItem)`
   && {
+    z-index: 1000;
     display: flex;
     justify-content: flex-start;
     color: ${({ theme, warning }) => (warning ? theme.palette.red800 : theme.palette.white)};
