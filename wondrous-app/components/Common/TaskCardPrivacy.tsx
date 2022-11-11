df4d3619741d2ebc407169ebb71ca2b0ab48665a
juniusfree <8164667+juniusfree@users.noreply.github.com>
2022-11-11 08:45:04 +0800

    fix: member icon tooltip

--- wondrous-app/components/Common/TaskCardPrivacy.tsx
+++ wondrous-app/components/Common/TaskCardPrivacy.tsx
@@ -1,29 +1,32 @@
 import Grid from '@mui/material/Grid';
 import GroupIcon from 'components/Icons/Sidebar/group.svg';
+import Tooltip from 'components/Tooltip';
 import palette from 'theme/palette';
 import { PRIVACY_LEVEL } from 'utils/constants';
 
 const TaskCardPrivacy = ({ privacyLevel }) => {
   if (privacyLevel !== PRIVACY_LEVEL.public) {
     return (
-      <Grid
-        container
-        width="28px"
-        height="28px"
-        alignItems="center"
-        justifyContent="center"
-        borderRadius="4px"
-        bgcolor={palette.grey87}
-        sx={[
-          {
-            '& svg': {
-              path: { stroke: palette.blue20 },
+      <Tooltip title="Members Only" placement="top">
+        <Grid
+          container
+          width="28px"
+          height="28px"
+          alignItems="center"
+          justifyContent="center"
+          borderRadius="4px"
+          bgcolor={palette.grey87}
+          sx={[
+            {
+              '& svg': {
+                path: { stroke: palette.blue20 },
+              },
             },
-          },
-        ]}
-      >
-        <GroupIcon />
-      </Grid>
+          ]}
+        >
+          <GroupIcon />
+        </Grid>
+      </Tooltip>
     );
   }
   return null;
