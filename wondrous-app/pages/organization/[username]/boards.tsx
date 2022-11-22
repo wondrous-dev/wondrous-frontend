258cd266f25087f1ca449d035258a6eda4b30950
terryli0095 <terryli0095@gmail.com>
2022-11-21 23:10:23 -0800

    new deply

--- wondrous-app/pages/organization/[username]/boards.tsx
+++ wondrous-app/pages/organization/[username]/boards.tsx
@@ -2,6 +2,7 @@ import MetaTags from 'components/MetaTags';
 import React from 'react';
 
 import BoardSkeleton from 'components/Dashboard/boards/BoardSkeleton';
+
 import lazy from 'utils/enhancements/lazy';
 import apollo from 'services/apollo';
 import { GET_TASK_BY_ID, GET_TASK_PROPOSAL_BY_ID, GET_PREVIEW_FILE } from 'graphql/queries';
