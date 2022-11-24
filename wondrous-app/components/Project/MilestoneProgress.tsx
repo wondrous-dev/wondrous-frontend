147df8cb8ce1298a1d30fe18dbdbeb97e8e5f617
juniusfree <8164667+juniusfree@users.noreply.github.com>
2022-11-24 14:00:02 +0800

    feat: added the listItemMilestone

---
+++ wondrous-app/components/Project/MilestoneProgress.tsx
@@ -0,0 +1,42 @@
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
+import ProgressBar from 'components/Common/ProgressBar';
+import useGetMilestoneTasksProgress from 'hooks/useGetMilestoneTasksProgress';
+import palette from 'theme/palette';
+
+const MilestoneProgress = ({ milestoneId }: { milestoneId: string }) => {
+  const { tasksTotal, tasksCompleted, progress } = useGetMilestoneTasksProgress({ milestoneId });
+  return (
+    <Grid container alignItems="center" justifyContent="flex-end" width="160px" gap="6px">
+      {tasksTotal ? (
+        <>
+          <Grid
+            container
+            item
+            justifyContent="flex-end"
+            fontSize="13px"
+            fontWeight="500"
+            color="#CCBBFF"
+            width="fit-content"
+          >
+            {progress}%
+          </Grid>
+          <Grid item container width="115px">
+            <ProgressBar
+              value={tasksCompleted}
+              total={tasksTotal}
+              height="8px"
+              color={`linear-gradient(269.75deg, ${palette.green30} -19.96%, ${palette.green30} 11.33%, ${palette.violet90} 75.55%)`}
+            />
+          </Grid>
+        </>
+      ) : (
+        <Typography color="#CCBBFF" fontSize="13px" fontWeight="500">
+          No tasks
+        </Typography>
+      )}
+    </Grid>
+  );
+};
+
+export default MilestoneProgress;
