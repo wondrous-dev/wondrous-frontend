0993769f15fbac95e5e2b62942e69e1171534fea
juniusfree <8164667+juniusfree@users.noreply.github.com>
2022-11-24 10:32:10 +0800

    feat: collab modal

--- wondrous-app/components/Project/index.tsx
+++ wondrous-app/components/Project/index.tsx
@@ -1,30 +1,26 @@
 import Grid from '@mui/material/Grid';
-import { CreateEntity } from 'components/CreateEntity';
 import ListGroup from 'components/Project/ListGroup';
 import PodCards from 'components/Project/PodCards';
-import { useState } from 'react';
 import { ProjectContext } from 'utils/contexts';
-import { EntitiesType } from './types';
+import { useCollaborationModal, useCreateEntityModal } from './hooks';
 
 const Project = () => {
-  const [entityType, setEntityType] = useState<EntitiesType>(null);
-  const handleOnCloseCreateEntity = () => setEntityType(null);
+  const { setEntityType, CreateEntityModal } = useCreateEntityModal();
+  const { CollaborationModal, setCollabModal } = useCollaborationModal();
   const projectContextValue = {
     setEntityType,
+    setCollabModal,
   };
   return (
     <ProjectContext.Provider value={projectContextValue}>
-      <CreateEntity
-        open={!!entityType}
-        entityType={entityType}
-        handleClose={handleOnCloseCreateEntity}
-        handleCloseModal={handleOnCloseCreateEntity}
-        cancel={handleOnCloseCreateEntity}
-      />
-      <Grid container flexDirection="column" gap="24px" paddingBottom="24px">
-        <PodCards pods={[]} />
-        <ListGroup />
-      </Grid>
+      <>
+        <CreateEntityModal />
+        <CollaborationModal />
+        <Grid container flexDirection="column" gap="24px" paddingBottom="24px">
+          <PodCards pods={[]} />
+          <ListGroup />
+        </Grid>
+      </>
     </ProjectContext.Provider>
   );
 };
