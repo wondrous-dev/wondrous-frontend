80e6750fb17a28fe0bfc82322b4f80723e151650
juniusfree <8164667+juniusfree@users.noreply.github.com>
2022-11-30 15:03:11 +0800

    fix: truncate text hover color

--- wondrous-app/components/Project/ListItem.tsx
+++ wondrous-app/components/Project/ListItem.tsx
@@ -1,6 +1,33 @@
 import Grid from '@mui/material/Grid';
 import { NextRouter, useRouter } from 'next/router';
 import palette from 'theme/palette';
+import styled from 'styled-components';
+
+const Wrapper = styled(Grid)``;
+
+const LeftComponentWrapper = styled(Grid)`
+  && {
+    align-items: center;
+    width: fit-content;
+    position: relative;
+    flex: 1;
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    &:after {
+      content: '';
+      background: linear-gradient(90deg, transparent 80%, #212121 100%);
+      display: block;
+      inset-block-end: 0;
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      ${Wrapper}:hover & {
+        background: linear-gradient(90deg, transparent 80%, ${palette.grey920} 100%);
+      }
+    }
+  }
+`;
 
 interface IEntityItem {
   LeftComponent: React.ElementType;
@@ -12,7 +39,7 @@ interface IEntityItem {
 const ListItem = ({ LeftComponent, RightComponent = () => null, onClick, data }: IEntityItem) => {
   const router = useRouter();
   return (
-    <Grid
+    <Wrapper
       container
       justifyContent="space-between"
       padding="4px"
@@ -28,34 +55,13 @@ const ListItem = ({ LeftComponent, RightComponent = () => null, onClick, data }:
         },
       }}
     >
-      <Grid
-        container
-        item
-        alignItems="center"
-        width="fit-content"
-        position="relative"
-        sx={{
-          flex: 1,
-          whiteSpace: 'nowrap',
-          overflow: 'hidden',
-          textOverflow: 'ellipsis',
-          '&:after': {
-            content: '""',
-            background: 'linear-gradient(90deg, transparent 80%,  #212121 100%)',
-            display: 'block',
-            insetBlockEnd: 0,
-            position: 'absolute',
-            width: '100%',
-            height: '100%',
-          },
-        }}
-      >
+      <LeftComponentWrapper container item>
         <LeftComponent {...data} />
-      </Grid>
+      </LeftComponentWrapper>
       <Grid container item alignItems="center" justifySelf="flex-end" width="fit-content">
         <RightComponent {...data} />
       </Grid>
-    </Grid>
+    </Wrapper>
   );
 };
 
