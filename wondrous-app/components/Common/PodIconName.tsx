b79360291d80ecf0f0e2f3098d807d24ffa32bd0
juniusfree <8164667+juniusfree@users.noreply.github.com>
2022-12-07 09:01:26 +0800

    fix: text content exceeds container

--- wondrous-app/components/Common/PodIconName.tsx
+++ wondrous-app/components/Common/PodIconName.tsx
@@ -1,7 +1,7 @@
-import { Grid, Typography } from '@mui/material';
+import Grid from '@mui/material/Grid';
+import Typography from '@mui/material/Typography';
 import PodIcon from 'components/Icons/podIcon';
 import Tooltip from 'components/Tooltip';
-import useMediaQuery from 'hooks/useMediaQuery';
 import palette from 'theme/palette';
 
 interface PodIconNameProps {
@@ -11,45 +11,44 @@ interface PodIconNameProps {
   IconComponentProps?: object;
 }
 
-const PodIconName = ({ color = palette.grey900, name, onClick = null, IconComponentProps }: PodIconNameProps) => {
-  const { isDesktopScreen } = useMediaQuery();
-  return (
-    <Tooltip title={name}>
-      <Grid
-        container
-        bgcolor={palette.grey99}
-        borderRadius="64px"
-        padding="2px 8px 2px 2px"
-        width="fit-content"
-        gap="4px"
-        height="28px"
-        onClick={onClick}
-        alignItems="center"
-        sx={{
-          cursor: 'pointer',
+const PodIconName = ({ color = palette.grey900, name, onClick = null, IconComponentProps }: PodIconNameProps) => (
+  <Tooltip title={name}>
+    <Grid
+      container
+      bgcolor={palette.grey99}
+      borderRadius="64px"
+      padding="2px 8px 2px 2px"
+      flex="1"
+      maxWidth="fit-content"
+      gap="4px"
+      height="28px"
+      onClick={onClick}
+      alignItems="center"
+      sx={{
+        cursor: 'pointer',
+      }}
+    >
+      <PodIcon
+        color={color}
+        style={{
+          width: '26px',
+          height: '26px',
         }}
+        {...IconComponentProps}
+      />
+      <Typography
+        fontFamily="Space Grotesk"
+        color={palette.white}
+        fontSize="13px"
+        fontWeight="500"
+        width="fit-content"
+        flex="1"
+        sx={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
       >
-        <PodIcon
-          color={color}
-          style={{
-            width: '26px',
-            height: '26px',
-          }}
-          {...IconComponentProps}
-        />
-        <Typography
-          fontFamily="Space Grotesk"
-          color={palette.white}
-          fontSize="13px"
-          fontWeight="500"
-          maxWidth={isDesktopScreen ? 'fit-content' : '40px'}
-          sx={{ whiteSpace: 'nowrap', overflow: 'hidden', textOverflow: 'ellipsis' }}
-        >
-          {name}
-        </Typography>
-      </Grid>
-    </Tooltip>
-  );
-};
+        {name}
+      </Typography>
+    </Grid>
+  </Tooltip>
+);
 
 export default PodIconName;
