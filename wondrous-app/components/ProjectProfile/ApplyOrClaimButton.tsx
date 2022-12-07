e53d52dde9b63492e859f01fd1475aa27e48d4ce
juniusfree <8164667+juniusfree@users.noreply.github.com>
2022-12-07 08:15:50 +0800

    fix: remove claimed text

--- wondrous-app/components/ProjectProfile/ApplyOrClaimButton.tsx
+++ wondrous-app/components/ProjectProfile/ApplyOrClaimButton.tsx
@@ -1,17 +1,18 @@
 import { useMutation } from '@apollo/client';
-import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
 import { useMe } from 'components/Auth/withAuth';
 import { ButtonPrimary } from 'components/Common/button';
 import { TaskApplicationButton } from 'components/Common/TaskApplication';
 import { UPDATE_TASK_ASSIGNEE } from 'graphql/mutations';
 import { GET_ORG_TASK_BOARD_TASKS } from 'graphql/queries';
+import { useState } from 'react';
 import palette from 'theme/palette';
 import { usePermissions } from 'utils/hooks';
 
 const ApplyOrClaimButton = ({ task }) => {
   const user = useMe();
   const { canClaim, canApply } = usePermissions(task);
+  const [claimed, setClaimed] = useState(false);
   const [updateTaskAssignee] = useMutation(UPDATE_TASK_ASSIGNEE);
   if (!canClaim && canApply) {
     return (
@@ -25,29 +26,14 @@ const ApplyOrClaimButton = ({ task }) => {
       />
     );
   }
-  if (task?.assigneeId) {
-    return (
-      <Grid
-        container
-        bgcolor={palette.black81}
-        padding="0 14px"
-        borderRadius="160px"
-        alignItems="center"
-        color={palette.green30}
-        fontWeight="500"
-        width="fit-content"
-      >
-        Claimed
-      </Grid>
-    );
-  }
 
-  if (canClaim) {
+  if (!claimed && canClaim) {
     return (
       <ButtonPrimary
         onClick={(e) => {
           e.preventDefault();
           e.stopPropagation();
+          setClaimed(true);
           updateTaskAssignee({
             variables: {
               taskId: task?.id,
