c56120a8321477b0851e51a2db483e13edf42977
Junius Free B. Fontamillas <8164667+juniusfree@users.noreply.github.com>
2022-12-13 15:43:38 +0800

    feat: error message when unable to complete task (#1324)
    * feat: error message when unable to complete task
    
    * refactor: remove useSnackbarAlert

--- wondrous-app/components/Common/TaskMenuStatus.tsx
+++ wondrous-app/components/Common/TaskMenuStatus.tsx
@@ -18,13 +18,14 @@ import {
   GET_USER_TASK_BOARD_TASKS,
 } from 'graphql/queries';
 import { GET_TASK_PROPOSAL_BY_ID } from 'graphql/queries/taskProposal';
-import { useState } from 'react';
+import { useContext, useState } from 'react';
 import { ENTITIES_TYPES_FILTER_STATUSES } from 'services/board';
 import styled from 'styled-components';
 import { getProposalStatus } from 'utils/board';
 import { ENTITIES_TYPES, PERMISSIONS, STATUS_APPROVED, STATUS_CLOSED, TASK_STATUS_ARCHIVED } from 'utils/constants';
 import { parseUserPermissionContext } from 'utils/helpers';
 import { useOrgBoard, usePodBoard, useUserBoard } from 'utils/hooks';
+import { SnackbarAlertContext } from './SnackbarAlert';
 
 const TaskStatusMenuWrapper = styled(Menu)`
   && {
@@ -190,11 +191,23 @@ const useTaskMenuStatusProposal = ({ task, entityType }) => {
 const useTaskMenuStatusNonProposal = ({ task, entityType }) => {
   const { id: taskId } = task;
   const { canArchive } = useUserPermission(task);
+  const { setSnackbarAlertOpen, setSnackbarAlertMessage, setSnackbarAlertSeverity } = useContext(SnackbarAlertContext);
   const [archiveTaskMutation] = useMutation(ARCHIVE_TASK, {
     refetchQueries: refetchNonProposalQueries,
   });
   const [updateTaskStatus] = useMutation(UPDATE_TASK_STATUS, {
     refetchQueries: refetchNonProposalQueries,
+    onError: ({ graphQLErrors }) => {
+      if (graphQLErrors[0].extensions.errorCode === 'must_go_through_submission') {
+        setSnackbarAlertSeverity('error');
+        setSnackbarAlertMessage('Cannot complete this task without submission');
+        setSnackbarAlertOpen(true);
+        return;
+      }
+      setSnackbarAlertMessage('Something went wrong');
+      setSnackbarAlertSeverity('error');
+      setSnackbarAlertOpen(true);
+    },
   });
   const handleOnChange = (newStatus) => {
     if (newStatus === TASK_STATUS_ARCHIVED) {
