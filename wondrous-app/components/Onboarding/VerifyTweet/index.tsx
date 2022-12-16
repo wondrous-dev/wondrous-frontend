2ff570785e8000e80c5cc8cce8f093c2f2ecd4da
Terry Li <terryli0095@gmail.com>
2022-12-16 11:17:26 -0800

    reenable promotion tweet (#1354)
    * reenable promotion tweet
    
    * use gcs image

--- wondrous-app/components/Onboarding/VerifyTweet/index.tsx
+++ wondrous-app/components/Onboarding/VerifyTweet/index.tsx
@@ -3,8 +3,6 @@ import { useRouter } from 'next/router';
 import { useQuery, useMutation } from '@apollo/client';
 import { GET_LOGGED_IN_USER } from 'graphql/queries';
 import { VERIFY_USER_TWEET } from 'graphql/mutations';
-import OnboardingLayout from 'components/Onboarding/OnboardingLayout';
-import { CompletedIcon } from 'components/Icons/statusIcons';
 import {
   ContinueButton,
   LaterButton,
@@ -17,6 +15,7 @@ import { useWonderWeb3 } from 'services/web3';
 import useEagerConnectConditional from 'services/web3/hooks/useEagerConnectConditional';
 import TwitterLogo from '../../../public/images/twitter.svg';
 import { InviteWelcomeBoxParagraph } from '../styles';
+import { PlayerWrapper } from './styles';
 import { useMe } from '../../Auth/withAuth';
 
 // const buttonStyle = {
@@ -35,7 +34,7 @@ function VerifyTweet({ firstOrg, firstPod }) {
   const { data: userData } = useQuery(GET_LOGGED_IN_USER, {
     fetchPolicy: 'network-only',
     onCompleted: (data) => {
-      if (data?.getLoggedinUser?.userInfo && !userData?.getLoggedinUser?.userInfo?.twitterUsername) {
+      if (data?.getLoggedinUser?.userInfo && !data?.getLoggedinUser?.userInfo?.twitterUsername) {
         router.push(`/onboarding/twitter`, undefined, {
           shallow: true,
           ...(collabInvite ? { query: { collabInvite } } : {}),
@@ -71,8 +70,8 @@ function VerifyTweet({ firstOrg, firstPod }) {
     }
   }, [userAlreadyTweeted]);
 
-  const hanldeLaterClick = () => {
-    // if user is part of a org maybe redirect to org
+  const handleLaterClick = () => {
+    // if user has collabInvite token and is not a member of an org we assume they want to create a new org
     if (collabInvite && !firstOrg) {
       return router.push(`/onboarding-dao?collabInvite=${collabInvite}`, undefined, {
         shallow: true,
@@ -86,9 +85,15 @@ function VerifyTweet({ firstOrg, firstPod }) {
         shallow: true,
       });
     } else if (firstOrg) {
-      router.push(`/organization/${firstOrg.username}/home`, undefined, {
-        shallow: true,
-      });
+      router.push(
+        `/${firstOrg?.shared ? 'collaboration' : 'organization'}/${firstOrg.username}/${
+          firstOrg?.shared ? 'boards' : 'home'
+        }`,
+        undefined,
+        {
+          shallow: true,
+        }
+      );
     } else {
       router.push('/mission-control', undefined, {
         shallow: true,
@@ -100,11 +105,11 @@ function VerifyTweet({ firstOrg, firstPod }) {
     if (user?.activeEthAddress) {
       if (wonderWeb3.ensName) {
         // && wonderWeb3.address === user?.activeEthAddress ?
-        return `https://twitter.com/intent/tweet?text=gm%20-%20I%E2%80%99m%20reserving%20my%20Launch%20Pass%20NFT%20as%20a%20contributor%20%40wonderverse_xyz%20%E2%9C%A8%0AENS%3A%20${wonderWeb3.ensName}&in_reply_to=1536797296263737345`;
+        return `https://twitter.com/intent/tweet?text=gm%20-%20I%E2%80%99m%20reserving%20my%20Orbit%201%20NFT%20as%20a%20contributor%20%40wonderverse_xyz%20%E2%9C%A8%0AENS%3A%20${wonderWeb3.ensName}&in_reply_to=1536797296263737345`;
       }
-      return `https://twitter.com/intent/tweet?text=gm%20-%20I%E2%80%99m%20reserving%20my%20Launch%20Pass%20NFT%20as%20a%20contributor%20%40wonderverse_xyz%20%E2%9C%A8%0A${user?.activeEthAddress}&in_reply_to=1536797296263737345`;
+      return `https://twitter.com/intent/tweet?text=gm%20-%20I%E2%80%99m%20reserving%20my%20Orbit%201%20NFT%20as%20a%20contributor%20%40wonderverse_xyz%20%E2%9C%A8%0A${user?.activeEthAddress}&in_reply_to=1536797296263737345`;
     }
-    return `https://twitter.com/intent/tweet?text=gm%20-%20I%E2%80%99m%20reserving%20my%20Launch%20Pass%20NFT%20as%20a%20contributor%20%40wonderverse_xyz%20%E2%9C%A8%0A&in_reply_to=1536797296263737345`;
+    return `https://twitter.com/intent/tweet?text=gm%20-%20I%E2%80%99m%20reserving%20my%20Orbit%201%20NFT%20as%20a%20contributor%20%40wonderverse_xyz%20%E2%9C%A8%0A&in_reply_to=1536797296263737345`;
   };
 
   return (
@@ -115,23 +120,26 @@ function VerifyTweet({ firstOrg, firstPod }) {
         minHeight: 'unset',
       }}
     >
-      <OnboardingTitle
-        style={{
-          marginTop: 0,
-        }}
-      >
-        Get your Launch Pass NFT
-      </OnboardingTitle>
-      <InviteWelcomeBoxParagraph
-        style={{
-          textAlign: 'left',
-          width: '100%',
-          fontSize: '15px',
-          fontWeight: '400',
-        }}
-      >
-        Want to get your launch NFT? <br /> Verify your Twitter to get the password and link.
-      </InviteWelcomeBoxParagraph>
+      <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
+        <OnboardingTitle
+          style={{
+            marginTop: 0,
+            textAlign: 'center',
+          }}
+        >
+          Get your Orbit 1 NFT
+        </OnboardingTitle>
+        <InviteWelcomeBoxParagraph
+          style={{
+            textAlign: 'center',
+            width: '100%',
+            fontSize: '15px',
+            fontWeight: '400',
+          }}
+        >
+          Want to get your Orbit 1 NFT? Tweet us!
+        </InviteWelcomeBoxParagraph>
+      </div>
       {!user?.activeEthAddress && (
         <InviteWelcomeBoxParagraph
           style={{
@@ -151,7 +159,19 @@ function VerifyTweet({ firstOrg, firstPod }) {
           alignItems: 'center',
         }}
       >
-        <Image alt="Background" src="/images/poap/launch-poap.png" quality={100} width={300} height={300} />
+        <PlayerWrapper>
+          <Image alt="Background" src="https://storage.googleapis.com/public-wonder/thisis.png" quality={100} width={300} height={300} />
+
+          {/* <StyledMuxPlayer
+            autoPlay="muted"
+            loop
+            streamType="on-demand"
+            playbackId="TrJJODUH400xzbi00R1t2Pq5ik00zA7MwQ9y59NyU2Ailo"
+            metadata={{
+              video_title: 'Orbit 1 NFT',
+            }}
+          /> */}
+        </PlayerWrapper>
         {!tweetVerified && (
           <div
             style={{
@@ -218,7 +238,7 @@ function VerifyTweet({ firstOrg, firstPod }) {
                 width: '100%',
                 height: '50px',
               }}
-              onClick={hanldeLaterClick}
+              onClick={handleLaterClick}
             >
               I’ll get it later
             </Later>
@@ -246,7 +266,7 @@ function VerifyTweet({ firstOrg, firstPod }) {
                 width: '100%',
                 height: '50px',
               }}
-              onClick={hanldeLaterClick}
+              onClick={handleLaterClick}
             >
               Continue to dashboard
             </ContinueButton>
