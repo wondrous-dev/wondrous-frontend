9edf2bc4fd641efad3e9e1d42f0db19828cb8683
Adrian <mustea.adrian95@gmail.com>
2022-12-19 22:00:34 +0200

    fix mobile visibility (#1376)

--- wondrous-app/components/Common/SidebarEntity/index.tsx
+++ wondrous-app/components/Common/SidebarEntity/index.tsx
@@ -35,7 +35,7 @@ const EntitySidebarButtons = () => {
 
   if (!canManage) {
     return (
-      <ButtonsContainer>
+      <ButtonsContainer minimized={minimized}>
         <JoinWorkspace />
       </ButtonsContainer>
     );
