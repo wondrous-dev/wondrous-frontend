c92306fbe4150f11d4ce4d4a2c062921c2942cee
Adrian Mustea <mustea.adrian95@gmail.com>
2022-12-22 10:15:49 +0200

    wip: project page dnd

--- wondrous-app/components/ProjectProfile/ProfileSectionsWrapper.tsx
+++ wondrous-app/components/ProjectProfile/ProfileSectionsWrapper.tsx
@@ -1,4 +1,7 @@
 import Grid from '@mui/material/Grid';
+import StrictModeDroppable from 'components/StrictModeDroppable';
+
+import { DragDropContext, Draggable } from 'react-beautiful-dnd';
 
 import ProfileBountySection from './ProfileBountySection';
 import ProfileCategorySection from './ProfileCategorySection';
@@ -9,26 +12,57 @@ import ProfileMilestoneSection from './ProfileMilestoneSection';
 import ProfileProposalSection from './ProfileProposalSection';
 import ProfileTaskSection from './ProfileTaskSection';
 
-const ProfileSectionsWrapper = () => (
-  <Grid
-    container
-    justifyContent="space-between"
-    gap="24px"
-    sx={{
-      '& > *': {
-        maxWidth: 'calc(50% - 12px)',
-      },
-    }}
-  >
-    <ProfileTaskSection />
-    <ProfileBountySection />
-    <ProfileMilestoneSection />
-    <ProfileProposalSection />
-    <ProfileMemberSection />
-    <ProfileCollabSection />
-    <ProfileGrantSection />
-    <ProfileCategorySection />
-  </Grid>
-);
+const ProfileSectionsWrapper = () => {
+  const onDragEnd = () => console.log('on drag end');
+
+  const onDragStart = () => console.log('on drag start');
+
+  const Components = [
+    ProfileTaskSection,
+    ProfileBountySection,
+    ProfileMilestoneSection,
+    ProfileProposalSection,
+    ProfileMemberSection,
+    ProfileCollabSection,
+    ProfileGrantSection,
+    ProfileCategorySection,
+  ];
+  return (
+    <DragDropContext onDragEnd={onDragEnd} onDragStart={onDragStart}>
+      <StrictModeDroppable droppableId="droppableId">
+        {(provided) => (
+          <Grid
+            container
+            justifyContent="space-between"
+            gap="24px"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            sx={{
+              '& > *': {
+                maxWidth: 'calc(50% - 12px)',
+              },
+            }}
+          >
+            {Components.map((Component, index) => (
+              <Draggable key={index} draggableId={`${index}`} index={index}>
+                {(provided, snapshot) => (
+                  <div
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                    ref={provided.innerRef}
+                    isDragging={snapshot.isDragging}
+                  >
+                    <Component key={index} />
+                  </div>
+                )}
+              </Draggable>
+            ))}
+            {provided.placeholder}
+          </Grid>
+        )}
+      </StrictModeDroppable>
+    </DragDropContext>
+  );
+};
 
 export default ProfileSectionsWrapper;
