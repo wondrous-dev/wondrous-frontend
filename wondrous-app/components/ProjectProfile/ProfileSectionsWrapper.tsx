71ecdf533d8d7f9f89870f2825dbf9a979963cba
Adrian <mustea.adrian95@gmail.com>
2022-12-23 20:20:55 +0200

    Feat/project page dnd (#1388)
    * wip: project page dnd
    
    * added dnd to org project page

--- wondrous-app/components/ProjectProfile/ProfileSectionsWrapper.tsx
+++ wondrous-app/components/ProjectProfile/ProfileSectionsWrapper.tsx
@@ -1,5 +1,10 @@
+import { useMutation } from '@apollo/client';
 import Grid from '@mui/material/Grid';
+import StrictModeDroppable from 'components/StrictModeDroppable';
+import { UPSERT_ORG_PROFILE_PAGE, UPSERT_POD_PROFILE_PAGE } from 'graphql/mutations';
+import { DragDropContext, Draggable } from 'react-beautiful-dnd';
 
+import palette from 'theme/palette';
 import ProfileBountySection from './ProfileBountySection';
 import ProfileCategorySection from './ProfileCategorySection';
 import ProfileCollabSection from './ProfileCollabSection';
@@ -9,26 +14,93 @@ import ProfileMilestoneSection from './ProfileMilestoneSection';
 import ProfileProposalSection from './ProfileProposalSection';
 import ProfileTaskSection from './ProfileTaskSection';
 
-const ProfileSectionsWrapper = () => (
-  <Grid
-    container
-    justifyContent="space-between"
-    gap="24px"
-    sx={{
-      '& > *': {
-        maxWidth: 'calc(50% - 12px)',
-      },
-    }}
-  >
-    <ProfileTaskSection />
-    <ProfileBountySection />
-    <ProfileMilestoneSection />
-    <ProfileProposalSection />
-    <ProfileMemberSection />
-    <ProfileCollabSection />
-    <ProfileGrantSection />
-    <ProfileCategorySection />
-  </Grid>
-);
+const ProfileSectionsWrapper = ({ layout, orgId }) => {
+  const [upsertOrgProfilePage] = useMutation(UPSERT_ORG_PROFILE_PAGE, {
+    refetchQueries: ['getOrgFromUsername'],
+  });
+
+  // TODO: Not used yet
+  // const [upsertPodProfilePage] = useMutation(UPSERT_POD_PROFILE_PAGE);
+
+  const upsert = (newOrder) => {
+    if (orgId) {
+      upsertOrgProfilePage({
+        variables: {
+          input: {
+            orgId,
+            layout: newOrder,
+          },
+        },
+      });
+    }
+  };
+
+  const onDragEnd = (result) => {
+    const orderClone = [...layout];
+    const [removed] = orderClone.splice(result.source.index, 1);
+    orderClone.splice(result.destination.index, 0, removed);
+    return upsert(orderClone);
+  };
+
+  if (!layout) return null;
+
+  const Components = {
+    task: ProfileTaskSection,
+    bounty: ProfileBountySection,
+    milestone: ProfileMilestoneSection,
+    proposal: ProfileProposalSection,
+    member: ProfileMemberSection,
+    collab: ProfileCollabSection,
+    grant: ProfileGrantSection,
+    resource: ProfileCategorySection,
+  };
+
+  return (
+    <DragDropContext onDragEnd={onDragEnd}>
+      <StrictModeDroppable droppableId="droppableId">
+        {(provided) => (
+          <Grid
+            container
+            justifyContent="space-between"
+            gap="24px"
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            sx={{
+              '& > *': {
+                maxWidth: 'calc(50% - 12px)',
+              },
+            }}
+          >
+            {layout?.map((order, index) => {
+              const Component = Components[order];
+              return (
+                <Draggable key={index} draggableId={`${index}`} index={index}>
+                  {(provided, snapshot) => (
+                    <Grid
+                      container
+                      item
+                      flexDirection="column"
+                      justifyContent="space-between"
+                      height="390px"
+                      bgcolor={palette.grey900}
+                      borderRadius="6px"
+                      {...provided.draggableProps}
+                      {...provided.dragHandleProps}
+                      ref={provided.innerRef}
+                      isDragging={snapshot.isDragging}
+                    >
+                      <Component key={index} />
+                    </Grid>
+                  )}
+                </Draggable>
+              );
+            })}
+            {provided.placeholder}
+          </Grid>
+        )}
+      </StrictModeDroppable>
+    </DragDropContext>
+  );
+};
 
 export default ProfileSectionsWrapper;
