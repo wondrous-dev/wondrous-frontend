bbec799e6d39a6fd8a572392e941ef30f5628729
terryli0095 <terryli0095@gmail.com>
2022-12-29 20:47:48 -0800

    change icon

--- wondrous-app/utils/web3Constants.tsx
+++ wondrous-app/utils/web3Constants.tsx
@@ -7,7 +7,7 @@ import Optimism from 'components/Icons/Optimism';
 import Klaytn from 'components/Icons/Klaytn';
 import Polygon from 'components/Icons/polygonMaticLogo.svg';
 import Harmony from 'components/Icons/harmony';
-import { GnosisWalletTypeIcon } from 'components/Icons/walletSetupModalIcons';
+import GnosisChain from 'components/Icons/GnosisChain';
 import { Matic } from 'components/Icons/matic';
 import { USDCoin } from 'components/Icons/USDCoin';
 import { WonderCoin } from 'components/Icons/wonderCoin';
@@ -191,7 +191,7 @@ export const CHAIN_LOGO = {
   '42161': <Arbitrum />,
   '43114': <Avalanche />,
   '1666600000': <Harmony />,
-  '100': <GnosisWalletTypeIcon />,
+  '100': <GnosisChain />,
 };
 
 export const CURRENCY_SYMBOL = {
