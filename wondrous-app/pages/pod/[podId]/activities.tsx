cb566604cf516da05a1639b5d5f84e65db450fad
Adrian Mustea <mustea.adrian95@gmail.com>
2023-01-09 22:34:18 +0200

    breadcrumbs fixes

--- wondrous-app/pages/pod/[podId]/activities.tsx
+++ wondrous-app/pages/pod/[podId]/activities.tsx
@@ -1,27 +1,28 @@
 import { useQuery } from '@apollo/client';
 import { useRouter } from 'next/router';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { withAuth } from 'components/Auth/withAuth';
 import Activities from 'components/Pod/activities';
-import { GET_USER_PERMISSION_CONTEXT } from 'graphql/queries';
+import { GET_POD_BY_ID, GET_USER_PERMISSION_CONTEXT } from 'graphql/queries';
 import { PodBoardContext } from 'utils/contexts';
-import { useGetPodById } from 'utils/hooks';
+import { useGetPodById, usePageDataContext, usePodPageFetch } from 'utils/hooks';
 import EntitySidebar from 'components/Common/SidebarEntity';
 
 function ActivitiesPage() {
   const router = useRouter();
   const { podId } = router.query;
-  const getPodById = useGetPodById(podId);
   const { data: userPermissionsContext } = useQuery(GET_USER_PERMISSION_CONTEXT, {
     fetchPolicy: 'cache-and-network',
   });
 
+  const { data } = usePodPageFetch(podId);
+
   return (
     <PodBoardContext.Provider
       value={{
-        pod: getPodById,
+        pod: data?.getPodById,
         podId,
-        orgId: getPodById?.orgId,
+        orgId: data?.getPodById?.orgId,
         userPermissionsContext: userPermissionsContext?.getUserPermissionContext
           ? JSON.parse(userPermissionsContext?.getUserPermissionContext)
           : null,
--- wondrous-app/pages/pod/[podId]/docs.tsx
+++ wondrous-app/pages/pod/[podId]/docs.tsx
@@ -1,33 +1,33 @@
-import { useLazyQuery, useQuery } from '@apollo/client';
+import { useQuery } from '@apollo/client';
+import { withAuth } from 'components/Auth/withAuth';
+import Docs from 'components/Pod/docs/docs';
+import { GET_POD_BY_ID, GET_USER_PERMISSION_CONTEXT } from 'graphql/queries';
 import { useRouter } from 'next/router';
 import React, { useEffect } from 'react';
-import { withAuth } from 'components/Auth/withAuth';
-import { GET_POD_BY_ID, GET_USER_PERMISSION_CONTEXT } from 'graphql/queries';
-import Docs from 'components/Pod/docs/docs';
 import { PodBoardContext } from 'utils/contexts';
-import { useGetPodById } from 'utils/hooks';
+import { useGetPodById, usePageDataContext, usePodPageFetch } from 'utils/hooks';
 
 function DocsPage() {
   const router = useRouter();
   const { podId } = router.query;
-
   const { data: userPermissionsContext } = useQuery(GET_USER_PERMISSION_CONTEXT, {
     fetchPolicy: 'cache-and-network',
   });
 
-  const pod = useGetPodById(podId);
+  const { data } = usePodPageFetch(podId);
+
   return (
     <PodBoardContext.Provider
       value={{
-        pod,
+        pod: data?.getPodById,
         podId,
         userPermissionsContext: userPermissionsContext?.getUserPermissionContext
           ? JSON.parse(userPermissionsContext?.getUserPermissionContext)
           : null,
-        orgId: pod?.orgId,
+        orgId: data?.getPodById?.orgId,
       }}
     >
-      <Docs podData={pod} />
+      <Docs podData={data?.getPodById} />
     </PodBoardContext.Provider>
   );
 }
