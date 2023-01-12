3d661eded9491b972810d7112cf5ac33f4ef5574
terryli0095 <terryli0095@gmail.com>
2023-01-11 20:38:00 -0800

    improve local speed

--- wondrous-app/next.config.js
+++ wondrous-app/next.config.js
@@ -20,6 +20,23 @@ module.exports = withBundleAnalyzer({
       'images.mintkudos.xyz',
     ],
   },
+  experimental: {
+    modularizeImports: {
+      lodash: {
+        transform: 'lodash/{{member}}',
+      },
+      '@mui/material': {
+        transform: '@mui/material/{{member}}',
+      },
+      '@mui/lab': {
+        transform: '@mui/lab/{{member}}',
+      },
+      '@mui/icons-material/?(((\\w*)?/?)*)': {
+        transform: '@mui/icons-material/{{ matches.[1] }}/{{member}}',
+      },
+    },
+  },
+
   webpack(config) {
     config.module.rules.push({
       test: /\.svg$/,
