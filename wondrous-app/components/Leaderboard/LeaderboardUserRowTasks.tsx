a241b87a2a2792b8c5ca69cbe70a187575d04756
juniusfree <8164667+juniusfree@users.noreply.github.com>
2023-02-23 19:42:45 +0800

    refactor: updated designs

--- wondrous-app/components/Leaderboard/LeaderboardUserRowTasks.tsx
+++ wondrous-app/components/Leaderboard/LeaderboardUserRowTasks.tsx
@@ -1,114 +1,86 @@
-import { Divider } from '@mui/material';
+import { Button } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import Typography from '@mui/material/Typography';
-import { Button } from 'components/Button';
 import PodIconName from 'components/Common/PodIconName';
-import CalendarIcon from 'components/Icons/calendar';
-import CheckBoxIcon from 'components/Icons/Sidebar/checkBox.svg';
-import StarIcon from 'components/Icons/Sidebar/star.svg';
 import format from 'date-fns/format';
 import { useRouter } from 'next/router';
 import palette from 'theme/palette';
-import { BOUNTY_TYPE, PRIVATE_TASK_TITLE } from 'utils/constants';
+import { PRIVATE_TASK_TITLE } from 'utils/constants';
 import { cutString } from 'utils/helpers';
 
 const LeaderboardUserRowTasks = ({ contributorTask }) => {
   const router = useRouter();
   return (
-    <Grid
-      container
-      item
-      width="100%"
-      display="flex"
-      borderRadius="0 0 6px 6px"
-      padding="12px 8px"
-      gap="20px"
-      bgcolor={palette.grey940}
-    >
-      {contributorTask?.tasks?.map((task) => {
-        const podName = task?.podName || task?.pod?.name;
-        const podColor = task?.podColor || task?.podColor;
-        const privateTask = task?.title === PRIVATE_TASK_TITLE;
-        const handleOnClickTask = (e) => {
-          e.preventDefault();
-          e.stopPropagation();
-          if (!privateTask) {
-            router.push({
-              pathname: router.pathname,
-              query: {
-                ...router.query,
-                task: task.id,
-              },
-            });
-          }
-        };
-        return (
-          <Grid
-            container
-            item
-            alignItems="center"
-            justifyContent="space-between"
-            key={task?.id}
-            sx={{
-              cursor: !privateTask && 'pointer',
-            }}
-            onClick={handleOnClickTask}
-          >
-            <Grid container item alignItems="center" gap="14px" width="fit-content">
-              <Grid
-                container
-                item
-                bgcolor={palette.grey87}
-                width="22px"
-                height="22px"
-                borderRadius="4px"
-                alignItems="center"
-                justifyContent="center"
-                sx={{
-                  '& svg': {
-                    path: {
-                      stroke: palette.blue20,
-                    },
-                    rect: {
-                      stroke: palette.blue20,
-                    },
-                  },
-                }}
-              >
-                {task.type === BOUNTY_TYPE ? <StarIcon /> : <CheckBoxIcon />}
+    <Grid container item width="100%" borderRadius="0 0 6px 6px">
+      <Grid container item width="100%" display="flex" gap="14px" padding="14px">
+        {contributorTask?.tasks?.map((task) => {
+          const podName = task?.podName || task?.pod?.name;
+          const podColor = task?.podColor || task?.podColor;
+          const privateTask = task?.title === PRIVATE_TASK_TITLE;
+          const handleOnClickTask = (e) => {
+            e.preventDefault();
+            e.stopPropagation();
+            if (!privateTask) {
+              router.push({
+                pathname: router.pathname,
+                query: {
+                  ...router.query,
+                  task: task.id,
+                },
+              });
+            }
+          };
+          return (
+            <Grid
+              container
+              item
+              borderRadius="4px"
+              padding="4px 8px"
+              height="36px"
+              alignItems="center"
+              justifyContent="space-between"
+              key={task?.id}
+              sx={{
+                cursor: !privateTask && 'pointer',
+                '&:hover': {
+                  backgroundColor: palette.grey87,
+                },
+              }}
+              onClick={handleOnClickTask}
+            >
+              <Grid container item alignItems="center" gap="14px" width="fit-content">
+                <Grid container item alignItems="center" justifyContent="center" width="fit-content">
+                  <Typography color={palette.white} fontSize="15px" fontWeight="500">
+                    {cutString(privateTask ? 'Private Task' : task.title)}
+                  </Typography>
+                </Grid>
               </Grid>
-              <Grid container item alignItems="center" justifyContent="center" width="fit-content">
-                <Typography color={palette.white} fontSize="15px" fontWeight="500">
-                  {cutString(privateTask ? 'Private Task' : task.title)}
-                </Typography>
+              <Grid container item width="fit-content" gap="12px" alignItems="center">
+                {podName && <PodIconName color={podColor} name={podName} />}
+                <Grid container item alignItems="center" width="fit-content" gap="6px">
+                  <Typography color={palette.white} fontWeight="500" fontSize="15px">
+                    {format(new Date(task?.completedAt), 'MMM dd')}
+                  </Typography>
+                </Grid>
               </Grid>
             </Grid>
-            <Grid container item width="fit-content" gap="12px" alignItems="center">
-              {podName && <PodIconName color={podColor} name={podName} />}
-              <Grid container item alignItems="center" width="fit-content" gap="6px">
-                <CalendarIcon />
-                <Typography color={palette.white} fontWeight="500" fontSize="15px">
-                  {format(new Date(task?.completedAt), 'MMM dd')}
-                </Typography>
-              </Grid>
-            </Grid>
-          </Grid>
-        );
-      })}
-      <Divider
-        sx={{
-          backgroundColor: '#363636',
-          width: '100%',
-        }}
-      />
+          );
+        })}
+      </Grid>
       <Button
-        color="grey"
-        buttonTheme={{
-          paddingX: 16,
-          width: 'fit-content',
-          fontWeight: '500',
-          background: palette.grey78,
+        sx={{
+          backgroundColor: palette.grey85,
+          borderRadius: '0 0 6px 6px',
+          width: '100%',
+          height: '40px',
+          color: palette.grey250,
+          fontWeight: 500,
+          fontSize: 14,
+          '&:hover': {
+            backgroundColor: palette.grey78,
+          },
         }}
+        disableRipple
       >
         Show more
       </Button>
