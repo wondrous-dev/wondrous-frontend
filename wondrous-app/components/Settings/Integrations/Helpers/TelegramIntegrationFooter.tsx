5c55c04cea104e9af9570dcd04a55b94373488bc
Adrian Mustea <mustea.adrian95@gmail.com>
2023-03-23 12:22:21 -0400

    fix tg links

--- wondrous-app/components/Settings/Integrations/Helpers/TelegramIntegrationFooter.tsx
+++ wondrous-app/components/Settings/Integrations/Helpers/TelegramIntegrationFooter.tsx
@@ -1,11 +1,12 @@
 import { useContext } from 'react';
+import { getTelegramBotLink } from 'utils/index';
 import ConnectionContext from './ConnectionContext';
 import FooterButtons from './FooterButtons';
 
 const TelegramIntegrationFooter = () => {
   const { onClose, data } = useContext(ConnectionContext);
-
-  const handleOnClick = () => window.open('https://t.me/wonderverse_bot', '_blank');
+  const tgLink = getTelegramBotLink();
+  const handleOnClick = () => window.open(tgLink, '_blank');
   return <FooterButtons title="Connect Telegram" onClose={onClose} action={handleOnClick} />;
 };
 
