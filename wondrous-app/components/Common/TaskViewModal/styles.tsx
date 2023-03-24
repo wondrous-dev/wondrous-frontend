d8f26a9e4c658ee407ab9fb67b03898e0aa0cdf3
juniusfree <8164667+juniusfree@users.noreply.github.com>
2023-03-24 14:47:07 +0800

    fix: calendar spacing error

--- wondrous-app/components/Common/TaskViewModal/styles.tsx
+++ wondrous-app/components/Common/TaskViewModal/styles.tsx
@@ -427,7 +427,6 @@ export const TaskSectionDisplayData = styled.div`
   display: flex;
   flex-direction: column;
   gap: 12px;
-  line-height: 0;
 `;
 
 export const TaskSectionDisplayDiv = styled.div`
