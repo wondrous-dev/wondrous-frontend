17087e8dd2a41b5b1bbc2d9b105d7a7455068414
adrian mustea <mustea.adrian95@gmail.com>
2023-05-04 11:50:04 +0300

    env vars

--- wondrous-bot-admin/vite.config.ts
+++ wondrous-bot-admin/vite.config.ts
@@ -1,10 +1,9 @@
-import { defineConfig } from 'vite';
+import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 import tsconfigPaths from 'vite-tsconfig-paths';
 import { NodeModulesPolyfillPlugin } from '@esbuild-plugins/node-modules-polyfill';
 import { NodeGlobalsPolyfillPlugin } from '@esbuild-plugins/node-globals-polyfill';
 import rollupNodePolyFill from 'rollup-plugin-node-polyfills';
-
 
 export default defineConfig({
   plugins: [react(), tsconfigPaths()],
@@ -16,6 +15,8 @@ export default defineConfig({
   },
   define: {
     'window.global': {},
+    __APP_ENV__: process.env.VITE_VERCEL_ENV,
+    VITE_GRAPHQL_SERVER_URL: process.env.VITE_GRAPHQL_SERVER_URL,
   },
   resolve: {
     alias: {
