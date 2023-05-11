b0faba231d57025c0455505f484e72e24c064a54
Adrian <mustea.adrian95@gmail.com>
2023-05-11 10:37:46 +0300

    fix double login (#1701)
    * fix double login
    
    * fix borders on subs

--- wondrous-bot-admin/src/components/Auth/index.tsx
+++ wondrous-bot-admin/src/components/Auth/index.tsx
@@ -19,7 +19,7 @@ import {
 import { useLocation, useNavigate } from "react-router-dom"
 
 const MyContext = React.createContext(null)
-const EXCLUDED_PATHS = ["/login"]
+export const EXCLUDED_PATHS = ["/login"]
 
 export const useMe = () => useContext(MyContext)
 
