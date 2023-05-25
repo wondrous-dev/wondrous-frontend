e8f4ee3f720a3d8046246e007aafe41b8315579a
Andros <wong22@hotmail.co.uk>
2023-05-25 12:48:20 -0700

    fix: discord display

--- wondrous-bot-admin/src/components/TableComponent/index.tsx
+++ wondrous-bot-admin/src/components/TableComponent/index.tsx
@@ -1,24 +1,10 @@
-import { Twitter } from '@mui/icons-material';
-import {
-  TableContainer,
-  Table,
-  TableHead,
-  TableBody,
-  TableCell,
-  Typography,
-  Box,
-} from '@mui/material';
-import EditableText from 'components/EditableText';
-import { ShapedHexagonWrapper, WhiteBgDiscord } from 'components/Icons/Discord';
-import { Label } from 'components/QuestsList/styles';
-import { StyledViewQuestResults } from 'components/ViewQuestResults/styles';
-import {
-  IconWrapper,
-  PaperComponent,
-  StyledTableHeader,
-  StyledTableHeaderCell,
-  StyledTableRow,
-} from './styles';
+import { Twitter } from "@mui/icons-material";
+import { TableContainer, Table, TableHead, TableBody, TableCell, Typography, Box } from "@mui/material";
+import EditableText from "components/EditableText";
+import { ShapedHexagonWrapper, WhiteBgDiscord } from "components/Icons/Discord";
+import { Label } from "components/QuestsList/styles";
+import { StyledViewQuestResults } from "components/ViewQuestResults/styles";
+import { IconWrapper, PaperComponent, StyledTableHeader, StyledTableHeaderCell, StyledTableRow } from "./styles";
 
 const TableComponent = ({ headers, data }) => {
   return (
@@ -27,9 +13,7 @@ const TableComponent = ({ headers, data }) => {
         <TableHead>
           <StyledTableHeader>
             {headers?.map((header) => (
-              <StyledTableHeaderCell key={header}>
-                {header}
-              </StyledTableHeaderCell>
+              <StyledTableHeaderCell key={header}>{header}</StyledTableHeaderCell>
             ))}
           </StyledTableHeader>
         </TableHead>
@@ -37,112 +21,91 @@ const TableComponent = ({ headers, data }) => {
           {data?.map((row, idx) => (
             <StyledTableRow key={row.id} id={row.id}>
               {Object.keys(row)?.map((key) => {
-                if (key === 'id') return null;
+                if (key === "id") return null;
                 const column = row[key];
                 return (
-                  <TableCell key={key} sx={{
-                    borderRight: '2px solid #f2f2f2',
-                    borderBottom: '0px'
-                  }}>
-                    {column.component === 'label' ? (
+                  <TableCell
+                    key={key}
+                    sx={{
+                      borderRight: "2px solid #f2f2f2",
+                      borderBottom: "0px",
+                    }}
+                  >
+                    {column.component === "label" ? (
                       <Label
-                        fontSize='14px'
-                        lineHeight='14px'
-                        textAlign='center'
-                        width='100%'
+                        fontSize="14px"
+                        lineHeight="14px"
+                        textAlign="center"
+                        width="100%"
                         {...(column.componentProps || {})}
                       >
                         {column.value}
                       </Label>
                     ) : null}
-                    {column.component === 'hexagon' ? (
+                    {column.component === "hexagon" ? (
                       <Box
-                        display='flex'
-                        gap='10px'
-                        alignItems='center'
-                        position='relative'
-                        justifyContent='center'
+                        display="flex"
+                        gap="10px"
+                        alignItems="center"
+                        position="relative"
+                        justifyContent="center"
                         sx={{
-                          '&:hover #display-edit-icon': {
-                            visibility: 'visible',
-                          }
+                          "&:hover #display-edit-icon": {
+                            visibility: "visible",
+                          },
                         }}
                       >
                         <Box
-                          position='relative'
-                          width='fit-content'
-                          display='flex'
-                          justifyContent='center'
-                          alignItems='center'
+                          position="relative"
+                          width="fit-content"
+                          display="flex"
+                          justifyContent="center"
+                          alignItems="center"
                         >
                           <ShapedHexagonWrapper />
                           <Typography
-                            fontFamily='Poppins'
+                            fontFamily="Poppins"
                             fontWeight={700}
-                            fontSize='13px'
-                            lineHeight='17px'
-                            position='absolute'
-                            color='white'
+                            fontSize="13px"
+                            lineHeight="17px"
+                            position="absolute"
+                            color="white"
                           >
-                            {column.value === 'index' ? idx + 1 : column.value}
+                            {column.value === "index" ? idx + 1 : column.value}
                           </Typography>
                         </Box>
-                        {column.label ? (
-                          <EditableText
-                            value={column.label}
-                            {...column.labelProps}
-                          />
-                        ) : null}
+                        {column.label ? <EditableText value={column.label} {...column.labelProps} /> : null}
                       </Box>
                     ) : null}
-                    {column.component === 'discord' ? (
-                      <Box
-                        display='flex'
-                        justifyContent='center'
-                        alignItems='center'
-                      >
-                        <IconWrapper onClick={() => window.open(column.value)}>
-                          <WhiteBgDiscord />
-                        </IconWrapper>
+                    {column.component === "discord" ? (
+                      <Box display="flex" justifyContent="center" alignItems="center">
+                        <Label fontSize="14px" lineHeight="14px" textAlign="center" {...(column.componentProps || {})}>
+                          {column?.value}
+                        </Label>
                       </Box>
                     ) : null}
-                    {column.component === 'twitter' ? (
-                      <Box
-                        display='flex'
-                        justifyContent='center'
-                        alignItems='center'
-                      >
-                        {column?.value ? (
-                          <Label
-                            fontSize='14px'
-                            lineHeight='14px'
-                            textAlign='center'
-                            width='100%'
-                          >
+                    {column.component === "twitter" ? (
+                      <Box display="flex" justifyContent="center" alignItems="center">
+                        {!(column?.value && !column?.value?.includes("undefined")) ? (
+                          <Label fontSize="14px" lineHeight="14px" textAlign="center" width="100%">
                             None
                           </Label>
                         ) : (
-                          <IconWrapper
-                            onClick={() => window.open(column.value)}
-                          >
+                          <IconWrapper onClick={() => window.open(column.value)}>
                             <Twitter
                               sx={{
-                                color: 'white',
-                                fontSize: '15px',
+                                color: "white",
+                                fontSize: "15px",
                               }}
                             />
                           </IconWrapper>
                         )}
                       </Box>
                     ) : null}
-                    {column.component === 'reward' ? (
-                      <StyledViewQuestResults $isReward>
-                        {column.value}
-                      </StyledViewQuestResults>
+                    {column.component === "reward" ? (
+                      <StyledViewQuestResults $isReward>{column.value}</StyledViewQuestResults>
                     ) : null}
-                    {column.component === 'custom' ? (
-                      <column.customComponent value={column.value} />
-                    ) : null}
+                    {column.component === "custom" ? <column.customComponent value={column.value} /> : null}
                   </TableCell>
                 );
               })}
