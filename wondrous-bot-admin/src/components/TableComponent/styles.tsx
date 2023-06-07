5817f73fe64ef49fde7b85ac24b4afff8de61a74
Andros <wong22@hotmail.co.uk>
2023-06-07 11:56:24 -0700

    fix: styles

--- wondrous-bot-admin/src/components/TableComponent/styles.tsx
+++ wondrous-bot-admin/src/components/TableComponent/styles.tsx
@@ -1,36 +1,32 @@
-import styled from 'styled-components';
-import {
-  Table,
-  TableBody,
-  TableCell,
-  TableContainer,
-  TableHead,
-  TableRow,
-  Paper,
-  ButtonBase,
-} from '@mui/material';
+import styled from "styled-components";
+import { Table, TableBody, TableCell, TableContainer, TableHead, TableRow, Paper, ButtonBase } from "@mui/material";
 
 export const StyledTableHeader = styled(TableRow)`
-  background-color: #2a8d5c;
-  height: 48px;
+  && {
+    background-color: #2a8d5c;
+    height: 48px;
+  }
 `;
 
 export const StyledTableHeaderCell = styled(TableCell)`
-  font-weight: bold !important;
-  color: white !important;
-  font-family: 'Poppins' !important;
-  font-style: normal;
-  font-weight: 700 !important;
-  font-size: 14px !important;
-  line-height: 15px;
-  /* identical to box height, or 107% */
-  text-align: center;
-  letter-spacing: 0.01em;
+  && {
+    font-weight: bold !important;
+    color: white !important;
+    font-family: "Poppins" !important;
+    font-style: normal;
+    font-weight: 700 !important;
+    font-size: 14px !important;
+    line-height: 15px;
+    text-align: center !important;
+    letter-spacing: 0.01em;
+  }
 `;
 
 export const StyledTableRow = styled(TableRow)`
-  background-color: ${({ id }) => (id % 2 === 0 ? 'white' : '#f2f2f2')};
-  color: black;
+  && {
+    background-color: ${({ id }) => (id % 2 === 0 ? "white" : "#f2f2f2")};
+    color: black;
+  }
 `;
 
 export const PaperComponent = styled(Paper)`
