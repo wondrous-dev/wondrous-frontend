86c24090e71a14066718461864d4275b8434debe
Andros <wong22@hotmail.co.uk>
2023-07-01 12:08:42 -0700

    feat: add tool tips for max approvals and max submissions

--- wondrous-bot-admin/src/components/ViewQuestResults/ViewCampaignOverview.tsx
+++ wondrous-bot-admin/src/components/ViewQuestResults/ViewCampaignOverview.tsx
@@ -1,32 +1,65 @@
-import { Grid } from '@mui/material';
-import { Label } from 'components/CreateTemplate/styles';
-import { StyledViewQuestResults } from './styles';
+import { Grid, Tooltip } from "@mui/material";
+import { Label } from "components/CreateTemplate/styles";
+import InformationTooltip from "components/Icons/information.svg";
+import { StyledViewQuestResults } from "./styles";
+import { StyledInformationTooltip } from "components/Shared/Tooltip";
 
-const getBooleanText = (value) => (value ? 'Yes' : 'No');
+const getBooleanText = (value) => (value ? "Yes" : "No");
 
 const ViewCampaignOverview = ({ questSettings }) => {
   return (
     <>
       {questSettings?.map((quest, idx) => {
+        console.log("quest, qe", quest);
         return (
           <Grid
-            display='flex'
-            justifyContent='flex-start'
-            alignItems='center'
-            width='100%'
-            key={idx + 'quest'}
-            gap='10%'
+            display="flex"
+            justifyContent="flex-start"
+            alignItems="center"
+            width="100%"
+            key={idx + "quest"}
+            gap="10%"
           >
             <Label>{quest.label}</Label>
-            {quest.type !== 'rewards' ? (
-              <StyledViewQuestResults>
-                {quest.type === 'boolean' ? getBooleanText(quest.value) : null}
-                {quest.type === 'text' ? quest.value : null}
-              </StyledViewQuestResults>
+            {quest.type !== "rewards" ? (
+              <>
+                <StyledViewQuestResults
+                  style={{
+                    position: "relative",
+                  }}
+                >
+                  {quest.type === "boolean" ? getBooleanText(quest.value) : null}
+                  {quest.type === "text" ? quest.value : null}
+                  {quest.label === "Max Submissions" && (
+                    <StyledInformationTooltip
+                      placement="right"
+                      title="The maximum number of approved submissions each user can submit for this quest"
+                    >
+                      <img
+                        src={InformationTooltip}
+                        alt="information"
+                        style={{ width: "16px", height: "16px", marginLeft: "8px", position: "absolute", right: -26 }}
+                      />
+                    </StyledInformationTooltip>
+                  )}
+                  {quest.label === "Max Approvals" && (
+                    <StyledInformationTooltip
+                      placement="right"
+                      title="The total number of approved submissions allowed for this quest"
+                    >
+                      <img
+                        src={InformationTooltip}
+                        alt="information"
+                        style={{ width: "16px", height: "16px", marginLeft: "8px", position: "absolute", right: -26 }}
+                      />
+                    </StyledInformationTooltip>
+                  )}
+                </StyledViewQuestResults>
+              </>
             ) : null}
-            {quest.type === 'rewards'
+            {quest.type === "rewards"
               ? quest.value.map((reward, key) => (
-                  <StyledViewQuestResults $isReward key={reward.type + 'reward'}>
+                  <StyledViewQuestResults $isReward key={reward.type + "reward"}>
                     {reward.value} {reward.type}
                   </StyledViewQuestResults>
                 ))
