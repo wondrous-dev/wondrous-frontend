2568fbdc9881a5d92a4429a77efab2c28c114658
terryli0095 <terryli0095@gmail.com>
2023-07-05 11:09:45 -0700

    add comments

--- wondrous-bot-admin/src/services/web3/contractRouter.tsx
+++ wondrous-bot-admin/src/services/web3/contractRouter.tsx
@@ -4,22 +4,22 @@ export enum ContractType {
     ERC1155 = 'ERC1155',
 }
 const CHAIN_TO_MULTISEND_ADDRESS = {
-    1: {
+    1: { // mainnet
         [ContractType.ERC20]: '',
         [ContractType.ERC721]: '',
         [ContractType.ERC1155]: '',
     },
-    4: {
+    4: { // rinkeby
         [ContractType.ERC20]: '',
         [ContractType.ERC721]: '',
         [ContractType.ERC1155]: '',
     },
-    137: {
+    137: { // polygon
         [ContractType.ERC20]: '0xa97EF76725D7A7B3066EA7b6c17C4DAbA469FDbF',
         [ContractType.ERC721]: '0xa97EF76725D7A7B3066EA7b6c17C4DAbA469FDbF',
         [ContractType.ERC1155]: '0xa97EF76725D7A7B3066EA7b6c17C4DAbA469FDbF',
     },
-    5: {
+    5: { // goerli
         [ContractType.ERC20]: '0x83703892a562d5ec05b5f9453e8a57c8cffc99e6',
         [ContractType.ERC721]: '0x83703892a562d5ec05b5f9453e8a57c8cffc99e6',
         [ContractType.ERC1155]: '0x83703892a562d5ec05b5f9453e8a57c8cffc99e6',
