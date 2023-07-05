c1ac7c7d07004713a35bb91490054520d09c5540
Andros <wong22@hotmail.co.uk>
2023-07-04 18:21:07 -0700

    fix: add tutorial button

--- wondrous-bot-admin/src/components/Navbar/styles.tsx
+++ wondrous-bot-admin/src/components/Navbar/styles.tsx
@@ -1,8 +1,8 @@
-import styled from 'styled-components';
+import styled from "styled-components";
 
-import AppBar from '@mui/material/AppBar';
-import { HEADER_HEIGHT } from 'utils/constants';
-import { ButtonBase } from '@mui/material';
+import AppBar from "@mui/material/AppBar";
+import { HEADER_HEIGHT } from "utils/constants";
+import { ButtonBase } from "@mui/material";
 
 export const HeaderBar = styled(AppBar)`
   && {
@@ -18,7 +18,7 @@ export const HeaderBar = styled(AppBar)`
     justify-content: space-between;
     align-items: center;
     flex-direction: row;
-    ${({ theme }) => theme.breakpoints.down('md')} {
+    ${({ theme }) => theme.breakpoints.down("md")} {
       top: 0;
       right: 0;
       width: 100%;
@@ -44,13 +44,13 @@ export const LinkButton = styled(ButtonBase)`
   && {
     border: 2px solid #ffffff;
     width: 100%;
-    background: ${({ bgColor }) => bgColor || 'white'};
+    background: ${({ bgColor }) => bgColor || "white"};
     border-radius: 12px;
     justify-content: space-between;
     align-items: center;
     padding: 14px 24px;
     display: flex;
-    font-family: 'Poppins';
+    font-family: "Poppins";
     font-style: normal;
     font-weight: 600;
     font-size: 18px;
@@ -61,4 +61,26 @@ export const LinkButton = styled(ButtonBase)`
 
     color: #000000;
   }
+`;
+
+export const TutorialButton = styled.div`
+  width: 34.5px;
+  height: 34.5px;
+  border-radius: 50%;
+  margin-right: 16px;
+  margin-left: -40px;
+  border: 1.5px solid #baacfa;
+  color: #baacfa;
+  display: flex;
+  cursor: pointer;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const TutorialLink = styled.a`
+  text-decoration: none;
+  outline: 0;
+  color: #baacfa;
+  font-weight: 500;
+  font-size: 19px;
 `;
