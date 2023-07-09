b7f9736971ec6bcc29302df6937cabcea8425532
juniusfree <8164667+juniusfree@users.noreply.github.com>
2023-07-09 17:53:04 +0800

    feat: premium feature modal

---
+++ wondrous-bot-admin/src/components/PremiumFeatureDialog/index.tsx
@@ -0,0 +1,91 @@
+import { Button, Dialog, Grid, Typography } from "@mui/material";
+
+type PremiumFeatureDialogProps = {
+  open: boolean;
+  onClose: (value: string) => void;
+};
+
+const PremiumFeatureDialog = ({ open = false, onClose }: PremiumFeatureDialogProps) => {
+  return (
+    <Dialog
+      open={open}
+      onClose={onClose}
+      PaperProps={{
+        sx: { borderRadius: "16px", boxShadow: "none", outline: "1px solid black" },
+      }}
+      slotProps={{
+        backdrop: {
+          sx: { backgroundColor: "#AF9EFFCC", backdropFilter: "blur(5px)" },
+        },
+      }}
+    >
+      <Grid container width="464px" display="flex" flexDirection="column" alignItems="center" borderRadius="16px">
+        <Grid container item padding="24px" display="flex" flexDirection="column" alignItems="center" gap="14px">
+          <Typography color="#2A8D5C" fontFamily="Poppins" fontSize="24px" fontWeight="700" textAlign="center">
+            This is a premium feature
+          </Typography>
+          <Typography color="#5E5E5E" fontFamily="Poppins" fontSize="15px" fontWeight="500" textAlign="center">
+            Ready to increase your engagement with Communities Premium.
+          </Typography>
+          <img src="/images/premium-feature-modal.png" alt="premium feature modal" />
+        </Grid>
+        <Grid
+          container
+          item
+          padding="24px"
+          borderTop="1px solid #E8E8E8"
+          flexDirection="column"
+          alignItems="center"
+          width="100%"
+          gap="12px"
+        >
+          <Button
+            disableRipple
+            disableFocusRipple
+            sx={{
+              backgroundColor: "#84BCFF",
+              height: "40px",
+              width: "100%",
+              borderRadius: "100px",
+              textTransform: "none",
+              color: "#000",
+              textAlign: "center",
+              fontFamily: "Poppins",
+              fontSize: "15px",
+              fontWeight: 600,
+
+              "&:hover": {
+                backgroundColor: "#84BCFF",
+              },
+            }}
+          >
+            Upgrade
+          </Button>
+          <Button
+            disableRipple
+            disableFocusRipple
+            sx={{
+              height: "40px",
+              width: "100%",
+              borderRadius: "100px",
+              textTransform: "none",
+              color: "#6D6D6D",
+              textAlign: "center",
+              fontFamily: "Space Grotesk",
+              fontSize: "16px",
+              fontWeight: 700,
+
+              "&:hover": {
+                backgroundColor: "#fff",
+              },
+            }}
+          >
+            I'll upgrade later
+          </Button>
+        </Grid>
+      </Grid>
+    </Dialog>
+  );
+};
+
+export default PremiumFeatureDialog;
