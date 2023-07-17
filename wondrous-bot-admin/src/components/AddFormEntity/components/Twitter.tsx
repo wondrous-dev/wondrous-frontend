fe96df121bee3d60f327d0ab528e98a06b1b5071
terryli0095 <terryli0095@gmail.com>
2023-07-17 16:55:20 -0700

    fix twitter

--- wondrous-bot-admin/src/components/AddFormEntity/components/Twitter.tsx
+++ wondrous-bot-admin/src/components/AddFormEntity/components/Twitter.tsx
@@ -53,7 +53,7 @@ const TwitterFollowText = ({ handleOnChange, value, error }) => (
     <Label>Follow this Twitter account</Label>
     <TextField
       placeholder="Please enter the twitter handle to follow e.g. wonderverse_xyz"
-      value={value?.tweetHandle}
+      value={value?.tweetHandle?.replace("@", "")}
       onChange={(value) => handleOnChange("tweetHandle", value)}
       multiline={false}
       error={error}
