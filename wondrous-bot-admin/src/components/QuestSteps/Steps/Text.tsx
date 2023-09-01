2a0a054d45d11e1130481747c49fd9ac202fdfad
adrian mustea <mustea.adrian95@gmail.com>
2023-09-01 12:18:29 +0300

    fix num

--- wondrous-bot-admin/src/components/QuestSteps/Steps/Text.tsx
+++ wondrous-bot-admin/src/components/QuestSteps/Steps/Text.tsx
@@ -1,15 +1,32 @@
 import { Grid } from "@mui/material";
-import { Label } from "components/AddFormEntity/components/styles";
 import TextField from "components/Shared/TextField";
 
+const validateTypes = (type, value) => {
+  if (type === "number" || type === "tel") {
+    const re = /^[0-9\b]+$/;
+    if (value === "" || re.test(value)) {
+      return true;
+    }
+    return false;
+  }
+  return true;
+};
+
 export const StepTextField = ({ step, onChange, value, placeholder = "", type = "text" }) => {
+  const handleInputChange = (value) => {
+    const isValid = validateTypes(type, value);
+    if (isValid) {
+      return onChange(value);
+    }
+  };
+
   return (
     <>
       <Grid display="flex" gap="8px" flexDirection="column" width="100%">
         <TextField
           multiline={false}
           type={type}
-          onChange={onChange}
+          onChange={handleInputChange}
           value={value}
           placeholder={placeholder}
           autoFocus="true"
