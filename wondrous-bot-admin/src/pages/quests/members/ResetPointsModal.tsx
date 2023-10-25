51545063d484128df59bf592261322417b4fab90
adrian mustea <mustea.adrian95@gmail.com>
2023-10-25 19:29:01 +0300

    change modal title

--- wondrous-bot-admin/src/pages/quests/members/ResetPointsModal.tsx
+++ wondrous-bot-admin/src/pages/quests/members/ResetPointsModal.tsx
@@ -129,7 +129,7 @@ const ResetPointsModal = ({ openResetPointsModal, setOpenResetPointsModal, point
         maxWidth={600}
         open={openResetPointsModal}
         onClose={() => setOpenResetPointsModal(false)}
-        title="Upload discount codes for this item"
+        title="Reset member points"
       >
         <ResetPointsModalBody onClose={() => setOpenResetPointsModal(false)} pointsBalance={pointsBalance} />
       </Modal>
