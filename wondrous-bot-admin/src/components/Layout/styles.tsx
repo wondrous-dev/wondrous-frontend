36ed7f92cded563d71355f11fe46b15717d8f3fa
adrian mustea <mustea.adrian95@gmail.com>
2023-12-14 21:45:01 +0200

    sidebar icons style

--- wondrous-bot-admin/src/components/Layout/styles.tsx
+++ wondrous-bot-admin/src/components/Layout/styles.tsx
@@ -1,7 +1,7 @@
 import styled from "styled-components";
 
 export const Main = styled.main`
-  padding-top: ${({ $isPageWithoutHeader }) => ($isPageWithoutHeader ? "0" : "82px")};
+  // padding-top: ${({ $isPageWithoutHeader }) => ($isPageWithoutHeader ? "0" : "82px")};
   flex: 1;
   width: 100%;
 `;
