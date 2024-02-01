bc17ffd5425bc3327902a6558232e88063b0bfa2
Andros <wong22@hotmail.co.uk>
2024-02-01 06:34:11 -0800

    fix: constants

--- wondrous-bot-admin/src/utils/constants.tsx
+++ wondrous-bot-admin/src/utils/constants.tsx
@@ -129,7 +129,7 @@ export const PAGES_WITHOUT_HEADER = [
   "/community-badge/claim",
   "/referral-campaign",
   "/onboarding/plan-select",
-  '/onboarding/finalize'
+  "/onboarding/finalize",
 ];
 
 export const BG_TYPES = {
@@ -419,7 +419,6 @@ const APEIRON_INTEGRATIONS = [
   },
 ];
 
-
 const FHENIX_INTEGRATIONS = [
   {
     label: "Verify Fhenix Wallet Gas Usage",
@@ -473,7 +472,7 @@ export const CUSTOM_INTEGRATIONS = {
     name: "Apeiron",
     integrations: APEIRON_INTEGRATIONS,
   },
-  "89444950095167649": {
+  "110521236745880560": {
     name: "Fhenix",
     integrations: FHENIX_INTEGRATIONS,
   },
